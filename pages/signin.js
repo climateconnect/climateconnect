6ee1d0ee4f0137e86899105c6a72068455d22fb4
reecelangerock <reecelangerock@gmail.com>
2019-10-29 06:25:40 -0500

    add placeholder pages

---
+++ pages/signin.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+import Layout from "../src/components/layout";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Sign In</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Layout></Layout>
+  </div>
+);
+
+export default Home;
