89e30485017f0b4f28ff2848811d42dba01435bf
reecelangerock <reecelangerock@gmail.com>
2019-10-29 06:26:16 -0500

    add links, more styling

--- src/components/general/Header.js
+++ src/components/general/Header.js
@@ -1,31 +1,24 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
-
-const links = [
-  { href: "https://zeit.co/now", label: "ZEIT" },
-  { href: "https://github.com/zeit/next.js", label: "GitHub" }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
-});
+import Button from "../ui/Button";
 
 const Header = () => (
   <Container>
     <img src="https://climateconnect.earth/logo.png"></img>
     <LinkContainer>
-      <Link href={"test"} passHref>
+      <Link href={"forum"} passHref>
         <StyledLink>Forum</StyledLink>
       </Link>
-      <StyledLink>
+      <Link href={"browse"} passHref>
         <StyledLink>Browse</StyledLink>
-      </StyledLink>
-      <StyledLink>
+      </Link>
+      <Link href={"create"} passHref>
         <StyledLink>Create A Project</StyledLink>
-      </StyledLink>
-      <StyledLink>
-        <StyledLink>SIGN IN</StyledLink>
-      </StyledLink>
+      </Link>
+      <Link href={"signin"}>
+        <StyledLink className="button">SIGN IN</StyledLink>
+      </Link>
     </LinkContainer>
   </Container>
 );
@@ -33,25 +26,43 @@ const Header = () => (
 export default Header;
 
 const Container = styled.nav`
-  grid-area: "header";
   display: flex;
   justify-content: space-between;
   height: 40px;
   align-items: center;
-  padding: 0.5rem 3rem;
-  border-bottom: 1px solid #eaeaea;
+  padding: 12px 48px;
+  border-bottom: 1px solid hsla(0, 0%, 82%, 1);
 
   img {
-    height: 40px;
-    width: 40px;
+    height: 54px;
+    width: 54px;
   }
 `;
 
 const LinkContainer = styled.div``;
 
 const StyledLink = styled.a`
-  padding: 8px;
-  color: #237177;
-  margin: 8px;
+  padding: 6px;
+  color: hsla(185, 56%, 30%, 1);
+  margin-right: 24px;
   text-decoration: none;
+  cursor: pointer;
+  font-size: 1.1rem;
+  transition: 0.25s ease-in-out;
+
+  :hover {
+    color: hsla(185, 56%, 30%, 1);
+    transition: 0.25s ease-in-out;
+  }
+
+  &.button {
+    border: 1px solid hsla(0, 0%, 55%, 1);
+    padding: 6px 16px;
+    border-radius: 4px;
+    color: hsla(0, 0%, 30%, 1);
+    :hover {
+      color: hsla(0, 0%, 20%, 1);
+      transition: 0.25s ease-in-out;
+    }
+  }
 `;
