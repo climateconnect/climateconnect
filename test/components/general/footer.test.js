524ee70165c9081156b7838ed5621630720e963b
reecelangerock <reecelangerock@gmail.com>
2019-11-02 11:41:30 -0500

    add component tests

---
+++ test/components/general/footer.test.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import { shallow } from "enzyme";
+// import Footer from "../../../src/components/general/Footer";
+
+// placeholder until footer has content
+describe("Footer Component Tests", () => {
+  it("contains correct image", () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ test/components/general/header.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "../../../src/components/general/Header";
+import Link from "../../../src/components/ui/Link";
+
+describe("Header Component Tests", () => {
+  it("contains correct image", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find("img")).toHaveLength(1);
+  });
+  it("contains four links", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find(Link)).toHaveLength(4);
+  });
+  it("links contain correct props", () => {
+    const wrapper = shallow(<Header />);
+
+    const expectedProps = [
+      { href: "forum", passHref: true, text: "Forum" },
+      { href: "browse", passHref: true, text: "Browse" },
+      { href: "create", passHref: true, text: "Create A Project" },
+      { href: "signin", passHref: true, text: "SIGN IN", type: "button" }
+    ];
+
+    for (let i = 0; i < 4; i++) {
+      const link = wrapper.find(Link).at(i);
+      expect(link.props()).toEqual(expectedProps[i]);
+    }
+  });
+});
