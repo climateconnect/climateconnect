8f2e0dc35f5dc0d02d0b7b4ffd53a8e892a22a0f
Christoph Stoll <christoph1.stoll@gmail.com>
2019-11-28 14:23:24 +0100

    add icons, text

--- src/layouts/AboutLayout.js
+++ src/layouts/AboutLayout.js
@@ -3,29 +3,34 @@ import styled from "styled-components";
 
 const info = [
   {
-    icon: null,
+    icon: "plus.svg",
     title: "Share your actions",
-    text: "test"
+    text:
+      "Have you ever wondered what is being done about climate change in your region and globally? By enabling climate protectors to share their project on Climate Connect, we will provide a comprehensive up to date list of what is being done about climate change."
   },
   {
-    icon: null,
+    icon: "collaboration.svg",
     title: "Measure the impact",
-    text: "test"
+    text:
+      "You are thinking about getting involved in climate action, but don't know how you can be most effective? On Climate Connect each action will have an impact score. This way you can find out how you can maximize your impact"
   },
   {
-    icon: null,
+    icon: "collaboration.svg",
     title: "Collaborate globally",
-    text: "test"
+    text:
+      "Climate change is a global issue. This is why we want to create a global network of climate protectors. Climate Connect allows you to see where you can make the biggest difference with your personal strengths and skillset. If you're already working on an impactful project, you will be able to find collaborators here."
   },
   {
-    icon: null,
+    icon: "lightbulb.svg",
     title: "Get inspired",
-    text: "test"
+    text:
+      "On Climate Connect, you will find blue prints of high-impact projects, that you will be able to replicate at your location. This way you can very easily find the most effective thing you can work on with your specific skillset."
   },
   {
-    icon: null,
+    icon: "eye.svg",
     title: "Maximum transparency",
-    text: "test"
+    text:
+      "Our code is available open source and can be found in our Github repository. The impact algorithm will be open source and the community will make the decisions on how to develop it further."
   }
 ];
 
@@ -41,9 +46,7 @@ export default function about() {
       <AboutImageContainer>
         <HeadlineContainer>
           <Headline>
-            <HeadlineText>
-            CLIMATE CONNECT
-            </HeadlineText>
+            <HeadlineText>CLIMATE CONNECT</HeadlineText>
           </Headline>
         </HeadlineContainer>
       </AboutImageContainer>
@@ -53,9 +56,18 @@ export default function about() {
       <h1>Our goal is to help you fight climate change most effectively</h1>
       <BubbleGrid>{renderBubbles(info)}</BubbleGrid>
       <h1>Find out more</h1>
-      <h2>Read our onepager</h2>
-      <h2>View our interactive prototype</h2>
-      <h2>View our FAQ section for in depth explanations</h2>
+      <LinkContainer>
+        <img src="/file.svg"></img>
+        <h2>Read our onepager</h2>
+      </LinkContainer>
+      <LinkContainer>
+        <img src="/blueprint.svg"></img>
+        <h2>View our interactive prototype</h2>
+      </LinkContainer>
+      <LinkContainer>
+        <img src="/algorithm.svg"></img>
+        <h2>View our FAQ section for in depth explanations</h2>
+      </LinkContainer>
       <h1>Join our Team</h1>
       {/*Photo Grid*/}
       <h2>Currently all members of our team are volunteers.</h2>
@@ -66,12 +78,27 @@ export default function about() {
 }
 
 const InfoBubble = ({ data }) => (
-  <div>
-    <Bubble></Bubble>
+  <InfoBubbleContainer>
+    <Bubble>
+      <IconWrapper>
+        <img src={data.icon}></img>
+      </IconWrapper>
+    </Bubble>
     <h3>{data.title}</h3>
-    <div>{data.text}</div>
-  </div>
+    <InfoText>{data.text}</InfoText>
+  </InfoBubbleContainer>
 );
+
+const InfoBubbleContainer = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  padding-left: 50px;
+  padding-right: 50px;
+  h3 {
+    font-size: 35px;
+  }
+  margin-bottom: 50px;
+`;
 
 const AboutImageContainer = styled.div`
   width: 100%;
@@ -79,25 +106,33 @@ const AboutImageContainer = styled.div`
   background-image: url("/about_background.png");
   background-position: center;
   background-size: cover;
+  margin-bottom: 75px;
 `;
 
 const HeadlineContainer = styled.div`
-  display:table;
+  display: table;
   height: 100%;
-  width:100%;
+  width: 100%;
 `;
 
-const Headline = styled.div`  
+const Headline = styled.div`
   display: table-cell;
   vertical-align: middle;
   width: 100%;
 `;
 
+const InfoText = styled.div`
+  max-width: 350px;
+  color: #484848;
+  margin: 0 auto;
+  font-size: 20px;
+`;
+
 const HeadlineText = styled.div`
-  color:white;
+  color: white;
   font-size: 85px;
   border: 5px solid white;
-  display:inline-block;
+  display: inline-block;
   margin: 0 auto;
   padding: 69.5px;
   padding-right: 291.5px;
@@ -117,13 +152,10 @@ const Container = styled.div`
 `;
 
 const BubbleGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 32px 24px;
-  align-items: center;
   margin-bottom: 3rem;
-  max-width:1390px;
-  margin:0 auto;
+  max-width: 1390px;
+  margin: 0 auto;
+  margin-top: 50px;
   @media (max-width: 1000px) {
     grid-template-columns: 1fr 1fr;
   }
@@ -138,4 +170,22 @@ const Bubble = styled.div`
   width: 150px;
   height: 150px;
   margin: auto;
+  display: table;
+`;
+
+const IconWrapper = styled.div`
+  width: 75px;
+  display: table-cell;
+  vertical-align: middle;
+  margin: 0 auto;
+`;
+
+const LinkContainer = styled.div`
+  img {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  h2 {
+    display: inline-block;
+  }
 `;
