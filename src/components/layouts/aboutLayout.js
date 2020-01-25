df7e339970f84849d63049cdf0d0684b8a5bde9d
Christoph Stoll <christoph1.stoll@gmail.com>
2020-01-25 22:42:15 +0100

    rm unnecesary container from about layout

--- src/components/layouts/aboutLayout.js
+++ src/components/layouts/aboutLayout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Header from "../general/Header";
 import Footer from "../general/Footer";
-import { Container, Typography } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
 import theme from "../../themes/aboutTheme";
 
@@ -16,14 +16,12 @@ const useStyles = makeStyles({
   }
 });
 
-export default function Layout({ title, children }) {
+export default function Layout({ children }) {
   const classes = useStyles();
 
   return (
     <>
       <Head>
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"
@@ -34,11 +32,6 @@ export default function Layout({ title, children }) {
       <ThemeProvider theme={theme}>
         <Header />
         <Container maxWidth={false} component="main" className={classes.main}>
-          <Container maxWidth={false}>
-            <Typography variant="h1" className={classes.mainHeading}>
-              {title}
-            </Typography>
-          </Container>
           {children}
         </Container>
         <Footer />
