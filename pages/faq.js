6b6cff8a3e87ed815b5969cd0038d46dd8b1735f
Christoph Stoll <christoph1.stoll@gmail.com>
2020-02-05 13:27:53 +0100

    Add coming soon msg to faq page

--- pages/faq.js
+++ pages/faq.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Layout from "../src/components/layouts/layout";
+import { Typography } from "@material-ui/core";
 
 const Faq = () => (
   <div>
-    <Layout title="FAQ" />
+    <Layout title="FAQ">
+      <Typography align="center" variant="h5" color="primary" component="h2">
+        This page is coming soon!
+      </Typography>
+    </Layout>
   </div>
 );
 
