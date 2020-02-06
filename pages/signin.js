f0b965f7625c2b035a4c8ce0a2c07eb006da3fb5
Christoph Stoll <christoph1.stoll@gmail.com>
2020-02-06 15:14:39 +0100

    Feature/sign in page (#81)

--- pages/signin.js
+++ pages/signin.js
@@ -1,13 +1,73 @@
 import React from "react";
 import Layout from "../src/components/layouts/layout";
-import { Typography } from "@material-ui/core";
+import Link from "next/link";
+import { TextField, Button, Card, Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Home = () => (
-  <Layout title="Sign In">
-    <Typography align="center" variant="h5" color="primary" component="h2">
-      This page is coming soon!
-    </Typography>
-  </Layout>
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4),
+    maxWidth: 350,
+    margin: "0 auto"
+  },
+  blockElement: {
+    display: "block",
+    maxWidth: 250,
+    height: 56,
+    margin: "0 auto",
+    marginTop: theme.spacing(2)
+  },
+  bottomMessages: {
+    textAlign: "center",
+    display: "block"
+  },
+  bottomMessageContainer: {
+    marginTop: theme.spacing(2)
+  }
+}));
 
-export default Home;
+export default function Signin() {
+  const classes = useStyles();
+
+  return (
+    <Layout title="Sign In">
+      <Card className={classes.root}>
+        <form action="/create" method="GET">
+          <div>
+            <TextField
+              required
+              fullWidth
+              autoFocus
+              label="Email"
+              variant="outlined"
+              className={classes.blockElement}
+            />
+          </div>
+          <TextField
+            required
+            fullWidth
+            label="Password"
+            variant="outlined"
+            className={classes.blockElement}
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            type="submit"
+            color="primary"
+            size="large"
+            className={classes.blockElement}
+          >
+            Sign In
+          </Button>
+        </form>
+        <Container className={classes.bottomMessageContainer}>
+          <div className={classes.bottomMessages}>New to Climate Connect?</div>
+          <Link href="/signup">
+            <a className={classes.bottomMessages}>Create an Account</a>
+          </Link>
+        </Container>
+      </Card>
+    </Layout>
+  );
+}
