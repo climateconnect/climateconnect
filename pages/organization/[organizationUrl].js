1a8adf7d3dfde2388b1647b1fae60a74d6c897ca
Christoph Stoll <christoph1.stoll@gmail.com>
2020-02-14 16:24:41 +0100

    build project page without showing members

--- pages/organization/[organizationUrl].js
+++ pages/organization/[organizationUrl].js
@@ -1,77 +1,78 @@
 import React from "react";
 import Link from "next/link";
 import WideLayout from "../../src/components/layouts/WideLayout";
+//import ProfilePreviews from "../../src/components/profile/ProfilePreviews";
 import ProjectPreviews from "../../src/components/project/ProjectPreviews";
 import { Container, Avatar, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import TEMP_FEATURED_DATA from "../../public/data/organizations.json";
 import TEMP_PROJECT_DATA from "../../public/data/projects.json";
+//import TEMP_MEMBER_DATA from "../../public/data/profiles.json";
 
 const DEFAULT_BACKGROUND_IMAGE = "/images/background1.jpg";
 
-const useStyles = makeStyles(theme => {
-  return {
-    background: {
-      width: "100%",
-      height: 305
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    height: theme.spacing(20),
+    width: theme.spacing(20),
+    margin: "0 auto",
+    marginTop: theme.spacing(-11),
+    fontSize: 50,
+    backgroundcolor: "white",
+    "& img": {
+      objectFit: "contain",
+      backgroundColor: "white"
     },
-    avatar: {
-      height: theme.spacing(20),
-      width: theme.spacing(20),
-      margin: "0 auto",
-      marginTop: theme.spacing(-11),
-      fontSize: 50
-    },
-    avatarWithInfo: {
-      textAlign: "center",
-      width: theme.spacing(40),
-      margin: "0 auto",
-      [theme.breakpoints.up("sm")]: {
-        margin: 0,
-        display: "inline-block",
-        width: "auto"
-      }
-    },
-    memberInfo: {
-      [theme.breakpoints.up("sm")]: {
-        display: "inline-block"
-      },
-      padding: 0
-    },
-    name: {
-      fontWeight: "bold",
-      padding: theme.spacing(1)
-    },
-    subtitle: {
-      color: `${theme.palette.secondary.main}`
-    },
-    content: {
-      paddingTop: theme.spacing(1),
-      paddingBottom: theme.spacing(1),
-      color: `${theme.palette.secondary.main}`,
-      fontWeight: "bold"
-    },
-    noPadding: {
-      padding: 0
-    },
-    infoContainer: {
-      [theme.breakpoints.up("sm")]: {
-        display: "flex"
-      }
-    },
-    cardHeadline: {
-      marginTop: theme.spacing(3),
-      marginBottom: theme.spacing(1)
-    },
-    noprofile: {
-      textAlign: "center",
-      padding: theme.spacing(5)
+    border: `1px solid ${theme.palette.grey[300]}`
+  },
+  avatarWithInfo: {
+    textAlign: "center",
+    width: theme.spacing(40),
+    margin: "0 auto",
+    [theme.breakpoints.up("sm")]: {
+      margin: 0,
+      display: "inline-block",
+      width: "auto"
     }
-  };
-});
+  },
+  organizationInfo: {
+    [theme.breakpoints.up("sm")]: {
+      display: "inline-block"
+    },
+    padding: 0
+  },
+  name: {
+    fontWeight: "bold",
+    padding: theme.spacing(1)
+  },
+  subtitle: {
+    color: `${theme.palette.secondary.main}`
+  },
+  content: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    color: `${theme.palette.secondary.main}`,
+    fontWeight: "bold"
+  },
+  noPadding: {
+    padding: 0
+  },
+  infoContainer: {
+    [theme.breakpoints.up("sm")]: {
+      display: "flex"
+    }
+  },
+  cardHeadline: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(1)
+  },
+  noprofile: {
+    textAlign: "center",
+    padding: theme.spacing(5)
+  }
+}));
 
-export default function ProfilePage({ organization, projects }) {
-  console.log(organization);
+export default function OrganizationPage({ organization, projects }) {
   return (
     <WideLayout title={organization ? organization.name + "'s profile" : "Not found"}>
       {organization ? (
@@ -83,27 +84,32 @@ export default function ProfilePage({ organization, projects }) {
   );
 }
 
-ProfilePage.getInitialProps = async ctx => {
+OrganizationPage.getInitialProps = async ctx => {
   return {
     organization: await getOrganizationByUrlIfExists(ctx.query.organizationUrl),
-    projects: await getProjects(ctx.query.profileUrl)
+    projects: await getProjects(ctx.query.organizationUrl)
   };
 };
 
 function OrganizationLayout({ organization, projects }) {
+  if (!organization.background_image) organization.background_image = DEFAULT_BACKGROUND_IMAGE;
   const classes = useStyles();
   return (
     <Container maxWidth="lg" className={classes.noPadding}>
-      <img
-        src={
-          organization.background_image ? organization.background_image : DEFAULT_BACKGROUND_IMAGE
-        }
-        className={classes.background}
+      <div
+        style={{
+          background: `url(${organization.background_image})`,
+          "background-size": "cover",
+          "background-position": "center",
+          width: "100%",
+          height: 305
+        }}
       />
       <Container className={classes.infoContainer}>
         <Container className={classes.avatarWithInfo}>
           <Avatar
             alt={organization.name}
+            component="div"
             size="large"
             src={"/images/" + organization.logo}
             className={classes.avatar}
@@ -113,11 +119,19 @@ function OrganizationLayout({ organization, projects }) {
           </Typography>
           <Typography className={classes.subtitle}>{organization.type}</Typography>
         </Container>
-        <Container className={classes.memberInfo}>
+        <Container className={classes.organizationInfo}>
           {organization.shortdescription ? (
             <>
               <div className={classes.subtitle}>Description:</div>
               <div className={classes.content}>{organization.shortdescription}</div>
+            </>
+          ) : (
+            <></>
+          )}
+          {organization.location ? (
+            <>
+              <div className={classes.subtitle}>Location:</div>
+              <div className={classes.content}>{organization.location}</div>
             </>
           ) : (
             <></>
@@ -155,7 +169,12 @@ async function getOrganizationByUrlIfExists(organizationUrl) {
   return TEMP_FEATURED_DATA.organizations.find(({ url }) => url === organizationUrl);
 }
 
-async function getProjects(profileUrl) {
-  console.log("projects");
-  return TEMP_PROJECT_DATA.projects.find(({ members }) => members.includes(profileUrl));
+async function getProjects(organizationUrl) {
+  return TEMP_PROJECT_DATA.projects.filter(project =>
+    project.organization_url.includes(organizationUrl)
+  );
 }
+
+/*async function getMembers(organizationUrl) {
+  return TEMP_MEMBER_DATA.projects.filter(member => member.organizations.includes(organizationUrl));
+}*/
