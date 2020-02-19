eb6af7c5dbc82a2e1111920380caf36bde4b1b8d
Dip Dhanesha <ddhanesha@rainboo.com>
2020-02-18 23:24:31 -0800

    Updated database settings with env variables.

--- backend/settings.py
+++ backend/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'climateconnect_db',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
         'PORT': '5432'
     }
 }
