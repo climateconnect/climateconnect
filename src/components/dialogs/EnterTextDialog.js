9fac4273684f25ebb56cbbea043590d430e7616a
Christoph Stoll <christoph1.stoll@gmail.com>
2020-03-02 15:13:08 +0100

    create components for various types of dialogs

---
+++ src/components/dialogs/EnterTextDialog.js
@@ -0,0 +1,71 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import GenericDialog from "./GenericDialog";
+
+const useStyles = makeStyles({
+  textField: {
+    width: "100%"
+  }
+});
+
+export default function EnterTextDialog({
+  onClose,
+  open,
+  title,
+  inputLabel,
+  applyText,
+  maxLength,
+  className
+}) {
+  const classes = useStyles();
+  const [element, setElement] = React.useState(null);
+
+  const handleClose = () => {
+    onClose();
+    setElement(null);
+  };
+
+  const applyElement = () => {
+    onClose(element);
+    setElement(null);
+  };
+
+  const handleChange = event => {
+    setElement(event.target.value);
+  };
+
+  return (
+    <GenericDialog
+      onClose={handleClose}
+      open={open}
+      title={title}
+      useApplyButton={true}
+      onApply={applyElement}
+      applyText={applyText ? applyText : "Apply"}
+    >
+      <div className={className}>
+        <TextField
+          className={classes.textField}
+          label={inputLabel}
+          autoFocus={true}
+          variant="outlined"
+          onChange={handleChange}
+          defaultValue={element}
+          inputProps={{ maxLength: { maxLength } }}
+        />
+      </div>
+    </GenericDialog>
+  );
+}
+
+EnterTextDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  inputLabel: PropTypes.string.isRequired,
+  applyText: PropTypes.string.isRequired,
+  maxLength: PropTypes.number,
+  className: PropTypes.string
+};
