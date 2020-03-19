4eca99a9c0bddedfcfd469a18e97b495ffd4a815
Dip Dhanesha <ddhanesha@rainboo.com>
2020-03-18 21:55:42 -0700

    Remove unused imports.

--- backend/climateconnect_api/views/status_views.py
+++ backend/climateconnect_api/views/status_views.py
@@ -1,7 +1,6 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
 
