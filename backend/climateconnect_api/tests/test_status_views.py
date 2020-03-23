17ef680412a55b3edb8b38b7ea77b2b2ce997f66
Dip Dhanesha <ddhanesha@rainboo.com>
2020-03-22 20:01:25 -0700

    Added test cases for ping-pong-api.

---
+++ backend/climateconnect_api/tests/test_status_views.py
@@ -0,0 +1,10 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+
+
+class TestPingPongAPI(APITestCase):
+    def test_ping_pong_api_success(self):
+        url = reverse('ping-pong-api')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
