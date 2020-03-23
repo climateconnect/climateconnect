d13f1f0c2b5f7b2c9a4fa41a49ceeaebf50342e4
Christoph Stoll <christoph1.stoll@gmail.com>
2020-03-23 11:58:04 +0100

    Merge develop into master (#137)
    * Feature/profile page (#90)
    
    * start buiding profile page
    
    * finish profile page
    
    * rename AboutLayout to WideLayout
    
    * organisation -> organization
    
    * add border to avatar
    
    * make sure all projects are shown on profile page
    
    * adapt PR to comments
    
    * split skills w/ commas, dont show empty projects
    
    * Feature/organization page (#98)
    
    * add json files with data
    
    * start implementing organization page
    
    * build project page without showing members
    
    * keep building organisation page
    
    * finish organization page
    
    * fix accidental change
    
    * use plural url names
    
    * remove nonexistent root class
    
    * fix naming, remove unnecesary type, fix spelling
    
    * add first page of create a project workflow (#102)
    
    * add AccountPage (#100)
    
    * Create components for various types of dialogs (#101)
    
    * create components for various types of dialogs
    
    * update yarn.lock, small fix
    
    * Delete redundant file organisation_types.json
    
    * make select field more generic (#103)
    
    * Feature/edit pages (#104)
    
    * add json files with data
    
    * start implementing organization page
    
    * build project page without showing members
    
    * keep building organisation page
    
    * finish organization page
    
    * fix accidental change
    
    * edit account page
    
    * very messy version of edit account page
    
    * clean up account page
    
    * add AccountPage
    
    * bugfix: use {} for args of page function
    
    * Clean up dialogs
    
    * start cleaning up edit account page
    
    * finish cleaning up edit account page
    
    * Started a backend project.
    
    Updated README.md to include backend startup.
    
    * Small update on README frotnend section.
    
    * Add project example, change projects.json file strucuture, adapt code to new file structure (#106)
    
    * add example, adapt project file structure
    
    * use metadata file for project status
    
    * use getInitialProps in index, show status on index
    
    * catch full project status in component, adapt test
    
    * show status name again next to icon
    
    * create Post component for posts, comments, replies (#108)
    
    * Change envriornment variable name.
    
    * Updated README.md file. Added a new env variable for ALLOWED_HOSTS.
    
    * Changed file name for env file to be `backend_env`
    
    Updated gitignore.
    
    * Bump acorn from 5.7.3 to 5.7.4 (#111)
    
    Bumps [acorn](https://github.com/acornjs/acorn) from 5.7.3 to 5.7.4.
    - [Release notes](https://github.com/acornjs/acorn/releases)
    - [Commits](https://github.com/acornjs/acorn/compare/5.7.3...5.7.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Added ping/pong API.
    
    * Remove unused imports.
    
    * Feature/add message user interface (#128)
    
    * fix link in MiniProfilePreview
    
    * add space
    
    * add mock messages file
    
    * Run "yarn upgrade"
    
    * add FixedHeightLayout, adapt Footer and Header
    
    * add 2 new dependencies, upgrade yarn packages
    
    * add generic Date display
    
    * add inbox component
    
    * adapt mock message file
    
    * add notification bell to header
    
    * add messageUser page
    
    * Added test cases for ping-pong-api.
    
    * Updated README.md to include how to run backend tests suite.
    
    * Feature/project page (#135)
    
    * start working on implementing project page
    
    * add project page
    
    * add like button to replies
    
    * fix synchronous catching of comment creator
    
    * rename Date to DateDisplay
    
    * use DateDisplay, sort comments, posts by date
    
    * only highlight message button when hovering it
    
    * create ProjectOverview component
    
    * implement project page,create component for status
    
    * remove inactive member
    
    * add Fabian Schiller
    
    * add exportPathMap to next config,update deploy cmd
    
    Co-authored-by: Dip Dhanesha <ddhanesha@rainboo.com>
    Co-authored-by: Dip Dhanesha <ddhanesha@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -5,5 +5,12 @@ module.exports = withCSS({
   cssModules: true,
   env: {
     PRE_LAUNCH: process.env.PRE_LAUNCH
+  },
+  exportPathMap: async function(defaultPathMap) {
+    if (process.env.PRE_LAUNCH)
+      return {
+        "/": { page: "/" }
+      };
+    else return defaultPathMap;
   }
 });
