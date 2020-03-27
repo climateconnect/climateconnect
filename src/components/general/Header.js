e192c23fd15d6d49ef3119364323b4b572a2b21e
Christoph Stoll <christoph1.stoll@gmail.com>
2020-03-27 12:54:52 +0100

    add logged in version of header

--- src/components/general/Header.js
+++ src/components/general/Header.js
@@ -11,7 +11,13 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  Badge
+  Badge,
+  Avatar,
+  MenuList,
+  MenuItem,
+  Popper,
+  Paper,
+  ClickAwayListener
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
@@ -20,6 +26,9 @@ import InfoIcon from "@material-ui/icons/Info";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import SettingsIcon from "@material-ui/icons/Settings";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 const LINKS = [
   {
@@ -39,17 +48,56 @@ const LINKS = [
     hasBadge: true,
     //TODO: replace by user's unread notifications
     badgeNumber: 3,
-    showIconOnNormalScreen: true,
+    onlyShowIconOnNormalScreen: true,
+    onlyShowIconOnMobile: true,
     icon: NotificationsIcon,
-    alwaysDisplayDirectly: true
+    alwaysDisplayDirectly: true,
+    onlyShowLoggedIn: true
   },
   {
     href: "/signin",
     text: "Sign in",
     iconForDrawer: AccountCircleIcon,
-    isOutlinedInHeader: true
+    isOutlinedInHeader: true,
+    onlyShowLoggedOut: true,
+    alwaysDisplayDirectly: true
+  },
+  {
+    href: "/signup",
+    text: "Sign up",
+    iconForDrawer: AccountCircleIcon,
+    isOutlinedInHeader: true,
+    onlyShowLoggedOut: true,
+    showOnMobileOnly: true
   }
 ];
+
+const getLoggedInLinks = ({ loggedInUser }) => {
+  return [
+    {
+      href: "/profiles/" + loggedInUser.url,
+      text: "My Profile",
+      iconForDrawer: AccountCircleIcon
+    },
+    {
+      href: "/settings",
+      text: "Settings",
+      iconForDrawer: SettingsIcon
+    },
+    {
+      avatar: true,
+      href: "/profiles/" + loggedInUser.url,
+      src: loggedInUser.image,
+      alt: loggedInUser.name,
+      showOnMobileOnly: true
+    },
+    {
+      href: "/logouts",
+      text: "Log out",
+      iconForDrawer: ExitToAppIcon
+    }
+  ];
+};
 
 const useStyles = makeStyles(theme => {
   return {
@@ -73,11 +121,31 @@ const useStyles = makeStyles(theme => {
     },
     marginRight: {
       marginRight: theme.spacing(3)
+    },
+    loggedInRoot: {
+      display: "inline-block",
+      verticalAlign: "middle",
+      marginLeft: theme.spacing(2)
+    },
+    loggedInAvatar: {
+      height: 30,
+      width: 30,
+      display: "inline-block"
+    },
+    loggedInAvatarMobile: {
+      height: 60,
+      width: 60,
+      display: "block",
+      margin: "0 auto"
+    },
+    menuLink: {
+      color: theme.palette.primary.main,
+      textDecoration: "inherit"
     }
   };
 });
 
-export default function Header({ className, noSpacingBottom }) {
+export default function Header({ className, noSpacingBottom, loggedInUser }) {
   const classes = useStyles();
 
   const isNarrowScreen = useMediaQuery(theme => theme.breakpoints.down("xs"));
@@ -96,19 +164,30 @@ export default function Header({ className, noSpacingBottom }) {
         {process.env.PRE_LAUNCH === "true" ? (
           <></>
         ) : (
-          <>{isNarrowScreen ? <NarrowScreenLinks /> : <NormalScreenLinks />}</>
+          <>
+            {isNarrowScreen ? (
+              <NarrowScreenLinks loggedInUser={loggedInUser} />
+            ) : (
+              <NormalScreenLinks loggedInUser={loggedInUser} />
+            )}
+          </>
         )}
       </Container>
     </Box>
   );
 }
 
-function NormalScreenLinks() {
+function NormalScreenLinks({ loggedInUser }) {
   const classes = useStyles();
 
   return (
     <Box>
-      {LINKS.map((link, index) => {
+      {LINKS.filter(
+        link =>
+          !(loggedInUser && link.onlyShowLoggedOut) &&
+          !(!loggedInUser && link.onlyShowLoggedIn) &&
+          !link.showOnMobileOnly
+      ).map((link, index) => {
         const buttonProps = {};
         if (index !== 0) {
           buttonProps.className = classes.buttonMarginLeft;
@@ -119,7 +198,7 @@ function NormalScreenLinks() {
         const Icon = link.icon;
         return (
           <Link href={link.href} key={link.href}>
-            {link.showIconOnNormalScreen ? (
+            {link.onlyShowIconOnNormalScreen ? (
               <IconButton color="primary" {...buttonProps}>
                 {link.hasBadge && link.badgeNumber > 0 ? (
                   <Badge badgeContent={link.badgeNumber} color="error">
@@ -137,33 +216,53 @@ function NormalScreenLinks() {
           </Link>
         );
       })}
+      {loggedInUser && <LoggedInNormalScreen loggedInUser={loggedInUser} />}
     </Box>
   );
 }
 
-function NarrowScreenLinks() {
+function NarrowScreenLinks({ loggedInUser }) {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const openDrawer = setIsDrawerOpen.bind(null, true);
   const closeDrawer = setIsDrawerOpen.bind(null, false);
   const classes = useStyles();
+  const linksOutsideDrawer = LINKS.filter(
+    link =>
+      link.alwaysDisplayDirectly &&
+      !(loggedInUser && link.onlyShowLoggedOut) &&
+      !(!loggedInUser && link.onlyShowLoggedIn)
+  );
 
   return (
     <>
       <Box>
-        {LINKS.filter(link => link.alwaysDisplayDirectly).map(link => {
+        {linksOutsideDrawer.map((link, index) => {
           const Icon = link.iconForDrawer;
+          const buttonProps = {};
+          if (link.isOutlinedInHeader) {
+            buttonProps.variant = "outlined";
+          }
+          if (index === linksOutsideDrawer.length - 1) {
+            buttonProps.className = classes.marginRight;
+          }
           return (
             <Link href={link.href} key={link.href}>
-              <IconButton color="primary" className={classes.marginRight}>
-                {link.hasBadge && link.badgeNumber > 0 ? (
-                  <Badge badgeContent={link.badgeNumber} color="error">
+              {link.onlyShowIconOnMobile ? (
+                <IconButton color="primary" className={classes.marginRight}>
+                  {link.hasBadge && link.badgeNumber > 0 ? (
+                    <Badge badgeContent={link.badgeNumber} color="error">
+                      <Icon />
+                    </Badge>
+                  ) : (
                     <Icon />
-                  </Badge>
-                ) : (
-                  <Icon />
-                )}
-              </IconButton>
+                  )}
+                </IconButton>
+              ) : (
+                <Button color="primary" {...buttonProps}>
+                  {link.text}
+                </Button>
+              )}
             </Link>
           );
         })}
@@ -178,11 +277,16 @@ function NarrowScreenLinks() {
           onClose={closeDrawer}
           disableBackdropTransition={true}
         >
-          <List>
-            {LINKS.filter(link => !link.alwaysDisplayDirectly).map(link => {
+          <List styles={{ height: "100vh" }}>
+            {LINKS.filter(
+              link =>
+                !link.alwaysDisplayDirectly &&
+                !(loggedInUser && link.onlyShowLoggedOut) &&
+                !(!loggedInUser && link.onlyShowLoggedIn)
+            ).map((link, index) => {
               const Icon = link.iconForDrawer;
               return (
-                <Link href={link.href} key={link.href}>
+                <Link href={link.href} key={index}>
                   <ListItem button component="a" onClick={closeDrawer}>
                     <ListItemIcon>
                       <Icon />
@@ -192,9 +296,85 @@ function NarrowScreenLinks() {
                 </Link>
               );
             })}
+            {loggedInUser &&
+              getLoggedInLinks({ loggedInUser }).map((link, index) => {
+                const Icon = link.iconForDrawer;
+                if (link.avatar)
+                  return (
+                    <Link href={link.href} key={index}>
+                      <Avatar
+                        className={classes.loggedInAvatarMobile}
+                        src={loggedInUser.image}
+                        alt={loggedInUser.name}
+                      />
+                    </Link>
+                  );
+                else
+                  return (
+                    <Link href={link.href} key={index}>
+                      <ListItem button component="a" onClick={closeDrawer}>
+                        <ListItemIcon>
+                          <Icon />
+                        </ListItemIcon>
+                        <ListItemText primary={link.text} />
+                      </ListItem>
+                    </Link>
+                  );
+              })}
           </List>
         </SwipeableDrawer>
       </Box>
     </>
   );
 }
+
+const LoggedInNormalScreen = ({ loggedInUser }) => {
+  const classes = useStyles();
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleCloseMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <Box className={classes.loggedInRoot}>
+      <Button
+        onClick={handleToggleMenu}
+        disableElevation
+        disableRipple
+        disableFocusRipple
+        style={{ backgroundColor: "transparent" }}
+        ref={anchorRef}
+      >
+        <Avatar
+          className={classes.loggedInAvatar}
+          src={loggedInUser.image}
+          alt={loggedInUser.name}
+        />
+        <ArrowDropDownIcon />
+      </Button>
+      <Popper open={menuOpen} anchorEl={anchorRef.current}>
+        <ClickAwayListener onClickAway={handleCloseMenu}>
+          <Paper>
+            <MenuList>
+              {getLoggedInLinks({ loggedInUser })
+                .filter(link => !link.showOnMobileOnly)
+                .map((link, index) => (
+                  <MenuItem key={index}>
+                    <Link href={link.href}>
+                      <a className={classes.menuLink}>{link.text}</a>
+                    </Link>
+                  </MenuItem>
+                ))}
+            </MenuList>
+          </Paper>
+        </ClickAwayListener>
+      </Popper>
+    </Box>
+  );
+};
