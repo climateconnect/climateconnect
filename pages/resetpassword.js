9f8e0b98508225205d6b80dbebd97baabc7ed927
Christoph Stoll <christoph1.stoll@gmail.com>
2020-03-31 10:21:12 +0200

    add settings page

---
+++ pages/resetpassword.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../src/components/layouts/layout";
+import { Typography } from "@material-ui/core";
+
+const ResetPassword = () => (
+  <div>
+    <Layout title="Reset Password">
+      <Typography align="center" variant="h5" color="primary" component="h2">
+        This page is coming soon!
+      </Typography>
+    </Layout>
+  </div>
+);
+
+export default ResetPassword;
