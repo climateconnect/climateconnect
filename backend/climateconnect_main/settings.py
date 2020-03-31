e22a6daf13de5330fd5b1beb7a8768429aac26b9
Christoph Stoll <christoph1.stoll@gmail.com>
2020-03-31 08:35:05 +0200

    make frontend use /login API route (#143)
    * make frontend use /login API route
    
    * display errorMessage on failed login
    
    * change error code and msg of failed auth
    
    * use cors_origin_whitelist instead of allow all

--- backend/climateconnect_main/settings.py
+++ backend/climateconnect_main/settings.py
@@ -44,7 +44,8 @@ LIBRARY_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'knox'
+    'knox',
+    'corsheaders'
 ]
 
 INSTALLED_APPS = CUSTOM_APPS + LIBRARY_APPS
@@ -52,12 +53,18 @@ INSTALLED_APPS = CUSTOM_APPS + LIBRARY_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
+APPEND_SLASH = False
 
 ROOT_URLCONF = 'climateconnect_main.urls'
 
