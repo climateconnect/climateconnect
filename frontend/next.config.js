9c73e0026d5dea41895d7c22788cb1db4ccbdf80
Evan Hahn <me@evanhahn.com>
2020-05-12 13:26:47 -0500

    Fix frontend environment variables for Google Cloud (#186)
    Google Cloud has us define environment variables in `app.yaml`. However,
    these environment variables are not available at build time, only at
    runtime.
    
    To get around this, we read the variables from `app.yaml` during the
    build.
    
    This is a bit of a hack but I hope it's straightforward. If we change
    our production build setup, we should revisit this solution.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,31 @@
-require("dotenv").config();
-
+const pick = require("lodash/pick");
 const withCSS = require("@zeit/next-css");
+
+// Google Cloud has us define environment variables in app.yaml. However, these
+// environment variables are not available at build time, only at runtime. To
+// get around this, we read the variables from app.yaml here.
+//
+// This is a bit of a hack but is fairly straightforward. If we change our
+// production build setup, we should revisit this code.
+let environmentVariableSource;
+if (process.env.IS_BUILDING_ON_GOOGLE_CLOUD) {
+  const yaml = require("js-yaml");
+  const fs = require("fs");
+  const path = require("path");
+
+  const appDotYamlPath = path.join(__dirname, "app.yaml");
+  const appDotYamlContents = fs.readFileSync(appDotYamlPath, "utf8");
+  const appDotYaml = yaml.safeLoad(appDotYamlContents);
+
+  environmentVariableSource = appDotYaml.env_variables;
+} else {
+  require("dotenv").config();
+  environmentVariableSource = process.env;
+}
+
 module.exports = withCSS({
   cssModules: true,
-  env: {
-    PRE_LAUNCH: process.env.PRE_LAUNCH,
-    API_URL: process.env.API_URL
-  },
+  env: pick(environmentVariableSource, ["PRE_LAUNCH", "API_URL"]),
   exportPathMap: async function(defaultPathMap) {
     if (process.env.PRE_LAUNCH)
       return {
