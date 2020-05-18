3059968bb3c77ea45a30f65a742fcffb86c20db3
Christoph Stoll <christoph1.stoll@gmail.com>
2020-05-18 21:45:14 +0200

    remove import at wrong spot

--- frontend/pages/carousel.js
+++ frontend/pages/carousel.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 
 export default function CarouselExample(){
   const responsive = {
