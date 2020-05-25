1c6aaf1e60eafd54683cc9b5d4bdd8cc0994a8a7
Dip <ddhanesha@fastmail.com>
2020-05-25 15:44:51 -0700

    Added a new permission class to allow certain user full administrator access.

---
+++ backend/organization/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from organization.models import (OrganizationMember, ProjectMember)
+from climateconnect_api.models import Role
+
+
+class OrganizationProjectCreationPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
+        if OrganizationMember.objects.filter(
+                user=request.user, role__role_type__in=[Role.ALL_TYPE, Role.READ_WRITE_TYPE]
+        ).exists() or ProjectMember.objects.filter(
+            user=request.user, role__role_type__in=[Role.ALL_TYPE, Role.READ_WRITE_TYPE]
+        ).exists():
+            return True
+
+        return False
