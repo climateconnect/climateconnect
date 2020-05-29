f735adf5ff282a75e4288df7732d638ca8ba28bd
Christoph Stoll <christoph1.stoll@gmail.com>
2020-05-29 14:59:12 +0200

    stop using next-redirect module

--- frontend/pages/zoom.js
+++ frontend/pages/zoom.js
@@ -1,8 +1,10 @@
-import redirect from "next-redirect"
+import React, { useEffect } from "react"
+
 export default function Zoom() {
-
-}
-
-Zoom.getInitialProps = (ctx) =>{
-  return redirect(ctx, "https://fau.zoom.us/j/95027152733")
+  useEffect(() => {
+    window.location = "https://fau.zoom.us/j/95027152733"
+  });
+  return (
+    <div>You are being redirected to the Climate Connect zoom...</div>
+  )
 }
