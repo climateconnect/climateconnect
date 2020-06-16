7e10039b0ebb0ccae03c788be94a96f9ac94ea48
Christoph Stoll <christoph1.stoll@gmail.com>
2020-06-16 11:47:14 +0200

    remove unnecesary module from settings.py

--- backend/climateconnect_main/settings.py
+++ backend/climateconnect_main/settings.py
@@ -14,8 +14,6 @@ import os
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv('.backend_env'))
 env = os.environ.get
-import logging
-logger = logging.getLogger(__name__)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
