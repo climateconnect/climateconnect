24514b49ad0160ae0a5fd508c21fe37d6bf057f0
Christoph Stoll <christoph1.stoll@gmail.com>
2020-06-18 14:00:37 +0200

    change page size to 12

--- backend/organization/pagination.py
+++ backend/organization/pagination.py
@@ -1,11 +1,11 @@
 from rest_framework.pagination import PageNumberPagination
 
 class ProjectsPagination(PageNumberPagination):
-  page_size = 10
+  page_size = 12
   page_size_query_param = 'page_size'
   max_page_size = 20
 
 class MembersPagination(PageNumberPagination):
-  page_size = 10
+  page_size = 12
   page_size_query_param = 'page_size'
   max_page_size = 20
