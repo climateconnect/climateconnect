4c1b8c511c021c101b780ec978c410768862713a
Dip <ddhanesha@fastmail.com>
2020-06-30 21:53:26 -0700

    Added a new permission for user to access the API.

--- backend/climateconnect_api/permissions.py
+++ backend/climateconnect_api/permissions.py
@@ -4,7 +4,7 @@ from climateconnect_api.models import UserProfile
 
 class UserPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_autenticated() and UserProfile.objects.filter(user=request.user).exists():
+        if request.user.is_authenticated and UserProfile.objects.filter(user=request.user).exists():
             return True
         else:
             return False
