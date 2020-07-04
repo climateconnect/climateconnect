a19ec0fefd28daede6feb5ebaee95d7f4868db6b
Christoph Stoll <christoph1.stoll@gmail.com>
2020-07-05 00:43:25 +0200

    show different image url based on environment

--- frontend/public/lib/imageOperations.js
+++ frontend/public/lib/imageOperations.js
@@ -1,5 +1,11 @@
+const DEVELOPMENT = ["development", "develop", "test"].includes(process.env.ENVIRONMENT)
+
 export function getImageUrl(url){
   if(!url)
     return
-  return process.env.API_URL+url
+  if(DEVELOPMENT){
+    console.log(process.env.API_URL+url)
+    return process.env.API_URL+url
+  }else
+    return url
 }
