0e85a64828c3483f0ab8f2a47c595947f3cd9c63
Susan Chesnut <scc0019@tigermail.auburn.edu>
2020-08-08 11:29:12 -0500

    added desc to sign up page, matching design of share proj

--- frontend/src/components/signup/BasicInfo.js
+++ frontend/src/components/signup/BasicInfo.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Layout from "./../layouts/layout";
 import Form from "./../general/Form";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  appealText: {
+    textAlign: "center",
+    fontWeight: "bold"
+  }
+}));
 
 export default function BasicInfo({ handleSubmit, errorMessage, values }) {
+  const classes = useStyles();
   const fields = [
     {
       required: true,
@@ -40,6 +50,12 @@ export default function BasicInfo({ handleSubmit, errorMessage, values }) {
 
   return (
     <Layout title="Sign Up">
+      <Typography color="secondary" className={classes.appealText}>
+        Here you can create your personal account.
+      </Typography>
+      <Typography color="secondary" className={classes.appealText}>
+        You will have an opportunity to create/add an organization once signed up.
+      </Typography>
       <Form
         fields={fields}
         messages={messages}
