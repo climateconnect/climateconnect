5d20c052ea21754e1540fa30826803773c2c04e9
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-09-26 18:12:51 +0200

    Allow websockets to authenticate with knox

--- backend/climateconnect_main/routing.py
+++ backend/climateconnect_main/routing.py
@@ -2,9 +2,10 @@ from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat_messages.consumer import DirectMessageConsumer
+from climateconnect_main.token_auth import TokenAuthMiddlewareStack
 
 application = ProtocolTypeRouter({
-    "websocket": AuthMiddlewareStack(URLRouter([
+    "websocket": TokenAuthMiddlewareStack(URLRouter([
         path("ws/chat/", DirectMessageConsumer, name='chat-messaging')
     ]))
 })
