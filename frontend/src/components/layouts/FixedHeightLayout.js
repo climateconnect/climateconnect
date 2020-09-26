3a9cedc483324a4322f906e803c975305ce01710
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-09-26 18:17:34 +0200

    wrap LayoutWrapper around FixedHeightLayout

--- frontend/src/components/layouts/FixedHeightLayout.js
+++ frontend/src/components/layouts/FixedHeightLayout.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Head from "next/head";
 import Header from "../general/Header";
 import Footer from "../general/Footer";
-import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import theme from "../../themes/theme";
+import LayoutWrapper from "./LayoutWrapper";
 
 const useStyles = makeStyles({
   root: {
     margin: 0,
-    height: "100vh",
+    height: "calc(100vh)",
     minHeight: "100vh",
     display: "flex",
     flexDirection: "column"
@@ -22,17 +22,12 @@ export default function FixedHeightLayout({ title, children }) {
   const classes = useStyles();
 
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <ThemeProvider theme={theme}>
-        <div className={classes.root}>
-          <Header noSpacingBottom className={classes.noFlex} />
-          {children}
-          <Footer noSpacingTop noAbsolutePosition className={classes.noFlex} />
-        </div>
-      </ThemeProvider>
-    </>
+    <LayoutWrapper theme={theme} title={title} fixedHeight>
+      <div className={classes.root}>
+        <Header noSpacingBottom className={classes.noFlex} />
+        {children}
+        <Footer noSpacingTop noAbsolutePosition className={classes.noFlex} />
+      </div>
+    </LayoutWrapper>
   );
 }
