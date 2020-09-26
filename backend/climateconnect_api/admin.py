7c34760cd443e03c486525e64b342483d98ade63
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-09-26 18:13:02 +0200

    add new tables to admin panel

--- backend/climateconnect_api/admin.py
+++ backend/climateconnect_api/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from climateconnect_api.models import (UserProfile, Availability, Skill, Role)
 from climateconnect_api.models.common import Feedback
 from climateconnect_api.models.faq import (FaqSection, FaqQuestion)
+from chat_messages.models.message import (MessageParticipants, Message)
 
-pass_through_models = (UserProfile, Availability, Role, Feedback, FaqSection, FaqQuestion)
+pass_through_models = (UserProfile, Availability, Role, Feedback, FaqSection, FaqQuestion, MessageParticipants, Message)
 
 for model in pass_through_models:
     admin.site.register(model, admin.ModelAdmin)
