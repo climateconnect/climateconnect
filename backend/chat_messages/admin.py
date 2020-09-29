7cc36036dddc2afc0d41bae684881be10574d83f
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-09-29 18:36:57 +0200

    move appropriate classes in admin.py to chat_messaging

--- backend/chat_messages/admin.py
+++ backend/chat_messages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from chat_messages.models.message import (MessageParticipants, Message)
 
-# Register your models here.
+pass_through_models = (MessageParticipants, Message)
+
+for model in pass_through_models:
+    admin.site.register(model, admin.ModelAdmin)
--- backend/climateconnect_api/admin.py
+++ backend/climateconnect_api/admin.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 from climateconnect_api.models import (UserProfile, Availability, Skill, Role)
 from climateconnect_api.models.common import Feedback
 from climateconnect_api.models.faq import (FaqSection, FaqQuestion)
-from chat_messages.models.message import (MessageParticipants, Message)
+from climateconnect_api.models.notification import (Notification, UserNotification)
 
-pass_through_models = (UserProfile, Availability, Role, Feedback, FaqSection, FaqQuestion, MessageParticipants, Message)
+pass_through_models = (
+    UserProfile, Availability, Role, 
+    Feedback, FaqSection, FaqQuestion,
+    Notification, UserNotification
+)
 
 for model in pass_through_models:
     admin.site.register(model, admin.ModelAdmin)
