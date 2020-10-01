087a1fde73243c2da8be85c883b52281ccbc6762
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-10-01 22:14:23 +0200

    update chat messages backend to work with group chats

--- backend/chat_messages/serializers/message.py
+++ backend/chat_messages/serializers/message.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
-from chat_messages.models import Message, MessageParticipants
+from chat_messages.models import Message, MessageParticipants, MessageReceiver
 from climateconnect_api.models import UserProfile
 from climateconnect_api.serializers.user import UserProfileStubSerializer
-
+from django.utils import timezone
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = serializers.SerializerMethodField()
+    read_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
@@ -15,33 +16,52 @@ class MessageSerializer(serializers.ModelSerializer):
         user_profile = UserProfile.objects.filter(user=obj.sender)[0]
         return UserProfileStubSerializer(user_profile).data
 
+    def get_read_at(self, obj):
+        user = self.context.get('request', None).user
+        if user == obj.sender:
+            return timezone.now()
+        else:
+            receiver_object = MessageReceiver.objects.filter(receiver=user, message=obj)
+            if receiver_object.exists():
+                return receiver_object[0].read_at
+
 
 class MessageParticipantSerializer(serializers.ModelSerializer):
     last_message = serializers.SerializerMethodField()
     unread_count = serializers.SerializerMethodField()
     participants = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
 
     class Meta:
         model = MessageParticipants
         # Could use user profile serializer for participant_one and participant_two
         fields = (
-            'chat_uuid', 'participants', 'is_active', 'last_message', 'unread_count'
+            'chat_uuid', 'participants', 'is_active', 'last_message', 
+            'unread_count', 'user', 'created_at', 'name'
         )
 
     def get_last_message(self, obj):
         last_message = Message.objects.filter(message_participant=obj).first()
-
-        return {
-            'content': last_message.content,
-            'sent_at': last_message.sent_at
-        }
+        if last_message:
+            return {
+                'content': last_message.content,
+                'sent_at': last_message.sent_at
+            }
+        else:
+            return None
 
     def get_participants(self, obj):
         user_profiles = UserProfile.objects.filter(user__in=obj.participants.all())
         return UserProfileStubSerializer(user_profiles, many=True).data
 
     def get_unread_count(self, obj):
-        user = self.context.get('user', None)
-        unread_messages_unfiltered = Message.objects.filter(message_participant=obj, read_at=None)
-        unread_messages = Message.objects.filter(message_participant=obj, read_at=None).exclude(sender=user).count()
+        print(self.context)
+        user = self.context.get('request', None).user
+        unread_receivers = MessageReceiver.objects.filter(receiver=user, read_at=None).values('message')
+        unread_messages = Message.objects.filter(id__in=[(obj['message']) for obj in unread_receivers], message_participant=obj).count()
         return unread_messages
+    
+    def get_user(self, obj):
+        user = self.context.get('request', None).user
+        user_profile = UserProfile.objects.filter(user=user)[0]
+        return UserProfileStubSerializer(user_profile).data
--- backend/chat_messages/utility/chat_setup.py
+++ backend/chat_messages/utility/chat_setup.py
@@ -1,21 +1,23 @@
 from datetime import datetime
 from climateconnect_api.models.notification import UserNotification, Notification
+from chat_messages.models.message import MessageReceiver
 
 def set_read(messages, user, is_private_message):
-    unread_messages = messages.filter(read_at=None)
-    for message in unread_messages:
-        message.read_at = datetime.now()
-        message.save()
+    if not messages:
+        return None
+    unread_receivers = MessageReceiver.objects.filter(message__in=messages, receiver=user, read_at=None)
+    for receiver in unread_receivers:
+        receiver.read_at = datetime.now()
+        receiver.save()
     # Here we are assuming that all messages passed to this function are from the same chat
-    if is_private_message:
-        message_notifications = Notification.objects.filter(chat=messages[0].message_participant)
-        if message_notifications.exists():
-            unread_user_notifications = UserNotification.objects.filter(
-                notification__in=message_notifications,
-                read_at=None,
-                user=user
-            )
-            if unread_user_notifications.exists():
-                user_notification = unread_user_notifications[0]
-                user_notification.read_at = datetime.now()
-                user_notification.save()
+    message_notifications = Notification.objects.filter(chat=messages[0].message_participant)
+    if message_notifications.exists():
+        unread_user_notifications = UserNotification.objects.filter(
+            notification__in=message_notifications,
+            read_at=None,
+            user=user
+        )
+        if unread_user_notifications.exists():
+            user_notification = unread_user_notifications[0]
+            user_notification.read_at = datetime.now()
+            user_notification.save()
