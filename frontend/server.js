6f7aa1675355489edca987a5a334e4aac7d39dc5
Christoph Stoll <christoph1.stoll@gmail.com>
2020-10-05 12:59:57 +0200

    Create server.js

---
+++ frontend/server.js
@@ -0,0 +1,28 @@
+// server.js
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  }).listen(8080, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:8080')
+  })
+})
