6d03b5f1070743298b1914a211ff4b6f01d1d73c
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-10-19 10:51:31 +0200

    Add API_HOST variable

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,27 +1,9 @@
 const pick = require("lodash/pick");
 
-if (!process.env.IS_BUILDING_ON_AZURE) {
+if (!process.env.IS_BUILDING_ON_AZURE)
   require("dotenv").config();
-}
-
-const fallback_values = {
-  API_URL: "https://api.cc-test-domain.com",
-  SOCKET_URL: "wss://climateconnect-backend.azurewebsites.net",
-  ENVIRONMENT: "production"
-}
-
-
-const environmentVariableSource = process.env;
-console.log("env before")
-console.log(pick(environmentVariableSource, ["PRE_LAUNCH", "API_URL", "ENVIRONMENT", "SOCKET_URL"]))
-for(const key of Object.keys(fallback_values)) {
-  if(fallback_values[key] === undefined)
-    environmentVariableSource[key] = fallback_values[key]
-}
-console.log("env after")
-console.log(pick(environmentVariableSource, ["PRE_LAUNCH", "API_URL", "ENVIRONMENT", "SOCKET_URL"]))
 module.exports = {
-  env: pick(environmentVariableSource, ["PRE_LAUNCH", "API_URL", "ENVIRONMENT", "SOCKET_URL"]),
+  env: pick(process.env, ["PRE_LAUNCH", "API_URL", "ENVIRONMENT", "SOCKET_URL", "API_HOST"]),
   exportPathMap: async function(defaultPathMap) {
     if (process.env.PRE_LAUNCH)
       return {
