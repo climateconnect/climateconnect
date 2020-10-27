9d8c0c5783872dbbac552055b1d97322e6570288
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-10-27 19:29:08 +0100

    move viewport to app.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -11,10 +11,6 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
           <meta name="theme-color" content={theme.palette.primary.main} />
         </Head>
         <body>
