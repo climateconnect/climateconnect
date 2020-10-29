b3ae3316df4cff3b84ab418b9d65a57c4d3e5767
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-10-29 11:28:29 +0100

    add back all alternatingText options

--- frontend/src/components/general/AlternatingText.js
+++ frontend/src/components/general/AlternatingText.js
@@ -2,13 +2,28 @@ import React from "react";
 import TextLoop from "react-text-loop";
 import { Typography } from "@material-ui/core";
 
-export default function AlternatingText({ classes }) {
+export default function AlternatingText({ classes, mobile }) {
   if (!classes) classes = {};
   return (
-    <TextLoop mask={true} interval={5000}>
+    <TextLoop mask={true} interval={4000}>
       <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
-        Get inspired by
-      </Typography>
+          Share
+        </Typography>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          Find
+        </Typography>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          Work on
+        </Typography>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          Get inspired by
+        </Typography>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          Replicate
+        </Typography>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          {mobile ? "Join" : "Collaborate with"}
+        </Typography>
     </TextLoop>
   );
 }
