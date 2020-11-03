db429157a1d704da6bf944ec1fc76e9f1b2164a6
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-11-03 10:58:35 +0100

    redirect zoom page to new link

--- frontend/pages/zoom.js
+++ frontend/pages/zoom.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 export default function Zoom() {
   useEffect(() => {
-    window.location = "https://fau.zoom.us/j/95027152733";
+    window.location = "https://fau.zoom.us/j/91414261815";
   });
   return <div>You are being redirected to the Climate Connect zoom...</div>;
 }
