c902c05a84e05b36ec1a8c5cbb88a9911abd9cea
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-12-18 12:33:32 +0100

    Add source to StatBox

--- frontend/src/components/hub/StatBox.js
+++ frontend/src/components/hub/StatBox.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Typography, makeStyles } from "@material-ui/core";
+import { Typography, makeStyles, Link } from "@material-ui/core";
 import Stat from "./Stat";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minWidth: 310,
+    width: 310,
     background: "#EBEBEB",
     padding: theme.spacing(2),
   },
@@ -13,6 +13,14 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 600,
     fontSize: 21,
     marginBottom: theme.spacing(1),
+    textAlign: "center",
+  },
+  link: {
+    cursor: "pointer",
+  },
+  source: {
+    fontSize: 14,
+    textAlign: "center",
   },
 }));
 
@@ -21,11 +29,17 @@ export default function StatBox({ name, stats }) {
   return (
     <div className={classes.root}>
       <Typography component="h2" className={classes.h2}>
-        {name} accounts for
+        {name} is responsible for
       </Typography>
       {stats.map((s) => (
         <Stat key={s.name} statData={s} />
       ))}
+      <Typography className={classes.source}>
+        Source:{" "}
+        <Link className={classes.link} target="_blank" href={stats[0].source_link}>
+          {stats[0].source_name}
+        </Link>
+      </Typography>
     </div>
   );
 }
