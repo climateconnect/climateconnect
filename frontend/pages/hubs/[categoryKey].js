e1c14d03d2bf95143510684de53fcd7eec736703
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-12-18 12:25:52 +0100

    load correct projects and orgs into hub

--- frontend/pages/hubs/[categoryKey].js
+++ frontend/pages/hubs/[categoryKey].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import NextCookies from "next-cookies";
 import WideLayout from "../../src/components/layouts/WideLayout";
@@ -12,75 +12,215 @@ import {
   getOrganizationTagsOptions,
   getSkillsOptions,
   getStatusOptions,
+  membersWithAdditionalInfo,
 } from "../../public/lib/getOptions";
 import BrowseContent from "../../src/components/browse/BrowseContent";
+import Cookies from "universal-cookie";
+import FoodDescription from "../../src/components/hub/description/FoodDescription";
+import BrowseExplainer from "../../src/components/hub/BrowseExplainer";
+import { makeStyles } from "@material-ui/core";
 
-export default function Hub({ 
-  name, 
-  headline, 
-  image, 
-  quickInfo, 
-  detailledInfo, 
+const useStyles = makeStyles((theme) => ({
+  contentRefContainer: {
+    paddingTop: theme.spacing(4),
+    position: "relative",
+  },
+  contentUnderHeader: {
+    marginTop: 112,
+  },
+  contentRef: {
+    position: "absolute",
+    top: -90,
+  },
+}));
+
+export default function Hub({
+  categoryKey,
+  name,
+  headline,
+  image,
+  quickInfo,
   stats,
   initialProjects,
   initialOrganizations,
-  initialMembers,
-  filterChoices
+  filterChoices,
+  subHeadline,
+  segwayText,
 }) {
+  const classes = useStyles();
+  const token = new Cookies().get("token");
+  const [filters, setFilters] = useState({
+    projects: {},
+    members: {},
+    organizations: {},
+  });
+  const contentRef = useRef(null);
+  const scrollToSolutions = () => contentRef.current.scrollIntoView({ behavior: "smooth" });
+
+  const customSearchBarLabels = {
+    projects: "Search for climate solutions in the food sector",
+    organizations: "Search for climate organizations in the food sector",
+  };
+
+  const loadMoreData = async (type, page, urlEnding) => {
+    try {
+      const newDataObject = await getDataFromServer({
+        type: type,
+        page: page,
+        token: token,
+        urlEnding: urlEnding,
+        categoryKey: categoryKey,
+      });
+      const newData =
+        type === "members" ? membersWithAdditionalInfo(newDataObject.members) : newDataObject[type];
+
+      return {
+        hasMore: newDataObject.hasMore,
+        newData: newData,
+      };
+    } catch (e) {
+      console.log("error");
+      console.log(e);
+      throw e;
+    }
+  };
+
+  const applyNewFilters = async (type, newFilters, closeFilters, oldUrlEnding) => {
+    if (filters === newFilters) {
+      return;
+    }
+    setFilters({ ...filters, [type]: newFilters });
+    const newUrlEnding = buildUrlEndingFromFilters(newFilters);
+    if (oldUrlEnding === newUrlEnding) {
+      return null;
+    }
+    try {
+      const filteredItemsObject = await getDataFromServer({
+        type: type,
+        page: 1,
+        token: token,
+        urlEnding: newUrlEnding,
+        categoryKey: categoryKey,
+      });
+      if (type === "members") {
+        filteredItemsObject.members = membersWithAdditionalInfo(filteredItemsObject.members);
+      }
+      console.log(filteredItemsObject);
+      return {
+        closeFilters: closeFilters,
+        filteredItemsObject: filteredItemsObject,
+        newUrlEnding: newUrlEnding,
+      };
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const applySearch = async (type, searchValue, oldUrlEnding) => {
+    const newSearchQueryParam = `&search=${searchValue}`;
+    console.log(newSearchQueryParam);
+    if (oldUrlEnding === newSearchQueryParam) {
+      console.log("it's the same!");
+      return;
+    }
+    try {
+      const filteredItemsObject = await getDataFromServer({
+        type: type,
+        page: 1,
+        token: token,
+        urlEnding: newSearchQueryParam,
+        categoryKey: categoryKey,
+      });
+      if (type === "members") {
+        filteredItemsObject.members = membersWithAdditionalInfo(filteredItemsObject.members);
+      }
+      return {
+        filteredItemsObject: filteredItemsObject,
+        newUrlEnding: newSearchQueryParam,
+      };
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <WideLayout header={headline}>
-      <NavigationSubHeader hubName={name} />
-      <HubHeaderImage image={image} />
-      <HubContent
-        headline={headline}
-        quickInfo={quickInfo}
-        detailledInfo={detailledInfo}
-        name={name}
-        stats={stats}
-      />
-      <BrowseContent
-        initialProjects={initialProjects}
-        initialOrganizations={initialOrganizations}
-        initialMembers={initialMembers}
-        filterChoices={filterChoices}
-      />
+    <WideLayout header={headline} fixedHeader headerBackground="#FFF">
+      <div className={classes.contentUnderHeader}>
+        <NavigationSubHeader hubName={name} />
+        <HubHeaderImage image={image} />
+        <HubContent
+          headline={headline}
+          quickInfo={quickInfo}
+          name={name}
+          stats={stats}
+          scrollToSolutions={scrollToSolutions}
+          detailledInfo={<FoodDescription />}
+          subHeadline={subHeadline}
+          segwayText={segwayText}
+        />
+        <div className={classes.contentRefContainer}>
+          <div ref={contentRef} className={classes.contentRef} />
+          <BrowseExplainer />
+          <BrowseContent
+            initialProjects={initialProjects}
+            initialOrganizations={initialOrganizations}
+            filterChoices={filterChoices}
+            loadMoreData={loadMoreData}
+            applyNewFilters={applyNewFilters}
+            applySearch={applySearch}
+            hideMembers
+            customSearchBarLabels={customSearchBarLabels}
+          />
+        </div>
+      </div>
     </WideLayout>
   );
 }
 
+const buildUrlEndingFromFilters = (filters) => {
+  let url = "&";
+  Object.keys(filters).map((filterKey) => {
+    if (filters[filterKey] && filters[filterKey].length > 0) {
+      if (Array.isArray(filters[filterKey]))
+        url += encodeURI(filterKey + "=" + filters[filterKey].join()) + "&";
+      else url += encodeURI(filterKey + "=" + filters[filterKey] + "&");
+    }
+  });
+  return url;
+};
+
 Hub.getInitialProps = async (ctx) => {
   const categoryKey = ctx.query.categoryKey;
-  console.log(categoryKey)
+  console.log(categoryKey);
   const { token } = NextCookies(ctx);
   const [
     hubData,
     initialProjects,
     initialOrganizations,
-    initialMembers,
     project_categories,
     organization_types,
     skills,
     project_statuses,
   ] = await Promise.all([
     getHubData(categoryKey),
-    getProjects(1, token),
-    getOrganizations(1, token),
-    getMembers(1, token),
+    getProjects({ page: 1, token: token, categoryKey: categoryKey }),
+    getOrganizations({ page: 1, token: token, categoryKey: categoryKey }),
     getProjectTagsOptions(categoryKey),
-    getOrganizationTagsOptions(categoryKey),
-    getSkillsOptions(categoryKey),
-    getStatusOptions(categoryKey),
+    getOrganizationTagsOptions(),
+    getSkillsOptions(),
+    getStatusOptions(),
   ]);
   return {
-    url_slug: categoryKey,
+    categoryKey: categoryKey,
     name: hubData.name,
     headline: hubData.headline,
+    subHeadline: hubData.sub_headline,
+    segwayText: hubData.segway_text,
     image: hubData.image,
     quickInfo: hubData.quick_info,
     stats: hubData.stats,
     initialProjects: initialProjects,
     initialOrganizations: initialOrganizations,
-    initialMembers: initialMembers,
     filterChoices: {
       project_categories: project_categories,
       organization_types: organization_types,
@@ -103,35 +243,29 @@ const getHubData = async (url_slug) => {
   }
 };
 
-async function getProjects(page, token, urlEnding) {
+async function getProjects({ page, token, urlEnding, categoryKey }) {
   return await getDataFromServer({
     type: "projects",
     page: page,
     token: token,
     urlEnding: urlEnding,
+    categoryKey: categoryKey,
   });
 }
 
-async function getOrganizations(page, token, urlEnding) {
+async function getOrganizations({ page, token, urlEnding, categoryKey }) {
   return await getDataFromServer({
     type: "organizations",
     page: page,
     token: token,
     urlEnding: urlEnding,
+    categoryKey: categoryKey,
   });
 }
 
-async function getMembers(page, token, urlEnding) {
-  return await getDataFromServer({
-    type: "members",
-    page: page,
-    token: token,
-    urlEnding: urlEnding,
-  });
-}
-
-async function getDataFromServer({ type, page, token, urlEnding }) {
-  let url = `${process.env.API_URL}/api/${type}/?page=${page}`;
+async function getDataFromServer({ type, page, token, urlEnding, categoryKey }) {
+  let url = `${process.env.API_URL}/api/${type}/?page=${page}&project_category_parent=${categoryKey}`;
+  console.log(`getting ${type} data for category ${categoryKey}`);
   if (urlEnding) url += urlEnding;
 
   try {
