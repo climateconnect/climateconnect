8c1f876e73ad8c9fbd353235ed56f121fd931f5a
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-12-24 08:06:14 +0100

    minor changes

--- frontend/src/components/browse/NoItemsFound.js
+++ frontend/src/components/browse/NoItemsFound.js
@@ -10,12 +10,11 @@ const useStyles = makeStyles((theme) => {
   };
 });
 
-export default function NoItemsFound({ type }) {
+export default function NoItemsFound(/*{ type }*/) {
   const classes = useStyles();
   return (
     <Typography component="h4" variant="h5" className={classes.infoMessage}>
-      Error trying to retrieve {type} data from the server. <br />
-      Please contact contat@climateconnect.earth if this happens repeatedly.
+      No results.
     </Typography>
   );
 }
