236d49ba4cdb5c967f4341984f41f10d01163a09
Christoph Stoll <christoph.stoll@climateconnect.earth>
2020-12-24 08:04:31 +0100

    Allow adding subheader to layout

---
+++ frontend/src/components/indexPage/HubsSubHeader.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { makeStyles, Link, Container } from "@material-ui/core"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    background: theme.palette.primary.main,
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  link: {
+    color: "white",
+    display: "inline-block",
+    fontWeight: 600,
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    fontSize: 16
+  },
+  container: {
+    display: "flex",
+    justifyContent: "flex-end"
+  }
+}))
+
+export default function HubsSubHeader({hubs}) {
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <Container className={classes.container}>
+        <Link className={classes.link} key={"/hubs"} href={`/hubs/`}>All Hubs</Link>
+        {hubs && hubs.map(hub=>(
+          <Link className={classes.link} key={hub.url_slug} href={`/hubs/${hub.url_slug}`}>{hub.name}</Link>
+        ))}
+      </Container>
+    </div>
+  )
+}
