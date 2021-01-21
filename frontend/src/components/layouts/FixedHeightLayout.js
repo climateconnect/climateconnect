4ea01af2d26d69614a3c0f5817d57dbb2802668f
Christoph Stoll <christoph.stoll@climateconnect.earth>
2021-01-21 07:54:01 +0100

    import useState

--- frontend/src/components/layouts/FixedHeightLayout.js
+++ frontend/src/components/layouts/FixedHeightLayout.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 import Header from "../general/Header";
