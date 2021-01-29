77060c32c02779e7c8904cda9e3ebd71db2d372c
Piper <piperchester@gmail.com>
2021-01-29 15:05:23 -0800

    docs(*): comment

--- frontend/test/utils/index.test.js
+++ frontend/test/utils/index.test.js
@@ -11,8 +11,8 @@ describe.only("utils", () => {
     });
 
     // To test this, we don't want to calculate "time ago"
-    // from the default "Today" (Date.now()) -- we need to introduce
-    // a fixed reference time to perform the calcualtion.
+    // from the default "Today" (Date.now()) -- we use
+    // a fixed time to reference.
     it("should handle year and days correctly yaer", () => {
       const msSinceEpochFromFixedDate = new Date("2021-01-29T21:34:45.246Z").getTime();
       const msSinceEpochFromYearAgo = new Date("2020-01-10T22:17:38.387858Z").getTime();
