de79eb2a3bb7ea7850b27f72802cb6a6c3b1605e
Christoph Stoll <christoph.stoll@climateconnect.earth>
2021-04-06 07:09:43 +0200

    update livestream link

--- frontend/pages/stream.js
+++ frontend/pages/stream.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 export default function Stream() {
   useEffect(() => {
-    window.location = "https://youtu.be/fJEO-WV6w5w";
+    window.location = "https://youtu.be/lUbxcp-OWmo";
   });
   return <div>You are being redirected to the Climate Connect YouTube livestream...</div>;
 }
