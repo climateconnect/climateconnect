666c106f1cae3b63623dd28217ba79d2b9ca0869
Christoph Stoll <christoph.stoll@climateconnect.earth>
2021-04-07 16:26:07 +0200

    insert translations

--- frontend/public/texts/organization_texts.js
+++ frontend/public/texts/organization_texts.js
@@ -9,58 +9,59 @@ export default function getOrganizationTexts({ organization }) {
     },
     successfully_edited_organization: {
       en: "You have successfully edited your organization.",
-      de: "",
+      de: "Du hast deine Organisation erfolgreich bearbeitet.",
     },
     image_required_error: {
       en: 'Please add an avatar image by clicking the "Add Image" button.',
-      de: "",
+      de: 'Bitte füge ein Profilbild hinzu, indem du auf den Button "Bild hinzufügen" klickst.',
     },
     type_required_errror: {
       en:
         'Please choose at least one organization type by clicking the "Add Type" button under the avatar.',
-      de: "",
+      de: 'Bitte wähle mindestens einen Organisationstyp aus, indem du auf den Button "Typ hinzufügen" klickst.',
     },
     name_required_error: {
       en: "Please type your organization name under the avatar image",
-      de: "",
+      de: "Bitte gib deinen Organisationsnamen unter dem Profilbild der Organisation an",
     },
     location_required_error: {
       en: "Please specify your location",
-      de: "",
+      de: "Bitte gib einen Standort an",
     },
     how_to_describe_organization: {
       en:
         "Describe what your organization is doing, how you work and what impact you have on climate change. Please only use english!",
-      de: "",
+      de: "Beschreibe, was deine Organisation macht, wie ihr arbeitet und welchen Einfluss ihr auf den Klimawandeln habt.",
     },
     we_are_a_suborganization: {
       en: "We are a sub-organization of a larger organization (e.g. local group)",
-      de: "",
+      de: "Wir sind eine Sub-Organisation einer größeren Organisation (z.B. eine lokale Gruppe)",
     },
     parent_organization: {
       en: "Parent organization",
-      de: "",
+      de: "Übergeordnete Organisation",
     },
     edit_parent_organization_label: {
       en: "Edit your parent organization",
-      de: "",
+      de: "Bearbeite deine übergeordnete Organisation",
     },
     edit_parent_organization_helper_text: {
       en: "Type the name of your parent organization",
-      de: "",
+      de: "Gib den Namen der übergeordneten Organisation ein",
     },
     no_organization_found: {
       en: "No organizations found. Try changing or removing your filter or search query.",
-      de: "",
+      de: "Keine Organisation gefunden. Versuche, den Filter oder deine Suchanfrage zu ändern oder zu entfernen.",
     },
     to_manage_org_members: {
       en: "to manage the members of this organization",
-      de: "",
+      de: ", um die Mitglieder dieser Organisation zu verwalten",
     },
     you_are_not_a_member_of_this_organization: {
       en: "You are not a member of this organization.",
-      de: "",
+      de: "Du bist kein Mitglied dieser Organisation.",
     },
+    //Kommt drauf an wie "Join" übersetzt ist?
     go_to_org_page_and_click_join_to_join_it: {
       en: (
         <>
@@ -68,135 +69,137 @@ export default function getOrganizationTexts({ organization }) {
           and click join to join it.
         </>
       ),
-      de: <></>,
+      de: <>Gehe auf die <Link href={"/organizations/" + organization?.url_slug}>Organisationsseite</Link>{" "}
+      und klicke auf {"Join"} um der Organisation beizutreten.</>,
     },
     no_permission_to_manage_members_of_this_org: {
       en: "No Permission to Manage Members of this Organization",
-      de: "",
+      de: "Keine Berechtigung, um Mitglieder dieser Organisation zu verwalten",
     },
     need_to_be_admin_to_manage_org_members: {
       en: "You need to be an administrator of the organization to manage organization members.",
-      de: "",
+      de: "Du musst Admin dieser Organisation sein, um Mitglieder verwalten zu können.",
     },
     manage_organizations_members: {
       en: "Manage organization's Members",
-      de: "",
+      de: "Verwalte Mitglieder dieser Organisation",
     },
     you_have_to_log_in_to_manage_organization_members: {
       en: "You have to log in to manage an organization's members.",
-      de: "",
+      de: "Du musst dich anmelden, um Mitglieder dieser Organisation verwalten zu können.",
     },
     successfully_updated_org_members: {
       en: "You have successfully updated your organization members",
-      de: "",
+      de: "Du hast die Mitglieder deiner Organisation erfolgreich aktualisiert",
     },
     there_must_be_exactly_one_creator_of_organization: {
       en: "There must be exactly one creator of an organization.",
-      de: "",
+      de: "Es muss genau eine(n) Ersteller*in einer Organisation geben.",
     },
     manage_members_of_organization_name: {
       en: `Manage members of ${organization?.name}`,
-      de: ``,
+      de: `Verwalte Mitglieder von ${organization?.name}`,
     },
     search_for_your_organizations_members: {
       en: "Search for your organization's members",
-      de: "",
+      de: "Suche nach Mitglieder deiner Organisation",
     },
     type_name_of_next_team_member: {
       en: "Type the name of the team member you want to add next.",
-      de: "",
+      de: "Gib den Namen deines Team Mitglieds an, den du als nächstes hinzufügen möchtest",
     },
     role_in_organization: {
       en: "Role in organization",
-      de: "",
+      de: "Funktion innerhalb der Organisation",
     },
     edit_organization: {
       en: "Edit organization",
-      de: "",
+      de: "Organisation bearbeiten",
     },
     to_see_this_organizations_full_information: {
       en: "to see this organization's full information",
-      de: "",
+      de: ", um die vollständigen Informationen zu dieser Organisation zu sehen",
     },
     this_organizations_projects: {
       en: "This organization's Projects",
-      de: "",
+      de: "Projekte dieser Organisation",
     },
     this_organization_has_not_listed_any_projects_yet: {
       en: "This organization has not listed any projects yet!",
-      de: "",
+      de: "Diese Organisation hat noch keine Projekte geteilt!",
     },
     none_of_the_members_of_this_organization_has_signed_up_yet: {
       en: "None of the members of this organization has signed up yet!",
-      de: "",
+      de: "Keins der Mitglieder dieser Organisation hat sich bisher registriert!",
     },
     return_to_home: {
       en: "Return to home.",
-      de: "",
+      de: "Zurück zur Startseite",
     },
     not_found_lowercase: {
       en: "not found.",
-      de: "",
+      de: "nicht gefunden.",
     },
     organizations_logo: {
       en: `${organization?.name}'s logo`,
-      de: ``,
+      de: `${organization?.name}'s Logo`,
     },
     you_have_not_selected_a_parent_organization_either_untick: {
       en:
         "You have not selected a parent organization. Either untick the sub-organization field or choose/create your parent organization.",
-      de: "",
+      de: "Du hast keine übergeordnete Organisation ausgewählt. Deaktiviere entweder das Feld für die Sub-Organisation oder erstelle eine dazugehörige übergeordnete Organisation.",
     },
     an_organization_with_this_name_already_exists: {
       en: "An organization with this name already exists.",
-      de: "",
+      de: "Eine Organisation mit dem gleichen Namen existiert bereits.",
     },
     //Context: An organization with this name already exists. Click here to see it.
     to_see_it: {
       en: "to see it.",
-      de: "",
+      de: ", um mehr zu erfahren.",
     },
     you_have_successfully_created_an_organization_you_can_add_members: {
       en:
         "You have successfully created an organization! You can add members by scrolling down to the members section.",
-      de: "",
+      de: "Du hast erfolgreich eine Organisation erstellt! Du kannst nun Mitglieder hinzufügen, indem du nach unten zum Mitgliederbereich scrollst.",
     },
     to_create_an_organization: {
       en: "to create an organization",
-      de: "",
+      de: ", um eine Organisation zu erstellen",
     },
     create_an_organization: {
       en: "Create an Organization",
-      de: "",
+      de: "Erstelle eine Organisation",
     },
     organization_name: {
       en: "Organization name",
-      de: "",
+      de: "Organisationsname",
     },
     we_are_a_sub_organization_of_a_larger_organization: {
       en: "We are a sub-organization of a larger organization (e.g. local group)",
-      de: "",
+      de: "Wir sind eine Sub-Organisation einer größeren Organisation (z.B. eine lokale Gruppe)",
     },
     parent_organization_name: {
       en: "Parent organization name",
-      de: "",
+      de: "Name der übergeordneten Organisation",
     },
     search_for_your_parent_organization: {
       en: "Search for your parent organization",
-      de: "",
+      de: "Suche nach deiner übergeordneten Organisation",
     },
     type_the_name_of_your_parent_organization: {
       en: "Type the name of your parent organization.",
-      de: "",
+      de: "Gib den Namen der übergeordneten Organisation an",
     },
     i_verify_that_i_am_an_authorized_representative_of_this_organization: {
       en: `I verify that I am an authorized representative of this organization 
       and have the right to act on its behalf in the creation and management of this page.`,
-      de: ``,
+      de: `Ich bestätige, dass ich ein*e autorisierte*r Vertreter*in dieser Organisation 
+      bin und das Recht habe, in deren Namen bei der Erstellung und Verwaltung dieser Seite zu handeln.`,
     },
     almost_done_here_you_can_customize_your_organization_page_and_add_details: {
       en: "Almost done! Here you can customize your organization page and add details",
-      de: "",
+      de: "Fast geschafft! Hier kannst du deine Organisationsseite anpassen und Details hinzufügen",
     },
   };
 }
