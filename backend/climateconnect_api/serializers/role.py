f01b5d1ade3ad4abf7fcb275103d4eb1710b6cb5
Christoph Stoll <christoph.stoll@climateconnect.earth>
2021-04-22 14:27:56 +0200

    return role type to replace role name as unique identifier

--- backend/climateconnect_api/serializers/role.py
+++ backend/climateconnect_api/serializers/role.py
@@ -4,6 +4,10 @@ from climateconnect_api.models import Role
 
 
 class RoleSerializer(serializers.ModelSerializer):
+    role_type = serializers.SerializerMethodField()
     class Meta:
         model = Role
         exclude = ('created_at', 'updated_at')
+
+    def get_role_type(self, obj):
+        return Role.ROLE_TYPES[obj.role_type][1]
