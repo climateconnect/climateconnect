8eb66698136a29058439e6d64ad87c6074c29dfc
Christoph Stoll <christoph.stoll@climateconnect.earth>
2021-04-22 18:21:18 +0200

    Fix large footer and alternating text in german

--- frontend/src/components/general/AlternatingText.js
+++ frontend/src/components/general/AlternatingText.js
@@ -5,29 +5,46 @@ import getTexts from "../../../public/texts/texts";
 import UserContext from "../context/UserContext";
 
 export default function AlternatingText({ classes, mobile }) {
-  if (!classes) classes = {};
   const { locale } = useContext(UserContext);
-  const texts = getTexts({ page: "general", locale: locale });
+  const texts = getTexts({ page: "landing_page", locale: locale });
   return (
-    <TextLoop mask={true} interval={4000}>
-      <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
-        {texts.share_request}
-      </Typography>
-      <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
-        {texts.find}
-      </Typography>
-      <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
-        {texts.work_on}
-      </Typography>
-      <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
-        {texts.get_inspired_by}
-      </Typography>
-      <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
-        {texts.replicate}
-      </Typography>
-      <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
-        {mobile ? texts.join : texts.collaborate_with}
-      </Typography>
+    <TextLoop mask={true} interval={4000}>      
+      <div className={classes.titleTextFirstLine}>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          {texts.find}
+        </Typography>
+        {texts.climate_projects}        
+      </div>
+      <div className={classes.titleTextFirstLine}>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          {texts.work_on}
+        </Typography>
+        {texts.climate_projects_with} 
+      </div>
+      <div className={classes.titleTextFirstLine}>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          {texts.learn_from}
+        </Typography>
+        {texts.climate_projects_with} 
+      </div>   
+      <div className={classes.titleTextFirstLine}>       
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          {texts.replicate}
+        </Typography>
+        {texts.climate_projects}
+      </div>  
+      <div className={classes.titleTextFirstLine}>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          {texts.share_request}
+        </Typography>
+        {texts.climate_projects}        
+      </div>
+      <div className={classes.titleTextFirstLine}>
+        <Typography component="h1" variant="h5" color="primary" className={classes.titleText}>
+          {mobile ? texts.join : texts.collaborate_with}
+        </Typography>
+        {texts.climate_projects_with}
+      </div>
     </TextLoop>
   );
 }
