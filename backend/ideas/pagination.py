f7106acad5a70d590c2a770ae63c69d4eb0fb478
Christoph Stoll <christoph1.stoll@gmail.com>
2021-06-02 10:38:41 +0200

    fix idea pagination

--- backend/ideas/pagination.py
+++ backend/ideas/pagination.py
@@ -3,5 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class IdeasBoardPagination(PageNumberPagination):
     page_size = 12
-    page_query_param = "page_size"
     max_page_size = 20
