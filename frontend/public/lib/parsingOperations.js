6f2705d3d8b240835def2e04a86325cc8ea495c8
Christoph Stoll <christoph1.stoll@gmail.com>
2021-06-04 07:14:50 +0200

    make getInfoMetadataByType a bit more safe

--- frontend/public/lib/parsingOperations.js
+++ frontend/public/lib/parsingOperations.js
@@ -1,5 +1,4 @@
 import getProjectInfoMetadata from "../data/getProjectInfoMetadata";
-import { getLocationFilterKeys } from "../data/locationFilters";
 import getOrganizationInfoMetadata from "../data/organization_info_metadata";
 import profile_info_metadata from "../data/profile_info_metadata";
 
@@ -37,28 +36,6 @@ export function getMessageFromUrl(message) {
 
 export function getInfoMetadataByType(type, locale) {
   if (type === "organizations") return getOrganizationInfoMetadata(locale);
-  if (type === "profiles" || type === "members") return profile_info_metadata(locale);
+  if (type === "profiles") return profile_info_metadata(locale);
   if (type === "projects") return getProjectInfoMetadata();
-}
-
-export function getReducedPossibleFilters(possibleFilters, locationFilter) {
-  const reducedPossibleFilters = possibleFilters.reduce((map, obj) => {
-    const locationKeys = getLocationFilterKeys();
-    //don't add location keys to the reduced possible filters. They are represented by the locationFilter
-    if (locationKeys.includes(obj.key)) {
-      return map;
-    }
-    // Handle initializing to an array for multiselects, otherwise an empty string
-    if (obj.type === "multiselect" || obj.type === "openMultiSelectDialogButton") {
-      map[obj.key] = [];
-    } else {
-      map[obj.key] = "";
-    }
-
-    return map;
-  }, {});
-  if (locationFilter) {
-    reducedPossibleFilters.location = locationFilter;
-  }
-  return reducedPossibleFilters;
 }
