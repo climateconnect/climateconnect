b1d94873e4030f1215d9d9b3746ff67e33266fb5
Christoph Stoll <christoph1.stoll@gmail.com>
2021-06-04 07:08:43 +0200

    Allow getting the diff after a filter change and to get tabs that were unaffected by that change

--- frontend/public/lib/filterOperations.js
+++ frontend/public/lib/filterOperations.js
@@ -1,3 +1,7 @@
+import _ from "lodash";
+import { getLocationFilterKeys } from "../data/locationFilters";
+import possibleFilters from "../data/possibleFilters";
+
 const getLocationFilterUrl = (location) => {
   /*Pass place id. If the place id is found in our db we can use it's polygon,
   otherwise make a request to the location API with the backend */
@@ -20,4 +24,83 @@ export function buildUrlEndingFromFilters(filters) {
     }
   });
   return url;
+}
+
+export function getKeysOfDifferingValues({ obj, newObj, type, filterChoices, locale }) {
+  const possibleFilterKeys = possibleFilters({
+    key: type,
+    filterChoices: filterChoices,
+    locale: locale,
+  }).map((f) => f.key);
+  const locationKeys = getLocationFilterKeys();
+  console.log(locationKeys);
+  const differingKeys = [];
+  for (const key of possibleFilterKeys) {
+    if (key === "location") console.log(newObj[key]);
+    if (key === "location" && (!newObj[key] || typeof newObj[key] === "object")) {
+      let isLocationEqual = true;
+      //if there are no location keys in either object we still check whether the user selected a location
+      //If a user selects a location from the options it changes from a string to an obj
+      if (typeof obj?.location === "string" && typeof newObj?.location === "object") {
+        isLocationEqual = false;
+      } else {
+        //Otherwise we'll have to check for each key from the url whether it has changed.
+        for (const locKey of locationKeys) {
+          if (!_.isEqual(newObj[locKey], obj[locKey])) {
+            isLocationEqual = false;
+          }
+        }
+        //If no location was selected before and after the change but the radius changed:
+        //Pretend the location didn't change because a radius filter without a selected location does nothing
+        if (
+          _.isEqual(
+            locationKeys.filter((k) => newObj[k]?.length > 0 && obj[k]?.length > 0),
+            ["radius"]
+          )
+        ) {
+          console.log("actually it is equal!");
+          isLocationEqual = true;
+        }
+      }
+      if (!isLocationEqual) differingKeys.push(location);
+    } else if (!_.isEqual(newObj[key], obj[key])) {
+      differingKeys.push(key);
+    }
+  }
+  return differingKeys;
+}
+
+export function hasDifferingValues({ obj, newObj, type, filterChoices, locale }) {
+  return (
+    getKeysOfDifferingValues({
+      obj: obj,
+      newObj: newObj,
+      type: type,
+      filterChoices: filterChoices,
+      locale: locale,
+    }).length > 0
+  );
+}
+
+export function getUnaffectedTabs({ tabs, filterChoices, locale, filters, newFilters, type }) {
+  return tabs.filter((tab) => {
+    const possibleFiltersInTab = possibleFilters({
+      key: tab,
+      filterChoices: filterChoices,
+      locale: locale,
+    });
+    const keysOfDifferingValues = getKeysOfDifferingValues({
+      obj: filters,
+      newObj: newFilters,
+      type: type,
+      filterChoices: filterChoices,
+      locale: locale,
+    });
+    for (const filter of possibleFiltersInTab) {
+      if (keysOfDifferingValues.includes(filter.key)) {
+        return false;
+      }
+    }
+    return true;
+  });
 }
