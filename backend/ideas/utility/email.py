3c58ecd8652ccf6866bf1c002131cef87203e28c
Christoph Stoll <christoph1.stoll@gmail.com>
2021-07-12 19:29:57 +0200

    make clean generic send_email function with multilanguage support and use it everywhere

--- backend/ideas/utility/email.py
+++ backend/ideas/utility/email.py
@@ -1,22 +1,14 @@
 from climateconnect_api.models.user import UserProfile
 from climateconnect_api.utility.translation import get_user_lang_code, get_user_lang_url
-from climateconnect_api.utility.email_setup import get_template_id, send_email
+from climateconnect_api.utility.email_setup import send_email
 from django.conf import settings
 
-def send_idea_join_email(user, joining_user, idea, chat_uuid):    
-    try:
-        user_profile = UserProfile.objects.get(user=user)
-        # short circuit if the user has changed his settings to not receive emails on idea join
-        if user_profile.email_on_idea_join == False:
-            return
-    except UserProfile.DoesNotExist:
-        print("there is no user profile (send_idea_join_email")
+def send_idea_join_email(user, joining_user, idea, chat_uuid, notification):
     lang_code = get_user_lang_code(user)
     subjects_by_language = {
         "en": "{} joined your idea on Climate Connect".format(joining_user.first_name + " " + joining_user.last_name),
         "de": "{} macht jetzt bei deiner Idee auf Climate Connect mit!".format(joining_user.first_name + " " + joining_user.last_name)
     }
-    subject = subjects_by_language[lang_code]
     base_url = settings.FRONTEND_URL
     url_ending = "/chat/"+str(chat_uuid)
     variables = {
@@ -26,10 +18,11 @@ def send_idea_join_email(user, joining_user, idea, chat_uuid):
         "ChatUrl": base_url + get_user_lang_url(lang_code) + url_ending,
         "IdeaName": idea.name
     }
-    template_id = get_template_id(template_key="JOINED_IDEA_TEMPLATE", user=user, lang_code=lang_code)
     send_email(
         user=user, 
         variables=variables, 
-        template_id=template_id, 
-        subject=subject
+        template_key="JOINED_IDEA_TEMPLATE", 
+        subjects_by_language=subjects_by_language,
+        should_send_email_setting="email_on_idea_join",
+        notification=notification
     )
