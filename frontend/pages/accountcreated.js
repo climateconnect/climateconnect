227930940d0d060f4893d3178443893cd00f34b6
Piper <piperchester@users.noreply.github.com>
2021-08-24 09:14:36 -0700

    feat(*): minor polish of /accountcreated page (#686)
    * feat(*): minor polish of /accountcreated
    
    * fix(*): restore
    
    * chore(*): delete unused
    
    * chore(*): drop unused
    
    Co-authored-by: Christoph Stoll <christoph1.stoll@gmail.com>

--- frontend/pages/accountcreated.js
+++ frontend/pages/accountcreated.js
@@ -40,21 +40,22 @@ export default function AccountCreated() {
     return (
       <Layout title={texts.account_created} hideHeadline>
         <Paper className={classes.root}>
-          <Typography variant="h5" className={classes.headline}>
-            {texts.congratulations_just_one_more_step_to_complete_your_signup}
+          <Typography variant="h4" className={classes.headline}>
+            {texts.just_one_more_step_to_complete_your_signup}
           </Typography>
-          <Typography variant="h4">
-            <div>{texts.we_have_sent_you_an_email_with_a_link}</div>
-            <div>{texts.please_click_on_the_link_to_activate_your_account}</div>
+          <Typography variant="h5">
+            <div></div>
+            <div>{texts.please_click_on_the_link_we_emailed_you_to_activate_your_account}</div>
+            <br />
+            <Typography component="p" variant="h6">
+              {texts.if_the_email_does_not_arrive_after_5_minutes}
+            </Typography>
             <br />
             <Typography component="p" variant="h6">
               {texts.make_sure_to_also_check_your_spam}
             </Typography>
             <Typography component="p" variant="h6">
-              {texts.if_you_are_experiencing_any_problems_contact_us}
-            </Typography>
-            <Typography component="p" variant="h6">
-              {texts.if_the_email_does_not_arrive_after_5_minutes}
+              {texts.if_you_are_experiencing_any_problems_contact_us}.
             </Typography>
           </Typography>
         </Paper>
