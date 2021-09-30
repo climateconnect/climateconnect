4ae03d97f34b04ed803378116ca355c0fafbc0ea
Christoph Stoll <christoph1.stoll@gmail.com>
2021-09-30 17:22:07 +0200

    fix typos

--- frontend/public/texts/dashboard_texts.js
+++ frontend/public/texts/dashboard_texts.js
@@ -6,12 +6,12 @@ export default function getDashboardTexts({ user, location }) {
     },
     welcome_message_logged_in: {
       en: `Great to see you, ${user?.first_name}! Share your climate ideas and projects with the community from ${location?.city}!`,
-      de: `Schön, dass du da bist, ${user?.first_name}! Teile dein Klimaschutzprojekt mit Community aus ${location?.city}!`,
+      de: `Schön, dass du da bist, ${user?.first_name}! Teile dein Klimaschutzprojekt mit der Community aus ${location?.city}!`,
     },
     welcome_message_logged_out: {
       en: `Great that you're here! Here you can see what is being done for the climate in ${location?.city} and how you can get involved!`,
       de:
-        `Schön, dass du da bist! Hier siehst du, was in ${location?.city} das Klima getan wird und kannst mitmachen!`,
+        `Schön, dass du da bist! Hier siehst du, was in ${location?.city} für das Klima getan wird und kannst mitmachen!`,
     },
     create_idea: {
       en: "Create Idea",
