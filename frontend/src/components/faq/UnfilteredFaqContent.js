936db7fc26fa2881389fdd011c5ad1e82f858a8c
Piper <piperchester@gmail.com>
2021-10-09 18:53:18 -0700

    fix(*): key error; minor updates with readability

--- frontend/src/components/faq/UnfilteredFaqContent.js
+++ frontend/src/components/faq/UnfilteredFaqContent.js
@@ -30,17 +30,17 @@ export default function UnfilteredFaqContent({ questionsBySection }) {
   return (
     <>
       <Tabs
-        value={tabValue}
-        onChange={handleTabChange}
-        indicatorColor="primary"
-        textColor="primary"
         className={classes.tabs}
+        indicatorColor="primary"
+        onChange={handleTabChange}
+        textColor="primary"
+        value={tabValue}
       >
         {Object.keys(questionsBySection).map((key, index) => (
           <Tab
-            key={key + "tab"}
-            label={Object.keys(questionsBySection)[index].toUpperCase()}
             className={classes.tab}
+            key={`${key}-${index}-tab`}
+            label={Object.keys(questionsBySection)[index].toUpperCase()}
           />
         ))}
       </Tabs>
@@ -48,7 +48,7 @@ export default function UnfilteredFaqContent({ questionsBySection }) {
       {Object.keys(questionsBySection).map((key, index) => (
         <TabContent value={tabValue} index={index} key={key}>
           {questionsBySection[key].map((q) => (
-            <FaqQuestionElement key={key + "-" + q} questionObject={q} />
+            <FaqQuestionElement key={key + "-" + q.question} questionObject={q} />
           ))}
         </TabContent>
       ))}
