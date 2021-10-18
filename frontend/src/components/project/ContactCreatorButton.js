8ce479e975df076609c081c6315639234798effe
verena-e-p <verena.pohl@climateconnect.earth>
2021-10-18 16:27:40 +0200

    renamed component, implemented review from PR #717

---
+++ frontend/src/components/project/ContactCreatorButton.js
@@ -0,0 +1,118 @@
+import { Avatar, Button, Card, CardHeader, Collapse } from "@material-ui/core";
+import React, { useContext, useState } from "react";
+import SendIcon from "@material-ui/icons/Send";
+import UserContext from "../context/UserContext";
+import { getImageUrl } from "../../../public/lib/imageOperations";
+import getTexts from "../../../public/texts/texts";
+import { makeStyles } from "@material-ui/core/styles";
+import theme from "../../themes/theme";
+
+const useStyles = makeStyles({
+  root: {
+    position: "relative",
+  },
+  smallAvatar: {
+    height: theme.spacing(3),
+    width: theme.spacing(3),
+  },
+  slideInCard: {
+    width: 200,
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#F8F8F8",
+  },
+  slideInRoot: {
+    textAlign: "left",
+  },
+  slideInSubheader: {
+    color: "black",
+  },
+  slideInTitle: {
+    fontWeight: "bold",
+  },
+  contactButton: {
+    width: 200,
+    "&:hover": {
+      background: theme.palette.primary.main,
+    },
+  },
+});
+export default function ContactCreatorButton({
+  className,
+  projectAdmin,
+  contactProjectCreatorButtonRef,
+  handleClickContact,
+  smallScreen,
+  tinyScreen,
+}) {
+  const classes = useStyles();
+  const { locale } = useContext(UserContext);
+  const texts = getTexts({ page: "project", locale: locale });
+  const [hoveringButton, setHoveringButton] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHoveringButton(true);
+  };
+  const handleMouseLeave = () => {
+    setHoveringButton(false);
+  };
+
+  const creatorImageURL = getImageUrl(projectAdmin.thumbnail_image);
+  const creatorName = projectAdmin.name;
+  const creatorRole = projectAdmin.role;
+  const buttonText = texts.contact;
+
+  if (smallScreen) {
+    return (
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<SendIcon />}
+        endIcon={<Avatar src={creatorImageURL} className={classes.smallAvatar} />}
+        onClick={handleClickContact}
+      >
+        {buttonText}
+      </Button>
+    );
+  } else if (tinyScreen) {
+    return (
+      <Button variant="contained" color="primary" onClick={handleClickContact}>
+        {buttonText}
+      </Button>
+    );
+  } else {
+    return (
+      <div
+        className={`${classes.root} ${className}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={handleClickContact}
+      >
+        <Collapse in={hoveringButton} timeout={550}>
+          <Card className={classes.slideInCard} variant="outlined">
+            <CardHeader
+              classes={{
+                root: classes.slideInRoot,
+                subheader: classes.slideInSubheader,
+                title: classes.slideInTitle,
+              }}
+              avatar={<Avatar src={creatorImageURL} />}
+              title={creatorName}
+              subheader={creatorRole}
+            />
+          </Card>
+        </Collapse>
+        <Button
+          className={classes.contactButton}
+          variant="contained"
+          color="primary"
+          startIcon={<SendIcon />}
+          endIcon={<Avatar src={creatorImageURL} className={classes.smallAvatar} />}
+          ref={contactProjectCreatorButtonRef}
+        >
+          {buttonText}
+        </Button>
+      </div>
+    );
+  }
+}
