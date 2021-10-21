cb4a4d042da17f0b1fbbc72911372e24fdb9f130
Christoph Stoll <christoph1.stoll@gmail.com>
2021-10-21 14:42:08 +0200

    Split HubsSubHeader in multiple components and make it mobile friendly

---
+++ frontend/src/components/indexPage/hubsSubHeader/HubLinks.js
@@ -0,0 +1,36 @@
+import { Link, makeStyles, useMediaQuery } from "@material-ui/core";
+import React from "react";
+import { getLocalePrefix } from "../../../../public/lib/apiOperations";
+import theme from "../../../themes/theme";
+import HubsDropDown from "./HubsDropDown";
+
+const useStyles = makeStyles(() => ({
+  spaceAround: {
+    display: "flex",
+    justifyContent: "flex-end",
+    width: "100%"
+  }
+}))
+
+export default function HubLinks({ hubs, locale, linkClassName, isNarrowScreen }) {
+  const classes = useStyles()
+  const sectorHubs = hubs.filter((h) => h.hub_type === "sector hub");
+  const locationHubs = hubs.filter((h) => h.hub_type === "location hub");
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <div className={isNarrowScreen && classes.spaceAround}>
+      {!isMediumScreen &&
+        sectorHubs.slice(0, 3).map((hub) => (
+          <Link
+            className={linkClassName}
+            key={hub.url_slug}
+            href={`${getLocalePrefix(locale)}/hubs/${hub.url_slug}`}
+          >
+            {hub.name}
+          </Link>
+        ))}
+      {sectorHubs?.length > 3 && <HubsDropDown hubs={sectorHubs} label="SectorHubs" isNarrowScreen={isNarrowScreen} />}
+      {locationHubs?.length > 0 && <HubsDropDown hubs={locationHubs} label="CityHubs" isNarrowScreen={isNarrowScreen} />}
+    </div>
+  );
+}
---
+++ frontend/src/components/indexPage/hubsSubHeader/HubsDropDown.js
@@ -0,0 +1,63 @@
+import { Button, makeStyles } from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import React, { useRef, useState } from "react";
+import DropDownList from "./DropDownList";
+
+const useStyles = makeStyles(theme => ({
+  hubsDropDownButton: {
+    textTransform: "none",
+    color: "white",
+    fontSize: 16,
+    height: 54,
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+}))
+
+export default function HubsDropDown({ hubs, label, isNarrowScreen }){
+  const classes = useStyles();
+  const buttonRef = useRef(null);
+  const [open, setOpen] = useState(false);
+
+  const handleToggleOpen = (e) => {
+    e.preventDefault();
+    setOpen(!open)
+  }
+
+  const handleOpen = (e) => {
+    e.preventDefault();
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const toggleButtonProps = {}
+  if(!isNarrowScreen) {
+    toggleButtonProps.onMouseEnter = handleOpen
+    toggleButtonProps.onMouseLeave = handleClose
+  }
+
+  return (
+    <>
+      <Button
+        {...toggleButtonProps}
+        onClick={handleToggleOpen}
+        aria-haspopup="true"
+        ref={buttonRef}
+        className={classes.hubsDropDownButton}
+      >
+        {label}
+        <ArrowDropDownIcon />
+      </Button>
+      <DropDownList
+        buttonRef={buttonRef}
+        handleOpen={handleOpen}
+        hubs={hubs}
+        handleClose={handleClose}
+        open={open}
+      />
+    </>
+  );
+}
