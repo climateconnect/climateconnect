40275ec0e5271163a4fc9ea8b9ac5f40b38f7493
Christoph Stoll <christoph1.stoll@gmail.com>
2021-10-22 22:44:50 +0200

    remove unnecessary console.log

--- frontend/src/components/dashboard/UserImage.js
+++ frontend/src/components/dashboard/UserImage.js
@@ -3,7 +3,6 @@ import React from "react";
 import { getImageUrl } from "../../../public/lib/imageOperations";
 
 export default function UserImage({ user }) {
-  console.log(user);
   return (
     <Avatar
       alt={user?.first_name + " " + user?.last_name + "'s picture"}
