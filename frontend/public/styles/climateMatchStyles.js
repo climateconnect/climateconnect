a576fd8b7e5791a6b623fd336d74ee246204af16
Christoph Stoll <christoph1.stoll@gmail.com>
2021-10-27 23:57:04 +0200

    extract answer chip styles

---
+++ frontend/public/styles/climateMatchStyles.js
@@ -0,0 +1,17 @@
+const climateMatchStyles = (theme) => ({
+  possibleAnswerChip: {
+    background: theme.palette.primary.light,
+    color: "white",
+    fontWeight: 600,
+    fontSize: 18,
+    height: 40,
+    borderRadius: 20,
+    width: "100%",
+    cursor: "pointer",
+    "&:hover": {
+      background: "#89d9d2"
+    }
+  },
+});
+
+export default climateMatchStyles;
