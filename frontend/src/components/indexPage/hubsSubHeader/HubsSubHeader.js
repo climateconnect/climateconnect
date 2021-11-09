143b7c8f94c8a024269cd49809ef09ee9d9ac889
verena-e-p <verena.pohl@climateconnect.earth>
2021-11-09 14:10:35 +0100

    added GoBackButton to HubsSubHeader

--- frontend/src/components/indexPage/hubsSubHeader/HubsSubHeader.js
+++ frontend/src/components/indexPage/hubsSubHeader/HubsSubHeader.js
@@ -4,6 +4,7 @@ import { getLocalePrefix } from "../../../../public/lib/apiOperations";
 import getTexts from "../../../../public/texts/texts";
 import theme from "../../../themes/theme";
 import UserContext from "../../context/UserContext";
+import GoBackButton from "../../project/Buttons/GoBackButton";
 import HubLinks from "./HubLinks";
 
 const useStyles = makeStyles((theme) => ({
@@ -36,6 +37,10 @@ const useStyles = makeStyles((theme) => ({
   popoverContent: {
     pointerEvents: "auto",
   },
+  goBackButtonContainer: {
+    alignSelf: "center",
+    width: "100%",
+  },
 }));
 
 export default function HubsSubHeader({ hubs, subHeaderRef, onProjectPage }) {
@@ -46,6 +51,14 @@ export default function HubsSubHeader({ hubs, subHeaderRef, onProjectPage }) {
   return (
     <div className={classes.root} ref={subHeaderRef}>
       <Container className={classes.container}>
+        {!isNarrowScreen && (
+          <GoBackButton
+            containerClassName={classes.goBackButtonContainer}
+            texts={texts}
+            locale={locale}
+            tinyScreen={isNarrowScreen}
+          />
+        )}
         {!isNarrowScreen && !onProjectPage && (
           <Link className={classes.link} key={"/hubs"} href={`${getLocalePrefix(locale)}/hubs/`}>
             {texts.all_hubs}
