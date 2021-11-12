c68e6d87117a27ba62faf482e96dc48f1e85d013
verena-e-p <verena.pohl@climateconnect.earth>
2021-11-12 14:12:01 +0100

    added visual indications when clicking/hovering

--- frontend/src/components/project/DiscussionPreview.js
+++ frontend/src/components/project/DiscussionPreview.js
@@ -1,18 +1,25 @@
-import { Typography } from "@material-ui/core";
+import { ButtonBase, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
 import Posts from "../communication/Posts";
 
 const useStyles = makeStyles((theme) => ({
+  buttonBase: {
+    width: "100%",
+    textAlign: "start",
+    marginBottom: theme.spacing(4),
+    "&:hover": {
+      backgroundColor: "#f5f5f5",
+    },
+  },
   discussionPreview: {
     borderBottom: `1px solid ${theme.palette.grey[500]}`,
     borderTop: `1px solid ${theme.palette.grey[500]}`,
-    marginBottom: theme.spacing(4),
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(1),
-    "&:hover": {
-      cursor: "pointer",
-    },
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(2),
+    width: "100%",
   },
   topSectionDiscussionPreview: {
     display: "flex",
@@ -21,9 +28,6 @@ const useStyles = makeStyles((theme) => ({
   headingDiscussionPreview: {
     fontWeight: "bold",
     marginBottom: theme.spacing(0.5),
-  },
-  iconDiscussionPreview: {
-    marginRight: theme.spacing(2),
   },
 }));
 
@@ -40,19 +44,21 @@ export default function DiscussionPreview({
   }
 
   return (
-    <div className={classes.discussionPreview} onClick={switchToDiscussionTab}>
-      <div className={classes.topSectionDiscussionPreview}>
-        <Typography display="inline" color="primary" className={classes.headingDiscussionPreview}>
-          {discussionTabLabel}
-        </Typography>
-        <UnfoldMoreIcon color="secondary" className={classes.iconDiscussionPreview} />
+    <ButtonBase className={classes.buttonBase}>
+      <div className={classes.discussionPreview} onClick={switchToDiscussionTab}>
+        <div className={classes.topSectionDiscussionPreview}>
+          <Typography display="inline" color="primary" className={classes.headingDiscussionPreview}>
+            {discussionTabLabel}
+          </Typography>
+          <UnfoldMoreIcon color="secondary" />
+        </div>
+        <Posts
+          posts={latestParentComment}
+          type="preview"
+          user={latestParentComment.author_user}
+          truncate={3}
+        />
       </div>
-      <Posts
-        posts={latestParentComment}
-        type="preview"
-        user={latestParentComment.author_user}
-        truncate={3}
-      />
-    </div>
+    </ButtonBase>
   );
 }
