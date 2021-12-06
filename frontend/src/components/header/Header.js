41fcecdca1c1adaeb21c88543fde0be1018ade2a
Christoph Stoll <christoph1.stoll@gmail.com>
2021-12-06 19:35:47 +0100

    add team  page

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -17,7 +17,7 @@ import {
   Paper,
   Popper,
   SwipeableDrawer,
-  Typography,
+  Typography
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
@@ -268,6 +268,10 @@ const getStaticPageLinks = (texts) => [
   {
     href: "/faq",
     text: texts.faq,
+  },
+  {
+    href: "/team",
+    text: texts.team,
   },
   {
     href: "/join",
