58e40b8127f98180d6fac6fff95043b6db7936f1
verena-e-p <verena.pohl@climateconnect.earth>
2021-12-14 12:21:46 +0100

    use find for a single return value

--- frontend/pages/inbox.js
+++ frontend/pages/inbox.js
@@ -268,7 +268,7 @@ const parseChats = (chats, texts) =>
     ? chats.map((chat) => ({
         ...chat,
         chatting_partner:
-          chat.participants.length === 2 && chat.participants.filter((p) => p.id !== chat.user.id)[0],
+          chat.participants.length === 2 && chat.participants.find((p) => p.id !== chat.user.id),
         unread_count: chat.unread_count,
         content: chat.last_message ? chat.last_message.content : texts.chat_has_been_created,
       }))
