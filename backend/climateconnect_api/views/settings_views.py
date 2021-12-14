5086474bc0770146f04285f80baa538204594ed4
Piper <piperchester@gmail.com>
2021-12-14 08:13:28 -0800

    fix(*): lint

--- backend/climateconnect_api/views/settings_views.py
+++ backend/climateconnect_api/views/settings_views.py
@@ -79,7 +79,7 @@ class UserAccountSettingsView(APIView):
             user.user_profile.email_on_new_project_follower = request.data[
                 'email_on_new_project_follower']
             user.user_profile.email_on_new_project_like = request.data[
-                'email_on_new_project_like']    
+                'email_on_new_project_like']
             user.user_profile.email_on_comment_on_your_idea = request.data[
                 'email_on_comment_on_your_idea']
             user.user_profile.email_on_idea_join = request.data['email_on_idea_join']
