531ade909fd195c3720757ee29e8a03ba59eeac3
Christoph Stoll <christoph1.stoll@gmail.com>
2022-01-28 14:19:21 +0100

    make migrations and add LocalAmbassador to admin.py

--- backend/hubs/admin.py
+++ backend/hubs/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from hubs.models import (
     Hub, HubStat,
-    HubTranslation, HubStatTranslation
+    HubTranslation, HubStatTranslation,
+    LocalAmbassador
 )
 
 admin.site.register(Hub, admin.ModelAdmin)
@@ -12,3 +13,5 @@ admin.site.register(HubStat, admin.ModelAdmin)
 admin.site.register(HubTranslation, admin.ModelAdmin)
 
 admin.site.register(HubStatTranslation, admin.ModelAdmin)
+
+admin.site.register(LocalAmbassador, admin.ModelAdmin)
