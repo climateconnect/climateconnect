a68b04961d18c6263697c767ab5436e63b9c37d8
Dip Dhanesha <ddhanesha@Dips-MacBook-Pro-2.local>
2022-02-17 20:04:45 -0800

    Adding back redis cache to user profile GET request

--- backend/climateconnect_api/views/user_views.py
+++ backend/climateconnect_api/views/user_views.py
@@ -1,7 +1,3 @@
-from ideas.serializers.idea import IdeaFromIdeaSupporterSerializer
-from ideas.models.support import IdeaSupporter
-
-from hubs.models.hub import Hub
 import logging
 import uuid
 from datetime import datetime, timedelta
@@ -9,7 +5,8 @@ from datetime import datetime, timedelta
 from climateconnect_api.models import Availability, Skill, UserProfile
 from climateconnect_api.models.language import Language
 from climateconnect_api.models.user import UserProfileTranslation
-from climateconnect_api.pagination import MembersPagination, MembersSitemapPagination
+from climateconnect_api.pagination import (MembersPagination,
+                                           MembersSitemapPagination)
 from climateconnect_api.permissions import UserPermission
 from climateconnect_api.serializers.user import (
     EditUserProfileSerializer, PersonalProfileSerializer,
@@ -17,18 +14,21 @@ from climateconnect_api.serializers.user import (
     UserProfileSitemapEntrySerializer, UserProfileStubSerializer)
 from climateconnect_api.utility.email_setup import (
     send_password_link, send_user_verification_email)
-from climateconnect_api.utility.translation import (edit_translations)
+from climateconnect_api.utility.translation import edit_translations
 from climateconnect_main.utility.general import get_image_from_data_url
 from django.conf import settings
 from django.contrib.auth import authenticate, login
-
 from django.contrib.auth.models import User
 from django.contrib.gis.db.models.functions import Distance
 from django.db.models import Count, Q
 from django.utils import timezone
+from django.utils.decorators import method_decorator
 from django.utils.translation import gettext as _
-
+from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
+from hubs.models.hub import Hub
+from ideas.models.support import IdeaSupporter
+from ideas.serializers.idea import IdeaFromIdeaSupporterSerializer
 from knox.views import LoginView as KnoxLoginView
 from location.models import Location
 from location.utility import get_location, get_location_with_range
@@ -149,6 +149,10 @@ class ListMemberProfilesView(ListAPIView):
     filterset_fields = ['name']
     search_fields = ['name']
     serializer_class = UserProfileStubSerializer
+
+    @method_decorator(cache_page(settings.DEFAULT_CACHE_TIMEOUT))
+    def dispatch(self, *args, **kwargs):
+        return super(ListMemberProfilesView, self).dispatch(*args, **kwargs)
 
     def get_queryset(self):
         user_profiles = UserProfile.objects\
