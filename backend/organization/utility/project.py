2510c4ee932358fbf5c8d41627f24a4ceae5a11c
FnayouSeif <fnayouseif@gmail.com>
2022-02-18 12:17:55 +0100

    added the similarity calculation function

--- backend/organization/utility/project.py
+++ backend/organization/utility/project.py
@@ -10,6 +10,9 @@ from location.utility import get_location
 from organization.models import Project
 from organization.models.tags import ProjectTags
 
+import pandas as pd
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -72,6 +75,7 @@ def get_project_helpful_connections(project: Project, language_code: str) -> str
 
 
 def get_project_name(project: Project, language_code: str) -> str:
+    print(project)
     if project.language and language_code != project.language.language_code and \
             project.translation_project.filter(language__language_code=language_code).exists():
         return project.translation_project.get(
@@ -127,3 +131,94 @@ def get_project_translations(data: Dict):
         )
     except ValueError:
         raise ValueError
+    
+    
+    
+def get_similar_projects(url_slug:str,return_count=5):
+    """Returns a list of similar projects to the given project input
+    Arguments:
+    url_slug (str): url_slug of the source project for which similar projects will returned
+    return_count (int) : Maximum number of similar projects to return. Defaults to 5
+    
+    Returns:
+        List of similar projects url_slug
+    
+    """
+    def sets_match(source_set:set,target_set:set):
+        """returns the % of matching elements from the source set to the target set.
+            eg: source:  {1,2,3,4} / target : {3,4,5,6} ==> output : 0.5 (50%)
+                source: {3,4} / target : {3,4,5,6} ==> output : 1 (100%)
+        
+        """
+        source_set_count = len(source_set)
+        if source_set_count == 0: return 0
+        matching_elements = 0
+        for source_set_element in source_set:
+            if source_set_element in target_set:
+                matching_elements += 1
+                
+        return matching_elements/source_set_count
+        
+    target_projects = Project.objects.filter(is_active=True).values('url_slug','country','city','project_parent__id','tag_project','skills','language') #project_parent__id 'tag_project' 'skills'
+
+    df = pd.DataFrame.from_dict(target_projects)
+
+    #calcaulte skills match %
+    #since skills are 1 to Many to projects, we need to group the skills in a list
+    skills_df = df.groupby(['url_slug']).agg({'skills':set})
+    source_skills =  skills_df.loc[url_slug][0]
+    skills_df['source_skills'] = [source_skills for i in range(0,len(skills_df))]
+    skills_df['skills_match'] = skills_df.apply(lambda x:sets_match(x['source_skills'],x['skills']),axis=1)
+    
+    #calcualte tags match %
+    #since tags are 1 to Many to projects, we need to group the tags in a list
+    tags_df = df.groupby(['url_slug']).agg({'tag_project':set})
+    source_tags =  tags_df.loc[url_slug][0]
+    tags_df['source_tag_project'] = [source_tags for i in range(0,len(tags_df))]
+    tags_df['tags_match'] = tags_df.apply(lambda x:sets_match(x['source_tag_project'],x['tag_project']),axis=1)
+    
+    #calculate parent/country/city/language similarity. It is a binary evaluation where 1 means a match and 0 no match
+    source_proj_country = df.loc[df.url_slug==url_slug].country.iloc[0]
+    source_proj_city = df.loc[df.url_slug==url_slug].city.iloc[0]
+    source_proj_language = df.loc[df.url_slug==url_slug].language.iloc[0]
+    source_proj_parent_id = df.loc[df.url_slug==url_slug].project_parent__id.iloc[0]
+
+
+    df = df.drop(['skills','tag_project'],axis=1).drop_duplicates().set_index('url_slug')
+    
+
+
+    df['is_same_parent'] = df.project_parent__id.apply(lambda x:1 if x==source_proj_parent_id else 0)
+    df['is_same_city'] = df.apply(lambda x:1 if x['city']==source_proj_city and  x['country']==source_proj_country else 0,axis=1)
+    df['is_same_language'] = df.language.apply(lambda x:1 if x==source_proj_language else 0)
+    df['is_same_country'] = df.country.apply(lambda x:1 if x==source_proj_country else 0)
+    
+
+
+    #calculate similarity score based on the above calculated features 
+    
+    #join the dataframes from above on url_slug index
+    df = pd.concat([df[['is_same_parent','is_same_city','is_same_language','is_same_country']]
+                     ,skills_df[['skills_match']]
+                     ,tags_df[['tags_match']]],axis=1)
+    # weights given to each factor. Increase the integer value to strenghten the weight of a particular factor
+    weights_mapping = {
+    'is_same_parent':3,
+    'is_same_city':2,
+    'is_same_language':2,
+    'is_same_country':1,
+    'skills_match':3,
+    'tags_match':3
+    }
+    total_weights = sum(weights_mapping.values())
+    factors = weights_mapping.keys()
+
+    for factor in factors:
+        df[factor] = df[factor] * weights_mapping[factor]
+      
+    #calulate the similarity score  
+    df['similarity_score'] = df.apply(lambda x:sum(x)/total_weights,axis=1,raw=True)
+    
+
+    return df.sort_values(by=['similarity_score'],ascending=False).head(5).index.values
+    
