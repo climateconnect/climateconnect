15165caf6ff15487f2cc979887f8f5d7223dfea4
Piper <piperchester@gmail.com>
2022-02-28 16:00:02 -0800

    refactor(ProjectCommentsContent.js): drop .toLowerCase

--- frontend/src/components/project/ProjectCommentsContent.js
+++ frontend/src/components/project/ProjectCommentsContent.js
@@ -84,7 +84,7 @@ export default function CommentsContent({ user, project, token, setCurComments }
   return (
     <div>
       <CommentInput user={user} onSendComment={onSendComment} hasComments={comments.length > 0} />
-      <Typography>{comments.length + " " + texts.comments.toLowerCase()}</Typography>
+      <Typography>{comments.length + " " + texts.comments}</Typography>
       <Divider className={classes.divider} />
       {comments && comments.length > 0 && (
         <Posts
