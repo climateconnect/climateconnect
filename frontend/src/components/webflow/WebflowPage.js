b6f8ab736a97f6eb386d5520885f1f9176f07176
Christoph Stoll <christoph1.stoll@gmail.com>
2022-04-04 17:11:21 +0200

    add climatehub page (1st iteration) (#929)
    Co-authored-by: Christoph Stoll <christoph.stoll@climateconnect.earth>

--- frontend/src/components/webflow/WebflowPage.js
+++ frontend/src/components/webflow/WebflowPage.js
@@ -5,7 +5,7 @@ import getTexts from "../../../public/texts/texts";
 import UserContext from "../context/UserContext";
 import WideLayout from "../layouts/WideLayout";
 
-export default function WebflowPage({ bodyContent, headContent, pageKey, className }) {
+export default function WebflowPage({ bodyContent, headContent, pageKey, className, hideFooter }) {
   const { locale } = useContext(UserContext);
   const texts = getTexts({ page: "navigation", locale: locale });
   return (
@@ -17,6 +17,7 @@ export default function WebflowPage({ bodyContent, headContent, pageKey, classNa
         isStaticPage
         hideHeadline
         noSpaceBottom
+        hideFooter={hideFooter}
       >
         <div dangerouslySetInnerHTML={{ __html: bodyContent }} />
       </WideLayout>
