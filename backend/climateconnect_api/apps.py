9f53b40fab5129027209b014207fbfe6e26e5031
FnayouSeif <fnayouseif@gmail.com>
2022-04-19 09:57:32 +0200

    changed autofield default behavior settings for 3.2 version

--- backend/climateconnect_api/apps.py
+++ backend/climateconnect_api/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ClimateconnectApiConfig(AppConfig):
     name = 'climateconnect_api'
+    default_auto_field = 'django.db.models.BigAutoField'
--- backend/location/apps.py
+++ backend/location/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class LocationConfig(AppConfig):
     name = 'location'
+    default_auto_field = 'django.db.models.BigAutoField'
