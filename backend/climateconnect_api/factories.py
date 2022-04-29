776eaaf9e2089f345701af7369ed03059916cf6f
Philipp <philipp.ernstberger@hotmail.com>
2022-04-29 16:53:19 +0200

    changed requirements, finished tests, added pics

--- backend/climateconnect_api/factories.py
+++ backend/climateconnect_api/factories.py
@@ -1,12 +1,18 @@
 import factory
 from django.contrib.auth.models import User
+from django.conf import settings
 
 from climateconnect_api.models import UserProfile
-from organization.models.project import Project
+from organization.models.project import Project, ProjectParents
+from organization.models.likes import ProjectLike
 from climateconnect_api.models.language import Language
 from ideas.models.ideas import Idea
+from hubs.models.hub import Hub
+from organization.models.status import ProjectStatus
 from location.models import Location
 from organization.models.organization import Organization
+from organization.models.members import OrganizationMember
+from climateconnect_api.models.role import Role
 from climateconnect_api.models.user import UserProfile
 
 
@@ -14,7 +20,28 @@ class UserFactory(factory.DjangoModelFactory):
     class Meta:
         model = User
 
+    username = factory.Sequence(lambda n: 'user{}'.format(n+1))
     is_active = True
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.LazyAttribute(lambda a: '{}.{}@test.test'.format(a.first_name, a.last_name).lower())
+
+
+class LocationFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Location
+
+    name = factory.Sequence(lambda n: f'test-location{n+1}') 
+    city = factory.Sequence(lambda n: f'test-city{n+1}') 
+    country = factory.Sequence(lambda n: f'test-country{n+1}')
+    
+
+class LanguageFactory(factory.DjangoModelFactory):
+    class Meta: 
+        model = Language
+
+    name = factory.Sequence(lambda n: 'languagename{}'.format(n+1))
+    language_code = factory.Sequence(lambda n: '{}'.format(n+1))
 
 
 class UserProfileFactory(factory.DjangoModelFactory):
@@ -22,25 +49,110 @@ class UserProfileFactory(factory.DjangoModelFactory):
         model = UserProfile
 
     user = factory.SubFactory(UserFactory)
-    url_slug = "testing"
+    url_slug = factory.Sequence(lambda n: 'userslug{}'.format(n+1))
     image = "profile.png"
+    thumbnail_image = factory.django.ImageField(from_path=(settings.BASE_DIR + "/climateconnect_api/tests/media/" + "userprofile_thumbnail_image.jpeg"))
     background_image = "background.png"
-    country = "Planet"
-    state = "Earth"
-    city = "Climate"
-    biography = "Testing bio"
+    biography = "This is a biography"
+    send_newsletter = True
+    is_profile_verified = True
 
+
+class HubFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Hub
+
+    name = factory.Sequence(lambda n: 'hubname{}'.format(n+1))
+    url_slug = factory.Sequence(lambda n: 'hub_slug{}'.format(n+1))
+    segway_text = "This is a segway_text"
+    quick_info = "This is a quick_info"
+    hub_type = 0
+
+    # implementation of manytomany field
+    @factory.post_generation
+    def location(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of groups were passed in, use them
+            for loc in extracted:
+                self.location.add(loc)
+
+
+class ProjectStatusFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = ProjectStatus
+        django_get_or_create = ('name',)
+
+    name="In Progress"
+    status_type=1
+    has_end_date= True
+    has_start_date=True
 
 
 class ProjectFactory(factory.DjangoModelFactory):
     class Meta:
         model = Project
 
+    name = factory.Sequence(lambda n: 'Projectname{}'.format(n+1))
+    url_slug = factory.Sequence(lambda n: 'projectslug{}'.format(n+1))
+    status = factory.SubFactory(ProjectStatusFactory)
+    thumbnail_image = factory.django.ImageField(from_path=(settings.BASE_DIR + "/climateconnect_api/tests/media/" + "project_thumbnail_image.jpeg"))
+    short_description = "How can we maximize our chances to fight climate change? Climate Connect web-platform is a tool, free of use, helping all climate actors to gain visibility and collaborate with each other!"
+
+
+class ProjectParentsFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = ProjectParents
+
+    project = factory.SubFactory(ProjectFactory)
+    parent_user = factory.SubFactory(UserFactory)
+
+
+class ProjectLikeFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = ProjectLike
+
+    project = factory.SubFactory(ProjectFactory)
     user = factory.SubFactory(UserFactory)
-    url_slug = "testing"
-    image = "profile.png"
-    background_image = "background.png"
-    country = "Planet"
-    state = "Earth"
-    city = "Climate"
-    biography = "Testing bio"
+
+
+class OrganizationFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Organization
+    
+    name = factory.Sequence(lambda n: 'org_name{}'.format(n+1))
+    url_slug = factory.Sequence(lambda n: 'org_slug{}'.format(n+1))
+    thumbnail_image = factory.django.ImageField(from_path=(settings.BASE_DIR + "/climateconnect_api/tests/media/" + "org_thumbnail_image.jpeg"))
+
+
+class RoleFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Role
+        django_get_or_create = ('name', 'role_type')
+
+    name = 'Creator'
+    role_type = Role.ALL_TYPE
+
+
+class OrganizationMemberFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = OrganizationMember
+
+    user = factory.SubFactory(UserFactory)
+    organization = factory.SubFactory(OrganizationFactory)
+    role = factory.SubFactory(RoleFactory)
+
+
+class IdeaFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Idea
+
+    name = factory.Sequence(lambda n: 'idea_name{}'.format(n+1))
+    url_slug = factory.Sequence(lambda n: 'idea_slug{}'.format(n+1))
+    thumbnail_image = factory.django.ImageField(from_path=(settings.BASE_DIR + "/climateconnect_api/tests/media/" + "idea_thumbnail_image.jpeg"))
+    user = factory.SubFactory(UserFactory)
+    short_description="This is a short description"
+    
--- backend/climateconnect_api/tests/test_user_views.py
+++ backend/climateconnect_api/tests/test_user_views.py
@@ -1,13 +1,19 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.test import TestCase
 from datetime import timedelta
 from django.utils import timezone
+from django.conf import settings
 
-from climateconnect_api.tasks import (fetch_entities_for_weekly_recommendations, fetch_user_info_for_weekly_recommendations)
+from climateconnect_api.models.language import Language
+from location.models import Location
+from climateconnect_api.tests.create_test_data import (create_project_test_data, create_org_test_data, create_idea_test_data)
+
+from climateconnect_api.tasks import (fetch_entities_for_weekly_recommendations, fetch_user_info_for_weekly_recommendations, process_user_info_and_send_weekly_recommendations, fetch_and_create_globals_for_weekly_recommendations)
 
 from django.urls import reverse
 
-from climateconnect_api.factories import UserFactory
+from climateconnect_api.factories import HubFactory, LanguageFactory, LocationFactory, UserFactory, UserProfileFactory
 
 
 class TestUserLoginView(APITestCase):
@@ -70,22 +76,163 @@ class TestSignUpView(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
-class TestRecommendedEmail(APITestCase):
-    # tests the blabla
-    def setUp(self):
-        self.user = UserFactory(username="test_user", password="testing2020")
- 
+class TestRecommendedEmail(TestCase):
+    # test the recommended email feature
+    # unit test for generation of project ids, org ids and idea ids
+    # unit test for generation of user ids
+    # test that sends email to MAILJET_ADMIN_EMAIL
+
+    # this function is called once before any test functions are run; the setUp function is called before every test function 
+    @classmethod
+    def setUpTestData(cls):
+        english_language = LanguageFactory(name='english', native_name='english', language_code='en', currency='$')
+        german_language = LanguageFactory(name='german', native_name='Deutsch', language_code='de', currency='€')
+
+        cls.location_1 = LocationFactory()
+        cls.location_2 = LocationFactory()
+        cls.location_3 = LocationFactory()
+
+        hub_1 = HubFactory(hub_type=1, location=[cls.location_1])
+        hub_2 = HubFactory(hub_type=1, location=[cls.location_2])
+
+        cls.english_user_in_hub_1 = UserProfileFactory(language=english_language, location=cls.location_1)
+        cls.english_user_in_hub_2 = UserProfileFactory(language=english_language, location=cls.location_2)
+        cls.german_user_in_hub_1 = UserProfileFactory(language=german_language, location=cls.location_1)
+        cls.german_user_in_hub_2 = UserProfileFactory(language=german_language, location=cls.location_2)
+        cls.english_user_no_hub = UserProfileFactory(language=english_language)
+        cls.german_user_no_hub = UserProfileFactory(language=german_language)
+        cls.no_language_user_in_hub_1 = UserProfileFactory(location=cls.location_1)
+        cls.no_language_user_no_hub  = UserProfileFactory()
+        cls.english_user_in_hub_1_no_newsletter = UserProfileFactory(language=english_language, location=cls.location_1, send_newsletter=False)
+
+        cls.project_in_hub_1_3likes = create_project_test_data(number_of_likes=3, location=cls.location_1, has_parent_organization=True)
+        cls.project_in_hub_2_2likes = create_project_test_data(number_of_likes=2, location=cls.location_2)
+        cls.project_in_hub_2_1likes = create_project_test_data(number_of_likes=1, location=cls.location_2, has_parent_organization=True)
+        cls.project_in_hub_1_1likes = create_project_test_data(number_of_likes=1, location=cls.location_1)
+        cls.project_outside_of_timespan = create_project_test_data(number_of_likes=5, location=cls.location_1, created_outside_of_timespan=True)
+        cls.project_no_hub_4likes = create_project_test_data(number_of_likes=4, has_parent_organization=True)
+        cls.project_no_hub_0likes = create_project_test_data(number_of_likes=0)
+
+        cls.org_in_hub_1 = create_org_test_data(location=cls.location_1)
+        cls.org_in_hub_2 = create_org_test_data(location=cls.location_2)
+        cls.org_no_hub = create_org_test_data()
+        cls.org_in_hub_1_outside_of_timespan = create_org_test_data(location=cls.location_1, created_outside_of_timespan=True)
+
+        cls.idea_in_hub_1 = create_idea_test_data(location=cls.location_1, hub=hub_1)
+        cls.idea_in_hub_2_outside_of_timespan = create_idea_test_data(location=cls.location_2, hub=hub_2, created_outside_of_timespan=True)
+
+        # creating user that receives all emails
+        cls.admin_user = UserFactory(username='Admin', email=settings.MAILJET_ADMIN_EMAIL, first_name='You', last_name='Admin')
 
 
-
-    def test_entities(self):       
+    def test_entities(self):   
         max_entities = 3
         timespan = timezone.now() - timedelta(days=7)
-        fetch_entities_for_weekly_recommendations(max_entities, timespan, location_id, is_in_hub)
-        
-        self.assertEqual()
+        is_in_hub = True
+
+        # fetch entities in hub 1
+        result = fetch_entities_for_weekly_recommendations(max_entities, timespan, self.location_1.id, is_in_hub)
+        expected_result = [
+            [self.project_in_hub_1_3likes.id],
+            [self.org_in_hub_1.id], 
+            [self.idea_in_hub_1.id]
+            ]
+        self.assertEqual(result, expected_result)
+
+        # fetch entities in hub 2
+        result = fetch_entities_for_weekly_recommendations(max_entities, timespan, self.location_2.id, is_in_hub)        
+        expected_result = [
+            [self.project_in_hub_2_2likes.id, self.project_in_hub_2_1likes.id],
+            [self.org_in_hub_2.id],
+            []
+        ]
+        self.assertEqual(result, expected_result)
+
+        # fetch international entities
+        is_in_hub = False
+        no_location = 0
+        result = fetch_entities_for_weekly_recommendations(max_entities, timespan, no_location, is_in_hub)        
+        expected_result = [
+            [self.project_no_hub_4likes.id, self.project_in_hub_1_3likes.id],
+            [self.org_no_hub.id],
+            []
+        ]
+        self.assertEqual(result, expected_result)
+
 
     def test_user_data(self):
-        fetch_user_info_for_weekly_recommendations(location_id, is_in_hub) 
-        expected_result = [(1, 1), (1, 1),]
-        self.assertEqual(expected_result)
+        # tests if the users_ids are generated correctly
+        
+        # for hub_1
+        result = {}
+        is_in_hub = True
+        result_queries = fetch_user_info_for_weekly_recommendations(self.location_1.id, is_in_hub)
+        expected_result = {
+            'en':[self.english_user_in_hub_1.id, self.no_language_user_in_hub_1.id],
+            'de':[self.german_user_in_hub_1.id],
+        }    
+        # the function gives back a dict with lang_code as key and a query as value
+        # the query needs to be evaluated first
+        for key_result, result_query in result_queries.items():
+            result[key_result] = list(result_query)
+        self.assertCountEqual(result.items(), expected_result.items())
+
+        # for hub_2
+        result = fetch_user_info_for_weekly_recommendations(self.location_2.id, is_in_hub)
+        expected_result = {
+            'en':[self.english_user_in_hub_2.id],
+            'de':[self.german_user_in_hub_2.id],
+        } 
+        # the function gives back a dict with lang_code as key and a query as value
+        # the query needs to be evaluated first
+        for key_result, result_query in result.items():
+            result[key_result] = list(result_query)
+        self.assertCountEqual(result.items(), expected_result.items())
+
+        # for international
+        is_in_hub = False
+        # location_id is 0 for international 
+        loc = 0
+        result = fetch_user_info_for_weekly_recommendations(loc, is_in_hub)
+        expected_result = {
+            'en':[self.english_user_no_hub.id, self.no_language_user_no_hub.id],
+            'de':[self.german_user_no_hub.id],
+        } 
+        # the function gives back a dict with lang_code as key and a query as value
+        # the query needs to be evaluated first
+        for key_result, result_query in result.items():
+            result[key_result] = list(result_query)
+        self.assertCountEqual(result, expected_result)
+
+
+    def test_send_email(self):
+        # this test will send emails to the mailjet_admin_email adress set in .backend_env
+        # it sends emails for each hub/international and for each language, so a maximum number of 6 emails
+        # to turn email sending on, set sandbox_mode to False
+        sandbox_mode = True
+        # to determine how many emails get sent, please set max_emails_sent variable to your desired number
+        max_emails_sent = 2
+        
+        max_entities = 3
+        timespan = timezone.now() - timedelta(days=7)
+
+        all_locations_in_hubs = list(Location.objects.filter(hub_location__hub_type=1).values_list('id', flat = True).distinct())
+        # "0" acts as a flag for the international recommendations email
+        all_locations_in_hubs.append(0)
+        for location_id in all_locations_in_hubs:
+            if location_id:
+                is_in_hub = True
+            else:
+                is_in_hub = False
+
+            mailjet_global_vars = fetch_and_create_globals_for_weekly_recommendations(max_entities, timespan, location_id, is_in_hub)
+
+            lang_codes = list(Language.objects.values_list("language_code", flat=True).distinct())
+            if mailjet_global_vars:
+                for lang_code in lang_codes:
+                    if (max_emails_sent < 1):
+                        break
+                    result = process_user_info_and_send_weekly_recommendations([self.admin_user.id], mailjet_global_vars, lang_code, is_in_hub, sandbox_mode)
+                    self.assertEqual(result.status_code, status.HTTP_200_OK)
+                    max_emails_sent -= 1
+
