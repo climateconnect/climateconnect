d133cedd73670670d0481ab3857c3bf23f881387
Philipp <philipp.ernstberger@hotmail.com>
2022-05-30 08:11:05 +0200

    Merging into master

--- start_celery.sh
+++ start_celery.sh
@@ -9,5 +9,5 @@ cd backend
 # Install requirements
 pip install -r requirements.txt
 
-# starting celery worker and celery beat together.
-celery -A climateconnect_main worker -B -l INFO 
+# starting celery worker
+celery -A climateconnect_main -l INFO
