0455ffb4c72e57de31e25aa45bb0299de91c51b8
fnayou <fnayouseif@gmail.com>
2022-07-14 10:30:55 +0200

    Migrating Django to 3.2 (#936)
    * changed autofield default behavior settings for 3.2 version
    
    * added http protocal routing to remove warning of migration
    
    * updated requirements.txt
    
    * sql migration files
    
    * remove empty requirement
    
    * Remove version tag (deprecated)
    
    * Add redis variables to local env setup
    
    Co-authored-by: Christoph Stoll <christoph.stoll@climateconnect.earth>

--- backend/chat_messages/apps.py
+++ backend/chat_messages/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ChatMessagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat_messages'
--- backend/climateconnect_main/routing.py
+++ backend/climateconnect_main/routing.py
@@ -3,8 +3,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat_messages.consumer import DirectMessageConsumer
 from climateconnect_main.token_auth import TokenAuthMiddlewareStack
+from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
+    "http":get_asgi_application(),
     "websocket": TokenAuthMiddlewareStack(URLRouter([
         path("ws/chat/", DirectMessageConsumer, name='chat-messaging')
     ]))
