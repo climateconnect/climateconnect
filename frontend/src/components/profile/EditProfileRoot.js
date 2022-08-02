2fd182cff082f83791cea8da8d6bdf57344d4da7
JoshKornfeld <j.f.kornfeld@gmail.com>
2022-08-02 15:54:03 +0200

    Fixed availability not updating when editing profile

--- frontend/src/components/profile/EditProfileRoot.js
+++ frontend/src/components/profile/EditProfileRoot.js
@@ -90,8 +90,8 @@ export default function EditAccountRoot({
     }
     editedAccount.language = sourceLanguage;
     const parsedProfile = parseProfileForRequest(editedAccount, availabilityOptions, user);
-    console.log(parsedProfile);
     const payload = await getProfileWithoutRedundantOptions(user, parsedProfile);
+
     payload.translations = parseTranslationsForRequest(
       getTranslationsWithoutRedundantKeys(
         getTranslationsFromObject(initialTranslations, "user_profile"),
@@ -138,7 +138,7 @@ export default function EditAccountRoot({
     setEditedProfile(editedAccount);
     setStep(STEPS[1]);
   };
-  console.log(editedProfile);
+
   return (
     <>
       {profile ? (
@@ -189,10 +189,11 @@ export default function EditAccountRoot({
 }
 
 const parseProfileForRequest = (profile, availabilityOptions, user) => {
-  const availability = availabilityOptions.find((o) => o.name == profile.info.availability);
+  const availability = availabilityOptions.find((o) => o.key == profile.info.availability);
   const image = profile.image;
   const thumbnail = profile.thumbnail_image;
   const background = profile.background_image;
+
   return {
     first_name: profile.first_name,
     last_name: profile.last_name,
@@ -217,6 +218,7 @@ const getProfileWithoutRedundantOptions = async (user, newProfile) => {
     background_image: getImageUrl(user.background_image),
     availability: user.availability && user.availability.id,
   };
+
   const finalProfile = {};
   Object.keys(newProfile).map((k) => {
     if (
@@ -234,6 +236,7 @@ const getProfileWithoutRedundantOptions = async (user, newProfile) => {
     finalProfile.thumbnail_image = await blobFromObjectUrl(finalProfile.thumbnail_image);
   if (finalProfile.background_image)
     finalProfile.background_image = await blobFromObjectUrl(finalProfile.background_image);
+
   return finalProfile;
 };
 
