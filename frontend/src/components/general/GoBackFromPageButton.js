bd899a63b2191a782f9d535769ec17fdfa989274
JoshKornfeld <j.f.kornfeld@gmail.com>
2022-08-04 11:40:15 +0200

    GoBackFromPage button is now not only used by project page

---
+++ frontend/src/components/general/GoBackFromPageButton.js
@@ -0,0 +1,70 @@
+import { Button, IconButton, makeStyles } from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import { useRouter } from "next/router";
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    color: "white",
+    [theme.breakpoints.down("xs")]: {
+      minWidth: 35,
+      maxWidth: 35,
+      minHeight: 35,
+      maxHeight: 35,
+      backgroundColor: theme.palette.primary.main,
+      "&:hover": {
+        backgroundColor: theme.palette.primary.main,
+      },
+    },
+  },
+}));
+
+export default function GoBackFromPageButton({
+  texts,
+  tinyScreen,
+  locale,
+  containerClassName,
+}) {
+  const classes = useStyles();
+
+  const router = useRouter();
+  const goBack = () => {
+    const url = document.location.href;
+    const isOrganizationPage = url.includes("organizations");
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const hubPage = urlParams.get("hubPage");
+
+    const hubsLink = "/" + locale + "/hubs/" + hubPage;
+    const browseLink = "/" + locale + "/browse";
+    const organizationLink = "/" + locale + "/browse?&#organizations";
+
+    if (isOrganizationPage) {
+      console.log("here");
+      router.push(organizationLink);
+    } else if (hubPage) {
+      router.push(hubsLink);
+    } else {
+      router.push(browseLink);
+    }
+  };
+
+  if (tinyScreen)
+    return (
+      <div className={containerClassName}>
+        <IconButton onClick={goBack} className={classes.button}>
+          {/*adjusted viewBox to center the icon*/}
+          <ArrowBackIosIcon fontSize="small" viewBox="-4.5 0 24 24" />
+        </IconButton>
+      </div>
+    );
+  else
+    return (
+      <div className={containerClassName}>
+        <Button onClick={goBack} className={classes.button} startIcon={<ArrowBackIcon />}>
+          {texts.go_back}
+        </Button>
+      </div>
+    );
+}
