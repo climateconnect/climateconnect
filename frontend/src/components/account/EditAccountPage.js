5ff754a1e54abe4c1ee44da83132b7c6c596ebe9
JoshKornfeld <j.f.kornfeld@gmail.com>
2022-08-05 10:46:16 +0200

    Added link to email for request to delete account

--- frontend/src/components/account/EditAccountPage.js
+++ frontend/src/components/account/EditAccountPage.js
@@ -9,6 +9,7 @@ import {
   Tooltip,
   Typography,
   useMediaQuery,
+  Link
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
@@ -210,6 +211,9 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: "wrap",
     justifyContent: "center",
     marginTop: theme.spacing(10),
+  },
+  spaceStrings: {
+    width: 4
   },
   checkTranslationsButtonAndManageMembersButtonContainer: {
     display: "flex",
@@ -919,7 +923,14 @@ export default function EditAccountPage({
         {deleteEmail && (
           <Typography variant="subtitle2" className={classes.deleteMessage}>
             <InfoOutlinedIcon />
-            {texts.if_you_wish_to_delete} {deleteEmail}
+            {texts.if_you_wish_to_delete}
+            <div
+            className={classes.spaceStrings}> </div>
+            <Link
+            href="mailto:contact@climateconnect.earth"
+            > 
+            
+            {deleteEmail}</Link>
           </Typography>
         )}
       </form>
