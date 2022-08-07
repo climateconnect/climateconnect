9bcee8335a12448f94a0a97c3fb0c119c29bc029
Dip Dhanesha <ddhanesha@users.noreply.github.com>
2022-08-07 11:11:39 -0700

    Trying to run celery scheduler (#1013)
    * Trying to run celery scheduler
    
    * Updated CODEOWNERS file
    
    Co-authored-by: Dip Dhanesha <ddhanesha@Dips-MacBook-Pro-2.local>

--- backend/climateconnect_main/celery.py
+++ backend/climateconnect_main/celery.py
@@ -15,8 +15,13 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    "schedule_automated_email_reminder_for_notifications": {
-        "task": "climateconnect_api.tasks.schedule_automated_reminder_for_user_notifications",
-        "schedule": crontab(minute=0, hour=0),
+    "test_task": {
+        "task": "climateconnect_api.tasks.test_task",
+        "schedule": crontab(minute="*/1"),
+        "args": (6, 5)
     }
+    # "schedule_automated_email_reminder_for_notifications": {
+    #     "task": "climateconnect_api.tasks.schedule_automated_reminder_for_user_notifications",
+    #     "schedule": crontab(minute=0, hour=0),
+    # }
 }
