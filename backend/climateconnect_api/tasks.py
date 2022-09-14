d26bc45d9a0c6cd4a1899229ea4782251035423e
ddhanesha <ddhanesha@users.noreply.github.com>
2022-09-13 21:14:52 -0700

    Preparing to release celery on main backend app service

--- backend/climateconnect_api/tasks.py
+++ backend/climateconnect_api/tasks.py
@@ -16,42 +16,47 @@ logger = logging.getLogger(__name__)
 
 
 @app.task
-def schedule_automated_reminder_for_user_notifications():
-    # Get all user_ids for people who have not checked their notification
-    all_user_ids = list(
-        UserNotification.objects.filter(
-            read_at__isnull=True,
-            created_at__lte=(timezone.now() - timedelta(days=2)),
-            notification__notification_type=Notification.PRIVATE_MESSAGE,
-        )
-        .values_list("user_id", flat=True)
-        .distinct()
-    )
-    for i in range(0, len(all_user_ids), settings.USER_CHUNK_SIZE):
-        user_ids = [u_ids for u_ids in all_user_ids[i : i + settings.USER_CHUNK_SIZE]]
-        send_email_notifications.apply_async((user_ids,))
+def testing_task():
+    logger.info(f"CELERY {5+4}")
 
 
-@app.task(bind=True)
-def send_email_notifications(self, user_ids: List):
-    for u_id in user_ids:
-        try:
-            user = User.objects.get(user_id=u_id)
-        except User.DoesNotExist:
-            logger.info(f"User profile does not exists for user {u_id}")
-            continue
+# @app.task
+# def schedule_automated_reminder_for_user_notifications():
+#     # Get all user_ids for people who have not checked their notification
+#     all_user_ids = list(
+#         UserNotification.objects.filter(
+#             read_at__isnull=True,
+#             created_at__lte=(timezone.now() - timedelta(days=2)),
+#             notification__notification_type=Notification.PRIVATE_MESSAGE,
+#         )
+#         .values_list("user_id", flat=True)
+#         .distinct()
+#     )
+#     for i in range(0, len(all_user_ids), settings.USER_CHUNK_SIZE):
+#         user_ids = [u_ids for u_ids in all_user_ids[i : i + settings.USER_CHUNK_SIZE]]
+#         send_email_notifications.apply_async((user_ids,))
 
-        unread_user_notifications = UserNotification.objects.filter(
-            user_id=u_id,
-            read_at__isnull=True,
-            notification__notification_type=Notification.PRIVATE_MESSAGE,
-        )
 
-        if (
-            unread_user_notifications.exists()
-            and user.user_profile
-            and user.user_profile.email_on_private_chat_message is True
-        ):
-            send_email_reminder_for_unread_notifications(
-                user=user, user_notifications=unread_user_notifications
-            )
+# @app.task(bind=True)
+# def send_email_notifications(self, user_ids: List):
+#     for u_id in user_ids:
+#         try:
+#             user = User.objects.get(user_id=u_id)
+#         except User.DoesNotExist:
+#             logger.info(f"User profile does not exists for user {u_id}")
+#             continue
+
+#         unread_user_notifications = UserNotification.objects.filter(
+#             user_id=u_id,
+#             read_at__isnull=True,
+#             notification__notification_type=Notification.PRIVATE_MESSAGE,
+#         )
+
+#         if (
+#             unread_user_notifications.exists()
+#             and user.user_profile
+#             and user.user_profile.email_on_private_chat_message is True
+#         ):
+#             send_email_reminder_for_unread_notifications(
+#                 user=user, user_notifications=unread_user_notifications
+#             )
