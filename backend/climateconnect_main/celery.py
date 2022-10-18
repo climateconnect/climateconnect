766dd6310137ab4b0b32c0eb604b262365513d2c
Christoph Stoll <christoph.stoll@climateconnect.earth>
2022-10-18 17:56:12 +0200

    uncomment automated email reminder task

--- backend/climateconnect_main/celery.py
+++ backend/climateconnect_main/celery.py
@@ -15,11 +15,11 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    # "schedule_automated_email_reminder_for_notifications": {
-    #     "task": "climateconnect_api.tasks.schedule_automated_reminder_for_user_notifications",
-    #     # 0-6 = Sunday to Saturday
-    #     "schedule": crontab(day_of_week=4, minute=0, hour=0),
-    # },
+    "schedule_automated_email_reminder_for_notifications": {
+        "task": "climateconnect_api.tasks.schedule_automated_reminder_for_user_notifications",
+        # 0-6 = Sunday to Saturday
+        "schedule": crontab(day_of_week=3, minute=0, hour=4),
+    },
     "testing_task_1": {
         "task": "climateconnect_api.tasks.testing_task_1",
         "schedule": crontab(day_of_week=3, minute=0, hour=1),
