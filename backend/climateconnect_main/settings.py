a4df9dae5361c0081dc6ed6181e4b167d5054bc3
ddhanesha <ddhanesha@users.noreply.github.com>
2022-10-21 21:13:08 -0700

    Updated DEBUG logic

--- backend/climateconnect_main/settings.py
+++ backend/climateconnect_main/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG") == "True"
+DEBUG = env("DEBUG", False)
 
 ALLOWED_HOSTS = get_allowed_hosts(env("ALLOWED_HOSTS"))
 
