e0abd8afebf298fc16be98cc050ee866d65981f5
JoshKornfeld <j.f.kornfeld@gmail.com>
2022-11-10 17:19:44 +0100

    Removed css rule

--- frontend/src/components/dialogs/SignUpPromptDialog.js
+++ frontend/src/components/dialogs/SignUpPromptDialog.js
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
   },
   imageContainer: (props) =>  ({
    
-    background: `url('${props.image}') no-repeat`,
+    background: `url('${props.image}')`,
     backgroundSize: "cover",
     backgroundPositionX: "-110px",
     position: "relative",
