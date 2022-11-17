ce6283aeda4c367f6254643cf48898fb78903338
JoshKornfeld <j.f.kornfeld@gmail.com>
2022-11-17 17:48:49 +0100

    Feat: Add filtering to search name of message sender

--- backend/chat_messages/admin.py
+++ backend/chat_messages/admin.py
@@ -11,9 +11,14 @@ pass_through_models = (MessageParticipants, Participant, MessageReceiver)
 for model in pass_through_models:
     admin.site.register(model, admin.ModelAdmin)
 
+
 class MessageAdmin(admin.ModelAdmin):
-    search_fields = (
-        "message_participant__id",
-    )
+    
+    search_fields = [
+        "message_participant__id__iexact",
+        "sender__first_name__iexact",
+        "sender__last_name__iexact",
+    ]
+
 
 admin.site.register(Message, MessageAdmin)
