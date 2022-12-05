7367396b1a6dcbadf8bce419e68f1454365c3ef7
Piper Chester <piperchester@gmail.com>
2022-12-05 06:57:36 -0700

    update

---
+++ start_backend.sh
@@ -0,0 +1,14 @@
+# Enter virtual environment
+source pythonenv3.8/bin/activate
+
+# Install spatial dependencies
+apt-get update -qq && apt-get install binutils libproj-dev gdal-bin -yqq
+
+# Go to backend folder
+cd backend
+
+# Install python dependencies
+make install
+
+# Start server
+gunicorn --preload --bind=0.0.0.0 climateconnect_main.asgi:application -w 4 -k uvicorn.workers.UvicornWorker & celery -A climateconnect_main worker -B -l INFO
