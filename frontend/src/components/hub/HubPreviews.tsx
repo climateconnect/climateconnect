8ac63547aef7a77b674a4c41414be0b6303ff0ea
ddhanesha <ddhanesha@users.noreply.github.com>
2023-04-18 21:02:31 -0700

    Fixing build failure

--- frontend/src/components/hub/HubPreviews.tsx
+++ frontend/src/components/hub/HubPreviews.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Grid from "@mui/material/Unstable_Grid2";
+import Grid from '@mui/material/Grid';
 import makeStyles from "@mui/styles/makeStyles";
 import HubPreview from "./HubPreview";
 
