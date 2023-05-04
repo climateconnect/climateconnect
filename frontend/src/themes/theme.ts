9fbfb9cea3bd4250dd82d6b24d3a35ab3eeca06e
Marco <marco.rehmer@ironic-dev.de>
2023-05-04 21:23:20 +0200

    fix prettier

--- frontend/src/themes/theme.ts
+++ frontend/src/themes/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme, alpha } from "@mui/material/styles";
+import { alpha, createTheme } from "@mui/material/styles";
 import { grey } from "@mui/material/colors";
 
 declare module "@mui/material" {
@@ -61,9 +61,9 @@ const coreTheme = createTheme({
       fontWeight: 600,
     },
     /* TOOD: climateMatch: {
-      fontFamily: "flood-std, sans-serif",
-      fontWeight: 300,
-    },*/
+          fontFamily: "flood-std, sans-serif",
+          fontWeight: 300,
+        },*/
   },
 });
 
@@ -159,7 +159,6 @@ const theme = createTheme(coreTheme, {
           style: {
             background: "#e0e0e0",
             color: coreTheme.palette.secondary.main,
-            
           },
         },
       ],
@@ -172,14 +171,14 @@ const theme = createTheme(coreTheme, {
         deleteIconColorSecondary: {
           color: coreTheme.palette.secondary.main,
           "&:hover": {
-            color: coreTheme.palette.secondary.light
-          }
+            color: coreTheme.palette.secondary.light,
+          },
         },
         clickable: {
           "&:hover": {
-            background: "#cacaca"
-          }
-        }
+            background: "#cacaca",
+          },
+        },
       },
     },
     MuiLink: {
