44d195017ee559233c600485341006d63a8cb74e
phiresky <phireskyde+git@gmail.com>
2023-05-19 20:32:35 +0200

    switch to pdm

--- initial_dev_setup.sh
+++ initial_dev_setup.sh
@@ -53,7 +53,7 @@ else
 fi
 
 pushd backend
-source .venv/bin/activate
+$(pdm venv activate)
 make migrate
 make test-data
 popd
--- start_backend.sh
+++ start_backend.sh
@@ -1,14 +1,18 @@
-# Enter virtual environment
-source pythonenv3.8/bin/activate
 
 # Install spatial dependencies
 apt-get update -qq && apt-get install binutils libproj-dev gdal-bin -yqq
 
+# Install pdm
+curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 -
+
 # Go to backend folder
 cd backend
 
-# Install python dependencies
-pip install -r requirements.txt
+# install dependencies
+pdm install
+
+# activate venv
+$(pdm venv activate)
 
 # Start server
 gunicorn --preload --bind=0.0.0.0 climateconnect_main.asgi:application -w 4 -k uvicorn.workers.UvicornWorker & celery -A climateconnect_main worker -B -l INFO
