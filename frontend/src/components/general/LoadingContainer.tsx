e0d7d243caee25bf3cda0a28184188074cac0098
Christoph Stoll <christoph1.stoll@gmail.com>
2024-05-17 11:47:10 +0200

    Display spinning logo when loading (#1289)
    Co-authored-by: Christoph Stoll <christoph.stoll@climateconnect.earth>

--- frontend/src/components/general/LoadingContainer.tsx
+++ frontend/src/components/general/LoadingContainer.tsx
@@ -1,8 +1,10 @@
-import { Theme, Typography } from "@mui/material";
+import { Icon, SvgIcon, Theme, Typography } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
 import React, { useContext } from "react";
 import getTexts from "../../../public/texts/texts";
 import UserContext from "../context/UserContext";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { Box } from "@mui/system";
 
 const useStyles = makeStyles<Theme, { subtractedHeight?: string }>(() => ({
   spinnerContainer: (props) => ({
@@ -14,9 +16,9 @@ const useStyles = makeStyles<Theme, { subtractedHeight?: string }>(() => ({
     height: `calc(100vh - ${props.subtractedHeight}px)`,
     flexDirection: "column",
   }),
-  spinner: {
-    width: 100,
-  },
+  text: {
+    fontSize: 18
+  }
 }));
 export default function LoadingContainer({ headerHeight, footerHeight }) {
   const classes = useStyles({
@@ -24,12 +26,33 @@ export default function LoadingContainer({ headerHeight, footerHeight }) {
   });
   const { locale } = useContext(UserContext);
   const texts = getTexts({ page: "general", locale: locale });
+  console.log(texts)
   return (
     <div className={classes.spinnerContainer}>
       <div>
-        <img className={classes.spinner} src="/images/logo.svg" alt="Climate Connect logo" />
+        <LoadingIcon />
       </div>
-      <Typography component="div">{texts.loading_and_waiting}</Typography>
+      <Typography component="div" className={classes.text}>{texts.loading_and_waiting}</Typography>
     </div>
   );
+}
+
+function LoadingIcon() {
+  return (<Box
+    component="img"
+    src="/images/logo_spinner.svg" 
+    sx={{
+      height: 80,
+      width: 80,
+      animation: "spin 2s linear infinite",
+    "@keyframes spin": {
+      "0%": {
+        transform: "rotate(0deg)",
+      },
+      "100%": {
+        transform: "rotate(360deg)",
+      },
+    },
+    }}
+  />)
 }
