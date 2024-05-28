266f099d60281e413a277d1b20c8eb0400871cb6
fateme ramezanpour <ramezanpourftm@gmail.com>
2024-05-28 18:13:03 +0200

    fixed bug to show date tag for events on organization page

--- frontend/src/components/project/ProjectPreview.tsx
+++ frontend/src/components/project/ProjectPreview.tsx
@@ -113,7 +113,7 @@ export default function ProjectPreview({ project, projectRef, hubUrl, className 
   const projectType =
     projectTypes && projectTypes.length > 0
       ? projectTypes.find((t) => t.type_id === project.project_type)
-      : { name: project.project_type };
+      : { name: project.project_type, type_id: project.project_type };
   const texts = getTexts({ page: "project", locale: locale });
   const classes = useStyles({ hovering: hovering });
   const handleMouseEnter = () => {
