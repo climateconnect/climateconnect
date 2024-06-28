6080f6d6c28ef9b49c3ceb12b9b78d9747c1e310
fateme ramezanpour <ramezanpourftm@gmail.com>
2024-06-28 16:17:15 +0200

    fixing horizontall scroll for youtube video in mobile

--- frontend/src/components/communication/MessageContent.tsx
+++ frontend/src/components/communication/MessageContent.tsx
@@ -15,6 +15,9 @@ const useStyles = makeStyles({
       color: "inherit",
     },
   },
+  youtubeWrapper: {
+    maxWidth: "640px",
+  }
 });
 
 type Props = {
@@ -31,10 +34,10 @@ export default function MessageContent({ content, renderYoutubeVideos = false }:
       {text}
     </a>
   );
+  const screenWidth = window.innerWidth;
 
   const opts = {
-    height: "390",
-    width: "640",
+    width: "100%",
     host: "https://www.youtube-nocookie.com",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
@@ -53,7 +56,11 @@ export default function MessageContent({ content, renderYoutubeVideos = false }:
             let video_id = YouTubeGetID(w);
             const ampersandPosition = video_id.indexOf("&");
             if (ampersandPosition !== -1) video_id = video_id.substring(0, ampersandPosition);
-            return <YouTube videoId={video_id} opts={opts as any} />;
+            return (
+              <div className={classes.youtubeWrapper}>
+                <YouTube videoId={video_id} opts={opts as any} />
+              </div>
+            )
           } else {
             return <Linkify componentDecorator={componentDecorator}>{w + " "}</Linkify>;
           }
