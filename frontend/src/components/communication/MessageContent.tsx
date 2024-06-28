e102c112f20806cb028264ce16360de1b32ae128
fateme ramezanpour <ramezanpourftm@gmail.com>
2024-06-28 16:23:41 +0200

    clean the code after fixing the bug 1298

--- frontend/src/components/communication/MessageContent.tsx
+++ frontend/src/components/communication/MessageContent.tsx
@@ -34,7 +34,6 @@ export default function MessageContent({ content, renderYoutubeVideos = false }:
       {text}
     </a>
   );
-  const screenWidth = window.innerWidth;
 
   const opts = {
     width: "100%",
