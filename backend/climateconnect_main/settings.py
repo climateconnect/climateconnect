aa1a307a652dfc440c0f1c0d9eeec6ebf052a8fc
fateme ramezanpour <ramezanpourftm@gmail.com>
2024-10-31 12:02:21 +0100

    change debug

--- backend/climateconnect_main/settings.py
+++ backend/climateconnect_main/settings.py
@@ -38,8 +38,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = env("DEBUG", "false") == "true"  # needs to gravitate towards False!
-DEBUG = "false"
+DEBUG = env("DEBUG", "false") == "true"  # needs to gravitate towards False!
 ALLOWED_HOSTS = get_allowed_hosts(env("ALLOWED_HOSTS"))
 
 INTERNAL_IPS = [
