6da5950f20d00b2840fa9777f42e5867a976e0fa
Christoph Stoll <christoph.stoll@climateconnect.earth>
2025-01-09 08:40:22 +0100

    install pdm via pip

--- start_backend.sh
+++ start_backend.sh
@@ -3,9 +3,7 @@
 apt-get update -qq && apt-get install binutils libproj-dev gdal-bin -yqq
 
 # Install pdm
-curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 - --version 2.22.1
-#post install for pdm
-export PATH=/root/.local/bin:$PATH
+pip install pdm
 
 # Go to backend folder
 cd backend
