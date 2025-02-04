d0e40131c3467b7a7e8eb924dd026b4a1f3605cc
Christoph Stoll <christoph.stoll@climateconnect.earth>
2025-02-04 10:34:27 +0100

    fix 'yarn tsc'

--- frontend/pages/createorganization.tsx
+++ frontend/pages/createorganization.tsx
@@ -522,6 +522,7 @@ const parseOrganizationForRequest = async (
     source_language: sourceLanguage,
     parent_organization: undefined,
     school: undefined,
+    created_in_hub: undefined
   };
   if (o.parentorganization) organization.parent_organization = o.parentorganization;
   if (o.background_image)
