c435a6a74cd2aefa167800d00b4f32611dfc434c
Fateme Ramezanpour <ramezanpourftm@gmail.com>
2025-03-21 18:40:26 +0100

    fixed supporters and ambassadors box language when changing the language (#1464)

--- frontend/pages/hubs/[hubUrl].tsx
+++ frontend/pages/hubs/[hubUrl].tsx
@@ -192,7 +192,7 @@ export default function Hub({
         setHubSupporters(retrivedHubSupporters);
       }
     })();
-  }, []);
+  }, [locale]);
 
   const isSmallScreen = useMediaQuery<Theme>(theme.breakpoints.down("md"));
 
