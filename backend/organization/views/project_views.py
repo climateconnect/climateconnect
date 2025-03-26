9e4c196f6316560f7eb5fc8cb26cf0583f554e17
Karol Bakas <karol.bakas@fau.de>
2025-03-26 20:15:48 +0100

    created feature flag for improved caching

--- backend/organization/views/project_views.py
+++ backend/organization/views/project_views.py
@@ -23,6 +23,8 @@ from climateconnect_main.utility.general import get_image_from_data_url
 
 from dateutil.parser import parse
 
+from django.conf import settings
+
 from django.core.cache import cache
 
 from django.contrib.auth.models import User
@@ -302,36 +304,46 @@ class ListProjectsView(ListAPIView):
             )
             projects = projects.filter(loc__in=location_ids)
 
-        ### retrieve all cached rankings for all projects
-        ### and recalculate the rankings for projects that are missing cached rankings
+        if settings.CACHE_BACHED_RANK_REQUEST:
+            ### retrieve all cached rankings for all projects
+            ### and recalculate the rankings for projects that are missing cached rankings
 
-        # generate all cache keys cached rankings of all projects
-        project_ids = [project.id for project in projects]
-        cache_keys = [
-            generate_project_ranking_cache_key(project_id=pid) for pid in project_ids
-        ]
+            # generate all cache keys cached rankings of all projects
+            project_ids = [project.id for project in projects]
+            cache_keys = [
+                generate_project_ranking_cache_key(project_id=pid)
+                for pid in project_ids
+            ]
 
-        # prefetch the cached rankings all at once
-        cached_rankings = cache.get_many(cache_keys)
+            # prefetch the cached rankings all at once
+            cached_rankings = cache.get_many(cache_keys)
 
-        # save the cached values to the view
-        _cached_rankings = {
-            key: cached_rankings[key] if key in cached_rankings else project.ranking
-            for key, project in zip(cache_keys, projects)
-        }
+            # save the cached values to the view
+            _cached_rankings = {
+                key: cached_rankings[key] if key in cached_rankings else project.ranking
+                for key, project in zip(cache_keys, projects)
+            }
 
-        # sort by _cached_rankings and then drop the cache_keys
-        project_ids_sorted = map(
-            lambda x: x[0],
-            sorted(
-                zip(project_ids, cache_keys),
-                key=lambda x: _cached_rankings[x[1]] * (-1),
-            ),
-        )
+            # sort by _cached_rankings and then drop the cache_keys
+            project_ids = map(
+                lambda x: x[0],
+                sorted(
+                    zip(project_ids, cache_keys),
+                    key=lambda x: _cached_rankings[x[1]] * (-1),
+                ),
+            )
+        else:
+            project_ids = [
+                project.id
+                for project in sorted(
+                    projects, key=lambda project: -project.cached_ranking
+                )
+            ]
+
         preferred_order = Case(
             *(
                 When(id=id, then=position)
-                for position, id in enumerate(project_ids_sorted, start=1)
+                for position, id in enumerate(project_ids, start=1)
             )
         )
         return projects.order_by(preferred_order)
