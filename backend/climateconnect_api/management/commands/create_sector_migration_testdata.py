4ed7eece3d7a1fdee9f25574c9eef605de8861d9
Karol Bakas <karol.bakas@fau.de>
2025-04-22 15:02:48 +0200

    save current state

---
+++ backend/climateconnect_api/management/commands/create_sector_migration_testdata.py
@@ -0,0 +1,345 @@
+import random
+from django.utils import timezone
+
+from django.core.management.base import BaseCommand
+
+from climateconnect_api.models.common import Availability
+from climateconnect_api.models.language import Language
+from climateconnect_api.models.role import Role
+from climateconnect_api.models.user import UserProfile
+from organization.models.members import ProjectMember
+from organization.models.project import Project, ProjectParents
+from organization.models.status import ProjectStatus
+from organization.models.tags import ProjectTagging, ProjectTags
+
+TEST_PREFIX = "T-SECTOR"
+
+
+class ProjectTagDef:
+    def __init__(self, name, key, parent_tag=None):
+        self.name = name
+        self.key = key
+        self.parent_tag = parent_tag
+
+
+DEFINITIONS = [
+    ProjectTagDef("Air", "air"),
+    ProjectTagDef("Fighting air pollution", "fightingairpollution", "air"),
+    ProjectTagDef("Buildings", "buildings"),
+    ProjectTagDef("Construction", "construction", "buildings"),
+    ProjectTagDef("Energetic renovation", "energeticrenovation", "buildings"),
+    ProjectTagDef("Room cooling", "roomcooling", "buildings"),
+    ProjectTagDef("Room heating", "roomheating", "buildings"),
+    ProjectTagDef("Climate change adaption", "climatechangeadaption"),
+    ProjectTagDef(
+        "Building resilience to climate-related disasters",
+        "buildingresiliencetoclimate-relateddisasters",
+        "climatechangeadaption",
+    ),
+    ProjectTagDef(
+        "Education on climate change adaption",
+        "educationonclimatechangeadaption",
+        "climatechangeadaption",
+    ),
+    ProjectTagDef("Climate justice", "climatejustice"),
+    ProjectTagDef(
+        "Fighting discrimination", "fightingdiscrimination", "climatejustice"
+    ),
+    ProjectTagDef("Protecting human rights", "protectinghumanrights", "climatejustice"),
+    ProjectTagDef(
+        "Supporting global social justice",
+        "supportingglobalsocialjustice",
+        "climatejustice",
+    ),
+    ProjectTagDef(
+        "Supporting marginalized groups",
+        "supportingmarginalizedgroups",
+        "climatejustice",
+    ),
+    ProjectTagDef("Education & Social action", "education&socialaction"),
+    ProjectTagDef(
+        "Climate change sensibilisation",
+        "climatechangesensibilisation",
+        "education&socialaction",
+    ),
+    ProjectTagDef(
+        "Demonstrations & strikes", "demonstrations&strikes", "education&socialaction"
+    ),
+    ProjectTagDef("Education", "education", "education&socialaction"),
+    ProjectTagDef(
+        "Encouraging climate-friendly lifestyle",
+        "encouragingclimate-friendlylifestyle",
+        "education&socialaction",
+    ),
+    ProjectTagDef("Petition", "petition", "education&socialaction"),
+    ProjectTagDef("Energy", "energy"),
+    ProjectTagDef("Biomass energy", "biomassenergy", "energy"),
+    ProjectTagDef("Energy distribution", "energydistribution", "energy"),
+    ProjectTagDef("Energy storage", "energystorage", "energy"),
+    ProjectTagDef("Green fuels", "greenfuels", "energy"),
+    ProjectTagDef("Hydrogen", "hydrogen", "energy"),
+    ProjectTagDef("Hydropower energy", "hydropowerenergy", "energy"),
+    ProjectTagDef("Improving energy efficiency", "improvingenergyefficiency", "energy"),
+    ProjectTagDef(
+        "Making fossil fuel technology cleaner",
+        "makingfossilfueltechnologycleaner",
+        "energy",
+    ),
+    ProjectTagDef("Nuclear energy", "nuclearenergy", "energy"),
+    ProjectTagDef("Other renewable energy", "otherrenewableenergy", "energy"),
+    ProjectTagDef("Solar & photovoltaic energy", "solar&photovoltaicenergy", "energy"),
+    ProjectTagDef("Wind energy", "windenergy", "energy"),
+    ProjectTagDef("Event", "event"),
+    ProjectTagDef("Food", "food"),
+    ProjectTagDef(
+        "Encouraging a plant-based lifestyle",
+        "encouragingaplant-basedlifestyle",
+        "food",
+    ),
+    ProjectTagDef("Food production", "foodproduction", "food"),
+    ProjectTagDef("Gastronomy/Catering", "gastronomy/catering", "food"),
+    ProjectTagDef("Lowering food waste", "loweringfoodwaste", "food"),
+    ProjectTagDef("Funding", "funding"),
+    ProjectTagDef("Geoengineering", "geoengineering"),
+    ProjectTagDef("Greenhouse gas capture", "greenhousegascapture", "geoengineering"),
+    ProjectTagDef("Greenhouse gas reuse", "greenhousegasreuse", "geoengineering"),
+    ProjectTagDef("Greenhouse gas storage", "greenhousegasstorage", "geoengineering"),
+    ProjectTagDef("Land use", "landuse"),
+    ProjectTagDef(
+        "Afforestation/Reforestation", "afforestation/reforestation", "landuse"
+    ),
+    ProjectTagDef(
+        "Climate-friendly agriculture", "climate-friendlyagriculture", "landuse"
+    ),
+    ProjectTagDef(
+        "Conserving/restoring land ecosystems",
+        "conserving/restoringlandecosystems",
+        "landuse",
+    ),
+    ProjectTagDef(
+        "Conserving/restoring peatlands", "conserving/restoringpeatlands", "landuse"
+    ),
+    ProjectTagDef(
+        "Conserving/restoring permafrost", "conserving/restoringpermafrost", "landuse"
+    ),
+    ProjectTagDef("Fighting biodiversity loss", "fightingbiodiversityloss", "landuse"),
+    ProjectTagDef("Fighting deforestation", "fightingdeforestation", "landuse"),
+    ProjectTagDef(
+        "Restoring degraded land/soil", "restoringdegradedland/soil", "landuse"
+    ),
+    ProjectTagDef(
+        "Sustainable forest management", "sustainableforestmanagement", "landuse"
+    ),
+    ProjectTagDef("Mobility", "mobility"),
+    ProjectTagDef("Air transportation", "airtransportation", "mobility"),
+    ProjectTagDef("Freight transport", "freighttransport", "mobility"),
+    ProjectTagDef("Land transportation", "landtransportation", "mobility"),
+    ProjectTagDef("Passenger transport", "passengertransport", "mobility"),
+    ProjectTagDef("Transport infrastructure", "transportinfrastructure", "mobility"),
+    ProjectTagDef("Water transportation", "watertransportation", "mobility"),
+    ProjectTagDef("Policy & Governance", "policy&governance"),
+    ProjectTagDef("Climate consulting", "climateconsulting", "policy&governance"),
+    ProjectTagDef("Climate lobbyism", "climatelobbyism", "policy&governance"),
+    ProjectTagDef(
+        "Fighting climate-unfriendly subsidies",
+        "fightingclimate-unfriendlysubsidies",
+        "policy&governance",
+    ),
+    ProjectTagDef(
+        "Fighting environmental corruption",
+        "fightingenvironmentalcorruption",
+        "policy&governance",
+    ),
+    ProjectTagDef("Implementing policy", "implementingpolicy", "policy&governance"),
+    ProjectTagDef("Policy suggestions", "policysuggestions", "policy&governance"),
+    ProjectTagDef("Product", "product"),
+    ProjectTagDef(
+        "Production, consumption and recycling", "production,consumptionandrecycling"
+    ),
+    ProjectTagDef(
+        "Circular economy", "circulareconomy", "production,consumptionandrecycling"
+    ),
+    ProjectTagDef(
+        "Climate-friendly fashion",
+        "climate-friendlyfashion",
+        "production,consumptionandrecycling",
+    ),
+    ProjectTagDef(
+        "Climate-friendly production practices",
+        "climate-friendlyproductionpractices",
+        "production,consumptionandrecycling",
+    ),
+    ProjectTagDef("Recycling", "recycling", "production,consumptionandrecycling"),
+    ProjectTagDef(
+        "Reducing climate-unfriendly consumption",
+        "reducingclimate-unfriendlyconsumption",
+        "production,consumptionandrecycling",
+    ),
+    ProjectTagDef(
+        "Reducing waste", "reducingwaste", "production,consumptionandrecycling"
+    ),
+    ProjectTagDef(
+        "Sustainable use of natural resources",
+        "sustainableuseofnaturalresources",
+        "production,consumptionandrecycling",
+    ),
+    ProjectTagDef("Research", "research"),
+    ProjectTagDef(
+        "Research in natural science", "researchinnaturalscience", "research"
+    ),
+    ProjectTagDef("Research in social science", "researchinsocialscience", "research"),
+    ProjectTagDef("Research in technology", "researchintechnology", "research"),
+    ProjectTagDef("Water", "water"),
+    ProjectTagDef(
+        "Fighting ocean acidification", "fightingoceanacidification", "water"
+    ),
+    ProjectTagDef("Fighting overfishing", "fightingoverfishing", "water"),
+    ProjectTagDef(
+        "Improving water use efficiency", "improvingwateruseefficiency", "water"
+    ),
+    ProjectTagDef(
+        "Protecting marine ecosystems", "protectingmarineecosystems", "water"
+    ),
+    ProjectTagDef("Reducing marine pollution", "reducingmarinepollution", "water"),
+    ProjectTagDef("Water recycling", "waterrecycling", "water"),
+]
+
+# TODO: remove this line after testing
+DEFINITIONS = DEFINITIONS[:10]
+
+
+def create_project_tag(tagdef: ProjectTagDef):
+    if ProjectTags.objects.filter(key=tagdef.key).exists():
+        print(
+            "[x]\tProject tag with key {} already exists. Skipping tag {}".format(
+                tagdef.key, tagdef.name
+            )
+        )
+        return
+
+    if tagdef.parent_tag:
+        parent_tag = ProjectTags.objects.filter(key=tagdef.parent_tag).first()
+        if not parent_tag:
+            print(
+                "[x]\tParent tag with key {} does not exist. Skipping tag {}".format(
+                    tagdef.parent_tag, tagdef.name
+                )
+            )
+            return
+        ProjectTags.objects.create(
+            name=tagdef.name,
+            parent_tag=parent_tag,
+        )
+        print(f"[✓]\t{tagdef.name} created")
+    else:
+        ProjectTags.objects.create(
+            name=tagdef.name,
+        )
+        print(f"[✓]\t{tagdef.name} created")
+
+
+def create_project_for(key, tags):
+    english_language = Language.objects.filter(language_code="en")[0]
+
+    name = f"{TEST_PREFIX}-{key}"
+    url_slug = name.replace(" ", "")
+
+    if not Project.objects.filter(name=name).exists():
+        # Buffer some start date to create more
+        # sense of older projects. This originated out of
+        # fixing a bug around the "timeago" utility, where
+        # timeago would round up to 2 years, when a project
+        # is only 1 year and 1 day old. This should create the
+        # UTC timestamp, e.g. 2020-02-04T01:33:45.276997Z
+        one_year_and_one_day_ago = timezone.now() - timezone.timedelta(days=366)
+
+        project = Project.objects.create(
+            name=name,
+            city="SECTOR_TEST_DATA",
+            collaborators_welcome=True,
+            country="Germany",
+            short_description="This is a test project.",
+            start_date=one_year_and_one_day_ago,
+            status=ProjectStatus.objects.get(name="In Progress"),
+            url_slug=url_slug,
+            language=english_language,
+        )
+
+        parent_user = UserProfile.objects.all()[1].user
+
+        ProjectParents.objects.create(project=project, parent_user=parent_user)
+        admin_role = Role.objects.get(role_type=Role.ALL_TYPE)
+        all_availabilities = list(Availability.objects.all())
+        example_availability = random.choice(all_availabilities)
+        ProjectMember.objects.create(
+            project=project,
+            user=parent_user,
+            role=admin_role,
+            availability=example_availability,
+            role_in_project="Project manager",
+        )
+
+        for tag in tags:
+            ProjectTagging.objects.create(
+                project=project,
+                project_tag=tag,
+            )
+        print("[✓]\t{} project created.".format(name))
+    else:
+        print("[x]\t{} project already exists.".format(name))
+
+
+def create_all_project_tags():
+    print("Creating project tags test data...")
+    for tagdef in DEFINITIONS:
+        create_project_tag(tagdef)
+    print("finished creating project tags test data!")
+
+
+def create_projects_related_to_tags():
+    print("-" * 40)
+    print("Creating test data: projects with one tag ...")
+    for tagdef in DEFINITIONS:
+        tag = ProjectTags.objects.filter(key=tagdef.key).first()
+        if not tag:
+            print(
+                "[!]\tTag with key {} does not exist. Skipping tag {}".format(
+                    tagdef.key, tagdef.name
+                )
+            )
+            continue
+
+        key = tagdef.key + "-project"
+        create_project_for(key, [tag])
+
+    print("-" * 40)
+    print("Creating test data: projects with two tag ...")
+
+    for i, tagdef1 in enumerate(DEFINITIONS):
+        for j, tagdef2 in enumerate(DEFINITIONS):
+            if i >= j:
+                continue
+            # print("i: {}, j: {}".format(i, j))
+            # print(tagdef1.key, tagdef2.key)
+            tag1 = ProjectTags.objects.filter(key=tagdef1.key).first()
+            tag2 = ProjectTags.objects.filter(key=tagdef2.key).first()
+            if not tag1 or not tag2:
+                print(
+                    "[!]\tTag with key {} does not exist. Skipping tag {}".format(
+                        tagdef.key, tagdef.name
+                    )
+                )
+                continue
+
+            key = tagdef1.key + "&" + tagdef2.key + "-project"
+            create_project_for(key, [tag1, tag2])
+    print("finished creating project tags test data!")
+
+
+class Command(BaseCommand):
+    help = "Create badges data for users"
+
+    def handle(self, *args, **options) -> str:
+
+        create_all_project_tags()
+        create_projects_related_to_tags()
---
+++ backend/organization/models/sector.py
@@ -0,0 +1,112 @@
+from django.db import models
+
+from organization.models import Organization
+from organization.models.project import Project
+
+
+class Sector(models.Model):
+    name = models.CharField(
+        help_text="Name of the sector",
+        verbose_name="Name",
+        max_length=256,
+    )
+
+    name_de_translation = models.CharField(
+        help_text="German traslation of the name column",
+        verbose_name="Name DE translation",
+        max_length=256,
+        null=True,
+        blank=True,
+    )
+
+    # Adding this because we use project tags to filter project and its not possible to filter Name because
+    # it contains spaces.
+    key = models.CharField(
+        help_text="unique search key of the sector. Example: `Student organization` becomes studentorganization",
+        verbose_name="Key",
+        max_length=256,
+        unique=True,
+    )
+
+    created_at = models.DateTimeField(
+        help_text="Time when sector was created",
+        verbose_name="Created at",
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        help_text="Time when sector was updated",
+        verbose_name="Updated at",
+        auto_now=True,
+    )
+
+    class Meta:
+        app_label = "organization"
+        verbose_name = "Sector"
+        ordering = ["id"]
+
+    def __str__(self):
+        return "%s" % self.name
+
+
+class ProjectSectorMapping(models.Model):
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(
+        help_text="Time when mapping was created",
+        verbose_name="Created At",
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        help_text="Time when mapping was updated",
+        verbose_name="Updated At",
+        auto_now=True,
+    )
+
+    order = models.PositiveSmallIntegerField(
+        help_text="The bigger the number, the more to the top this sector will be displayed",
+        verbose_name="Position of tag",
+        default=0,
+    )
+
+    class Meta:
+        app_label = "organization"
+        verbose_name = "Project Sector Mapping"
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"{self.sector.name}--{self.project.name}"
+
+
+class OrganizationSectorMapping(models.Model):
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(
+        help_text="Time when mapping was created",
+        verbose_name="Created At",
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        help_text="Time when mapping was updated",
+        verbose_name="Updated At",
+        auto_now=True,
+    )
+
+    order = models.PositiveSmallIntegerField(
+        help_text="The bigger the number, the more to the top this sector will be displayed",
+        verbose_name="Position of tag",
+        default=0,
+    )
+
+    class Meta:
+        app_label = "organization"
+        verbose_name = "Organization Sector Mapping"
+        ordering = ["id"]
+
+    #
+    def __str__(self):
+        return f"{self.sector.name}--{self.organization.name}"
