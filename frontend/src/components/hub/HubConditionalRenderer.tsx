43c16d6fbbb4dea5728b3e4aec9c32b23312c965
fateme ramezanpour <ramezanpourftm@gmail.com>
2025-05-19 13:35:05 +0200

    Revert "added comment for new component"
    This reverts commit 2dab27d96f151028f5105b97a14dc1756afacdbb.

--- frontend/src/components/hub/HubConditionalRenderer.tsx
+++ frontend/src/components/hub/HubConditionalRenderer.tsx
@@ -1,26 +1,3 @@
-/**
- * HubConditionalRenderer
- *
- * Renders a specific component based on the given `hubUrl`.
- * - If `hubUrl` matches one of the values in `HUBS_NAME`, it renders the `Component`.
- * - Otherwise, it renders the `DefaultComponent` (if provided).
- * - If neither condition is met, it returns null.
- *
- * Useful for conditionally displaying custom UI for specific hub environments (e.g., "prio1").
- *
- * Example:
- *  <HubConditionalRenderer
-      hubUrl={hubUrl}
-      Component={EnPrio1Welcome}
-      DefaultComponent={LoggedOutLocationHubBox}
-      defaultComponentProps={{
-        headline: headline,
-        isLocationHub: isLocationHub,
-        location: hubData.name,
-      }}
-    />
- */
-
 import React from "react";
 
 interface HubConditionalRendererProps {
