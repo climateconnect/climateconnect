95cebb18c3614d2547958aa3203d050793b2854f
fateme ramezanpour <ramezanpourftm@gmail.com>
2025-06-23 16:55:57 +0200

    deleted console.log

--- frontend/src/components/shareProject/ShareProjectRoot.tsx
+++ frontend/src/components/shareProject/ShareProjectRoot.tsx
@@ -266,7 +266,6 @@ export default function ShareProjectRoot({
         sectors: sectorsAfterRemoval,
     });
   };
-  // console.log("ShareProjectRoot: project.sectors", project.sectors);
   
   return (
     <>
