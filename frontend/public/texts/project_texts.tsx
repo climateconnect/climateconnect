d76015f8e6b3f06fb0801e3538adcf886cef21ec
Christoph Stoll <christoph.stoll@climateconnect.earth>
2025-07-21 12:14:49 +0200

    rename sectors to topics, remove unnecessary import

--- frontend/public/texts/project_texts.tsx
+++ frontend/public/texts/project_texts.tsx
@@ -765,24 +765,24 @@ export default function getProjectTexts({ project, user, url_slug, locale, creat
       ),
     },
     add_sectors_that_fit: {
-      en: "Add Sectors which fit:",
-      de: "Füge Sektoren hinzu, die passen:",
+      en: "Add topics which fit:",
+      de: "Füge Themenfelder hinzu, die passen:",
     },
     select_1_to_3_sectors_that_fit_your_project: {
-      en: "Select 1-3 sectors that fit your project",
-      de: "Wähle 1-3 Sektoren, die zu deinem Projekt passen",
+      en: "Select 1-3 topics that fit your project",
+      de: "Wähle 1-3 Themenfelder, die zu deinem Projekt passen",
     },
     add_a_sector_where_you_are_active: {
-      en: "Add a Sector",
-      de: "Füge einen Sektor hinzu",
+      en: "Add a Topic",
+      de: "Füge ein Themenfeld hinzu",
     },
     please_choose_at_least_one_sector: {
-      en: "Please choose at least one sector!",
-      de: "Bitte wähle mindestens eine Sektor aus!",
+      en: "Please choose at least one topic!",
+      de: "Bitte wähle mindestens ein Themenfeld aus!",
     },
     you_can_only_choose_up_to_3_sectors: {
-      en: "You can only choose up to 3 sectors.",
-      de: "Du kannst nur bis zu 3 Sektoren auswählen",
+      en: "You can only choose up to 3 topics.",
+      de: "Du kannst nur bis zu 3 Themenfelder auswählen",
     },
     you_can_combine_categories_text: {
       en: `You can combine categories. For example if you fund treeplanting, select both
