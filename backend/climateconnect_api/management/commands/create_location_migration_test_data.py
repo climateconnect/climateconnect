6d9328b270d6fa410628e480c210c24d2cf4a035
Katharina Auer <katharina.auer@climateconnect.earth>
2025-10-27 18:38:05 +0100

    format and lint corrections

--- backend/climateconnect_api/management/commands/create_location_migration_test_data.py
+++ backend/climateconnect_api/management/commands/create_location_migration_test_data.py
@@ -3,27 +3,33 @@ import csv
 from pathlib import Path
 from location.models import Location
 
-DELETE_PREFIX = 'DELETE-'
+DELETE_PREFIX = "DELETE-"
+
 
 def safe_int(value_str):
     return int(value_str.strip()) if value_str and value_str.strip() else None
+
+
 def safe_bool(value_str):
     if not value_str:
         return None
-    cleaned_value = value_str.strip().replace('“', '').replace('”', '').replace('"', '').lower()
-    if cleaned_value in ('true'):
+    cleaned_value = (
+        value_str.strip().replace("“", "").replace("”", "").replace('"', "").lower()
+    )
+    if cleaned_value in ("true"):
         return True
     return False
+
 
 def create_test_data_from_csv(file_path: str, limit: int = None) -> None:
     csv_file = Path(file_path)
     locations_to_create = []
     try:
-        with open(csv_file, mode='r', encoding='utf-8') as file:
+        with open(csv_file, mode="r", encoding="utf-8") as file:
             reader = csv.DictReader(file)
-            if reader.fieldnames: #clean fieldnames from invisible strings
+            if reader.fieldnames:  # clean fieldnames from invisible strings
                 cleaned_fieldnames = [
-                    name.strip().replace('"', '').replace('\ufeff', '')
+                    name.strip().replace('"', "").replace("\ufeff", "")
                     for name in reader.fieldnames
                 ]
                 reader.fieldnames = cleaned_fieldnames
@@ -31,19 +37,19 @@ def create_test_data_from_csv(file_path: str, limit: int = None) -> None:
             if limit is not None:
                 data_to_process = data_to_process[:limit]
             for line in data_to_process:
-                test_place_name = DELETE_PREFIX + line['place_name']
+                test_place_name = DELETE_PREFIX + line["place_name"]
                 loc = Location(
-                    osm_id = safe_int(line['osm_id']),
-                    place_id = safe_int(line['place_id']),
-                    is_stub = safe_bool(line['is_stub']),
-                    is_formatted = safe_bool(line['is_formatted']),
-                    name = line['name'],
-                    place_name = test_place_name,
-                    exact_address = line['exact_address'],
-                    city = line['city'],
-                    state = line['state'],
-                    country = line['country'],
-                    centre_point = line['centre_point']
+                    osm_id=safe_int(line["osm_id"]),
+                    place_id=safe_int(line["place_id"]),
+                    is_stub=safe_bool(line["is_stub"]),
+                    is_formatted=safe_bool(line["is_formatted"]),
+                    name=line["name"],
+                    place_name=test_place_name,
+                    exact_address=line["exact_address"],
+                    city=line["city"],
+                    state=line["state"],
+                    country=line["country"],
+                    centre_point=line["centre_point"],
                 )
                 locations_to_create.append(loc)
 
@@ -54,6 +60,7 @@ def create_test_data_from_csv(file_path: str, limit: int = None) -> None:
         print(f"Error: file not found at path {csv_file}")
     except Exception as e:
         print(f"Error: {e}")
+
 
 def delete_test_data() -> None:
     Location.objects.filter(place_name__startswith=DELETE_PREFIX).delete()
@@ -79,7 +86,7 @@ class Command(BaseCommand):
             "--file",
             type=str,
             help="Path to the CSV file containing test data",
-            #required=True,
+            # required=True,
         )
 
     def handle(self, *args, **options) -> str:
@@ -87,10 +94,12 @@ class Command(BaseCommand):
         limit = options.get("limit")
         if options.get("delete"):
             delete_test_data()
-        elif file_path: 
+        elif file_path:
             create_test_data_from_csv(file_path, limit)
         else:
-            self.stdout.write(self.style.ERROR("no mode chosen, use --delete or provide a --file"))
+            self.stdout.write(
+                self.style.ERROR("no mode chosen, use --delete or provide a --file")
+            )
 
 
-#python manage.py create_location_migration_test_data.py --file <path_to_csv_table_with_test_database>
+# python manage.py create_location_migration_test_data.py --file <path_to_csv_table_with_test_database>
