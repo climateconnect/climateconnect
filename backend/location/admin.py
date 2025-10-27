35291dc0435f78ca4ab853ce8e3d17619f6d7f5e
Katharina Auer <katharina.auer@climateconnect.earth>
2025-10-27 18:31:37 +0100

    added search locations funcitonality (search by osm or name)

--- backend/location/admin.py
+++ backend/location/admin.py
@@ -3,7 +3,24 @@ from django.contrib import admin
 
 # Register your models here.
 
-admin.site.register(Location, admin.ModelAdmin)
+class LocationAdmin(admin.ModelAdmin):
+    search_fields = (
+        'name', 
+        'osm_id',
+    )
+    
+    list_filter = (
+        'osm_type', 
+    )
+    
+    list_display = (
+        'name', 
+        'osm_type',
+        'osm_id', 
+        'place_id',
+    )
+
+admin.site.register(Location, LocationAdmin)
 
 
 class LocationTranslationAdmin(admin.ModelAdmin):
