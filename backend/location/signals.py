15222dc3d603affda55901d366b62f5f1856048f
Katharina Auer <katharina.auer@climateconnect.earth>
2025-11-16 00:41:32 +0100

    rebase location translation to current save_osm_types branch

--- backend/location/signals.py
+++ backend/location/signals.py
@@ -1,12 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-#from location.models import Location
+from location.models import Location
 from location.tasks import fetch_and_create_location_translations
 
 
-
-@receiver(post_save, sender="location.Location")
+@receiver(post_save, sender=Location)
 def find_location_translations(sender, instance, created, **kwargs):
 
     '''this functions is triggered whenever a Location is created successfully 
