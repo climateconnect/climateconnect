e4f5a0a88956132db4800cfb4d537cecfd3bdaa4
Harald Walker <HaraldWalker@users.noreply.github.com>
2025-12-01 14:41:38 +0100

    Add hamlet location type to location bar search (#1678)
    * Fix jest and clean up
    
    * Add hamlet type search
    
    * Fix exact name handling when location is a city
    
    * Update node version in pull request workflow
    
    * Refactor code
    
    * Enable yarn tests in workflow
    
    * Apply name formatting changes also to backend
    
    * Address review feedback
    
    * Minor change and cleanup

--- backend/location/tests/test_utility.py
+++ backend/location/tests/test_utility.py
@@ -2,8 +2,125 @@ import unittest
 
 from django.test import TestCase, override_settings
 
-from location.utility import _osm_type_char, get_location
+from location.utility import _osm_type_char, get_location, format_location_name
 from location.models import Location
+
+class TestFormatLocationName(TestCase):
+    def test_country_location(self):
+        """
+        Test formatting when the location is a country.
+        """
+        location = {
+            "type": "administrative",
+            "address": {"country": "Canada", "country_code": "ca"},
+            "display_name": "Canada",
+        }
+        expected_result = {
+            "country": "Canada",
+            "city": "",
+            "state": "",
+            "name": "Canada",
+        }
+        result = format_location_name(location)
+        self.assertEqual(expected_result, result)
+
+    def test_city_location(self):
+        """
+        Test formatting when the location is a city.
+        """
+        location = {
+            "type": "city",
+            "address": {
+                "city": "City-Sample",
+                "state": "State-Sample",
+                "country": "Country-Sample",
+            },
+            "display_name": "Display-Name-Sample",
+        }
+        expected_result = {
+            "city": "City-Sample",
+            "state": "State-Sample",
+            "country": "Country-Sample",
+            "name": "City-Sample, Country-Sample",
+        }
+        result = format_location_name(location)
+        self.assertEqual(expected_result, result)
+
+    def test_location_with_hamlet(self):
+        """
+        Test formatting when the location is a hamlet.
+        """
+        location = {
+            "type": "hamlet",
+            "addresstype": "hamlet",
+            "class": "place",
+            "name": "Hamlet-Sample",
+            "address": {
+                "hamlet": "Hamlet-Sample",
+                "town": "Town-Sample",
+                "county": "County-Sample",
+                "state": "State-Sample",
+                "country": "Country-Sample",
+            },
+            "display_name": "Display-Name-Sample",
+        }
+        expected_result = {
+            "city": "Hamlet-Sample",
+            "state": "State-Sample",
+            "country": "Country-Sample",
+            "name": "Hamlet-Sample, Town-Sample, Country-Sample",
+        }
+        result = format_location_name(location)
+        self.assertEqual(expected_result, result)
+
+    def test_location_with_hamlet_without_town(self):
+        """
+        Test formatting when the location is a hamlet without town.
+        """
+        location = {
+            "type": "hamlet",
+            "addresstype": "hamlet",
+            "class": "place",
+            "name": "Hamlet-Sample",
+            "address": {
+                "hamlet": "Hamlet-Sample",
+                "county": "County-Sample",
+                "state": "State-Sample",
+                "country": "Country-Sample",
+            },
+            "display_name": "Display-Name-Sample",
+        }
+        expected_result = {
+            "city": "Hamlet-Sample",
+            "state": "State-Sample",
+            "country": "Country-Sample",
+            "name": "Hamlet-Sample, County-Sample, Country-Sample",
+        }
+        result = format_location_name(location)
+        self.assertEqual(expected_result, result)
+
+    def test_location_with_custom_name_mapping(self):
+        """
+        Test formatting when the name is overridden by the CUSTOM_NAME_MAPPINGS in utility.py.
+        """
+        location = {
+            "type": "administrative",
+            "addresstype": "state",
+            "name": "Scotland",
+            "address": {
+                "state": "Scotland",
+                "country": "United Kingdom",
+            },
+            "display_name": "Scotland, United Kingdom",
+        }
+        expected_result = {
+            "city": "Scotland (state)",
+            "state": "Scotland",
+            "country": "United Kingdom",
+            "name": "Scotland",
+        }
+        result = format_location_name(location)
+        self.assertEqual(expected_result, result)
 
 
 class TestOsmTypeChar(unittest.TestCase):
