6d6c3c086558e523adba2b9f8ca1c33acecb4ebf
Katharina Auer <katharina.auer@climateconnect.earth>
2025-12-17 16:29:50 +0100

    forgot one

--- backend/location/models.py
+++ backend/location/models.py
@@ -3,6 +3,7 @@ import logging
 import requests
 from django.conf import settings
 from django.contrib.gis.db import models
+from django.db import IntegrityError, transaction
 
 from climateconnect_api.models.language import Language
 from location.tasks import create_name_from_translation_data
