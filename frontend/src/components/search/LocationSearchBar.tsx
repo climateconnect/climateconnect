82c55103877df55e6afdd503aee924760d71193a
Katharina Auer <katharina.auer@climateconnect.earth>
2026-04-21 13:52:09 +0200

    ran yarn prettier

--- frontend/src/components/search/LocationSearchBar.tsx
+++ frontend/src/components/search/LocationSearchBar.tsx
@@ -204,11 +204,13 @@ export default function LocationSearchBar({
           const options = data.map((option) => ({
             ...option,
             // Nominatim returns "class" but our app uses "osm_class" consistently.
-            // Same with "type" and "osm_class_type". 
+            // Same with "type" and "osm_class_type".
             osm_class: option.osm_class ?? option.class,
             osm_class_type: option.osm_class_type ?? option.type,
             simple_name: getSimpleName(option, enableExactLocation),
-            key: `${option.osm_id || "na"}-${option.osm_type}-${option.osm_class ?? option.class ?? "na"}`,
+            key: `${option.osm_id || "na"}-${option.osm_type}-${
+              option.osm_class ?? option.class ?? "na"
+            }`,
           }));
           setOptions(getOptionsWithoutRedundancies(options));
           setLoading(false);
