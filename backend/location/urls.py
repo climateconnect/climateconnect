fe2b73081f648ea875113378bb2aa42253efe96e
Katharina Auer <katharina.kt.auer@gmail.com>
2026-06-16 08:31:01 +0200

    autocomplete_request_logging (#2043)
    * first changes
    
    * formatting
    
    * ran prettier
    
    * Run format on frontend
    
    * adjustments to store req count data in our db instead of redis
    
    * added another test and refactored some
    
    ---------
    
    Co-authored-by: Katharina Auer <katharina.auer@climateconnect.earth>
    Co-authored-by: Harald Walker <harald@bitwalker.eu>

--- backend/location/urls.py
+++ backend/location/urls.py
@@ -5,5 +5,18 @@ from location import location_views
 app_name = "location"
 urlpatterns = [
     # Location URLs
-    path("get_location/", location_views.GetLocationView.as_view(), name="get-location")
+    path(
+        "get_location/", location_views.GetLocationView.as_view(), name="get-location"
+    ),
+    # Nominatim autocomplete request tracking
+    path(
+        "nominatim_request_count/",
+        location_views.TrackNominatimRequestView.as_view(),
+        name="track-nominatim-request",
+    ),
+    path(
+        "nominatim_stats/",
+        location_views.NominatimStatsView.as_view(),
+        name="nominatim-stats",
+    ),
 ]
