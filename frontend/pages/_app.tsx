6b0e6271d6c0d2fb5872b82bf315eca43b0f0536
Fateme Ramezanpour <ramezanpourftm@gmail.com>
2026-06-17 09:36:08 +0200

    add Sentry to frontend (#1749)
    * add Sentry to frontend
    
    changed github workflow to include Sentry
    
    turned Sentry off on development
    
    * removed sentry test file
    
    * make improve
    
    * cleaned code
    
    * Update yarn lock after merge with master.
    
    * Adjust sentry config to comply with GDPR
    
    * Update yarn.lock
    
    * Fix package json after merge with master
    
    ---------
    
    Co-authored-by: Harald Walker <harald@bitwalker.eu>

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -15,21 +15,8 @@ import { FeatureToggles } from "../src/hooks/types/featureToggle";
 import type { CcEnvironment } from "../public/lib/environmentOperations";
 import theme from "../src/themes/theme";
 import { CcLocale, DonationGoal } from "../src/types";
-import * as Sentry from "@sentry/react";
 import "../devlink/css/global.css";
 import { getHubslugFromUrl } from "../public/lib/hubOperations";
-
-// initialize sentry
-
-Sentry.init({
-  dsn: process.env.FRONTEND_SENTRY_DSN,
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
 
 declare module "@mui/styles/defaultTheme" {
   // eslint-disable-next-line no-unused-vars
