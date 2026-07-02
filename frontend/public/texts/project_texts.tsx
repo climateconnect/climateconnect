173d814dae96761f9eab4c3f6827f18469548031
Harald Walker <harald@bitwalker.eu>
2026-07-02 09:00:35 +0200

    Rename text input field

--- frontend/public/texts/project_texts.tsx
+++ frontend/public/texts/project_texts.tsx
@@ -1395,8 +1395,8 @@ export default function getProjectTexts({ project, user, url_slug, locale, creat
       de: "Zeitfenster-Auswahl",
     },
     field_type_text: {
-      en: "Text",
-      de: "Textfeld",
+      en: "Text Input",
+      de: "Texteingabe",
     },
     registration_text_field_title_label: {
       en: "Title (shown to guest)",
