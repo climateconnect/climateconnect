acb892d13e7915d877e1051f85aa65a540fef3b2
Harald Walker <harald@bitwalker.eu>
2026-07-06 15:24:37 +0200

    Fix serializer

--- backend/organization/serializers/project.py
+++ backend/organization/serializers/project.py
@@ -334,10 +334,10 @@ class EditProjectSerializer(ProjectSerializer):
         return obj.short_description
 
     def get_description(self, obj):
-        return get_project_description(obj, get_language())
+        return obj.description
 
     def get_description_html(self, obj):
-        return get_project_description(obj, get_language())
+        return obj.description_html
 
     def get_related_hubs(self, obj):
         return [hub.url_slug for hub in obj.related_hubs.all()]
